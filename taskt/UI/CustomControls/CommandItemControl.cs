f5c3f04bd7c05f13d97fd3361e1fc2646b77f611
Jason Bayldon <jbayldon2005@gmail.com>
2018-06-03 22:25:13 -0400

    Updated License

--- taskt/UI/CustomControls/CommandItemControl.cs
+++ taskt/UI/CustomControls/CommandItemControl.cs
@@ -1,4 +1,4 @@
-﻿//Copyright (c) 2017 Jason Bayldon
+﻿//Copyright (c) 2018 Jason Bayldon
 //
 //Licensed under the Apache License, Version 2.0 (the "License");
 //you may not use this file except in compliance with the License.
