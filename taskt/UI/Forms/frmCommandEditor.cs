6ac320ceb00e9e3c673a81baddda42225d186ea5
Jason Bayldon <jbayldon2005@gmail.com>
2018-10-19 16:40:54 -0400

    Updated Width for InputBox
    https://github.com/saucepleez/taskt/issues/55

--- taskt/UI/Forms/frmCommandEditor.cs
+++ taskt/UI/Forms/frmCommandEditor.cs
@@ -482,6 +482,8 @@ namespace taskt.UI.Forms
                             InputControl.Items.Add(scriptVariable.VariableName);
                     }
 
+                    InputControl.Width = 350;
+
                     //if (scriptVariables.Count == 0)
                     //{
                     //    MessageBox.Show("The selected command requires a user variable but no user variables were found! To create a user variable, please select the 'Variables' button on the main form to define a new variable!", "No User-Defined Variables Found");
