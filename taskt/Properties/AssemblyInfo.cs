a8678ac7e238a0d624749c61020f6098afe37dd1
Jason Bayldon <jbayldon2005@gmail.com>
2019-01-05 15:01:28 -0500

    Updated Assembly Year

--- taskt/Properties/AssemblyInfo.cs
+++ taskt/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("taskt")]
-[assembly: AssemblyCopyright("Copyright ©2018")]
+[assembly: AssemblyCopyright("Copyright ©2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
