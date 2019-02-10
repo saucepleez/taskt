36fc1a3d0e031c58af92b7afefe068d0cd2567c0
Jason Bayldon <jbayldon2005@gmail.com>
2019-02-09 19:18:04 -0500

    Updated Attended Mode Color

--- taskt/UI/Forms/frmAttendedMode.Designer.cs
+++ taskt/UI/Forms/frmAttendedMode.Designer.cs
@@ -97,7 +97,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DimGray;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(59)))), ((int)(((byte)(59)))));
             this.ClientSize = new System.Drawing.Size(445, 57);
             this.Controls.Add(this.uiBtnClose);
             this.Controls.Add(this.uiBtnRun);
