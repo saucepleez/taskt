d916bedf72ce18016e4ed8666ae82dbff149c389
Jason Bayldon <jbayldon2005@gmail.com>
2019-05-06 17:36:15 -0400

    Enhanced the Number Of Times Loop Command to allow starting from a different index than 0

--- taskt/Core/Automation/Commands/BeginNumberOfTimesLoopCommand.cs
+++ taskt/Core/Automation/Commands/BeginNumberOfTimesLoopCommand.cs
@@ -25,12 +25,21 @@ namespace taskt.Core.Automation.Commands
         [Attributes.PropertyAttributes.Remarks("")]
         public string v_LoopParameter { get; set; }
 
+        [XmlAttribute]
+        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [Attributes.PropertyAttributes.PropertyDescription("Optional - Define Start Index (Default: 0)")]
+        [Attributes.PropertyAttributes.InputSpecification("Enter the starting index of the loop.")]
+        [Attributes.PropertyAttributes.SampleUsage("**5** or **10**")]
+        [Attributes.PropertyAttributes.Remarks("")]
+        public string v_LoopStart { get; set; }
+
         public BeginNumberOfTimesLoopCommand()
         {
             this.CommandName = "BeginNumberOfTimesLoopCommand";
             this.SelectionName = "Loop Number Of Times";
             this.CommandEnabled = true;
             this.CustomRendering = true;
+            this.v_LoopStart = "0";
         }
 
         public override void RunCommand(object sender, Core.Script.ScriptAction parentCommand)
@@ -52,12 +61,17 @@ namespace taskt.Core.Automation.Commands
 
             loopTimes = int.Parse(loopParameter);
 
-            for (int i = 0; i < loopTimes; i++)
+
+            int startIndex = 0;
+            int.TryParse(v_LoopStart.ConvertToUserVariable(sender), out startIndex);
+
+
+            for (int i = startIndex; i < loopTimes; i++)
             {
                 if (complexVarible != null)
                     complexVarible.CurrentPosition = i;
 
-                (i + 1).ToString().StoreInUserVariable(engine, "Loop.CurrentIndex");
+              //  (i + 1).ToString().StoreInUserVariable(engine, "Loop.CurrentIndex");
 
                 engine.ReportProgress("Starting Loop Number " + (i + 1) + "/" + loopTimes + " From Line " + loopCommand.LineNumber);
 
@@ -65,6 +79,8 @@ namespace taskt.Core.Automation.Commands
                 {
                     if (engine.IsCancellationPending)
                         return;
+
+                    (i + 1).ToString().StoreInUserVariable(engine, "Loop.CurrentIndex");
 
                     engine.ExecuteCommand(cmd);
 
@@ -77,7 +93,11 @@ namespace taskt.Core.Automation.Commands
 
                 }
 
+          
                 engine.ReportProgress("Finished Loop From Line " + loopCommand.LineNumber);
+
+       
+
             }
         }
         public override List<Control> Render(frmCommandEditor editor)
@@ -85,13 +105,23 @@ namespace taskt.Core.Automation.Commands
             base.Render(editor);
 
             RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_LoopParameter", this, editor));
+            RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_LoopStart", this, editor));
 
             return RenderedControls;
         }
 
         public override string GetDisplayValue()
         {
-            return "Loop " + v_LoopParameter + " Times";
+            if (v_LoopStart != "0")
+            {
+                return "Loop From (" + v_LoopStart + "+1) to " + v_LoopParameter;
+
+            }
+            else
+            {
+                return "Loop " +  v_LoopParameter + " Times";
+            }
+         
         }
     }
 }
