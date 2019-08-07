933e5a01359bcb4e178887ad1514f49317a96ea0
VirtualFab <49951329+VirtualFab@users.noreply.github.com>
2019-08-07 09:15:27 +0200

    HTTPRequestCommand.cs: Added 'Y/N' Dropdown group control to select wether to execute the request as current user or not.

--- taskt/Core/Automation/Commands/HTTPRequestCommand.cs
+++ taskt/Core/Automation/Commands/HTTPRequestCommand.cs
@@ -24,6 +24,15 @@ namespace taskt.Core.Automation.Commands
         public string v_WebRequestURL { get; set; }
 
         [XmlAttribute]
+        [Attributes.PropertyAttributes.PropertyDescription("Execute Request as the currently logged on user?")]
+        [Attributes.PropertyAttributes.PropertyUISelectionOption("Yes")]
+        [Attributes.PropertyAttributes.PropertyUISelectionOption("No")]
+        [Attributes.PropertyAttributes.InputSpecification("Sets currently logged on user authentication information for the request.")]
+        [Attributes.PropertyAttributes.SampleUsage("Select 'Yes' or 'No'")]
+        [Attributes.PropertyAttributes.Remarks("")]
+        public string v_WebRequestCredentials { get; set; }
+
+        [XmlAttribute]
         [Attributes.PropertyAttributes.PropertyDescription("Apply Result To Variable")]
         [Attributes.PropertyAttributes.InputSpecification("Select or provide a variable from the variable list")]
         [Attributes.PropertyAttributes.SampleUsage("**vSomeVariable**")]
@@ -47,7 +56,10 @@ namespace taskt.Core.Automation.Commands
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(v_WebRequestURL.ConvertToUserVariable(sender));
             request.Method = "GET";
             request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
-            request.Credentials = CredentialCache.DefaultCredentials;
+            if (v_WebRequestCredentials == "Yes")
+            {
+                request.Credentials = CredentialCache.DefaultCredentials;
+            }
 
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
@@ -65,7 +77,7 @@ namespace taskt.Core.Automation.Commands
 
             //create inputs for request url
             RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_WebRequestURL", this, editor));
-
+            RenderedControls.AddRange(CommandControls.CreateDefaultDropdownGroupFor("v_WebRequestCredentials", this, editor));
 
             //create window name helper control
             RenderedControls.Add(CommandControls.CreateDefaultLabelFor("v_userVariableName", this));
