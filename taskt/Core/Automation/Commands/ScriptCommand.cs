bf6f116130be0d3edfbbdbe437eecb95dbcf3fdf
Francesca Faerman <fodfran@gmail.com>
2020-03-19 14:40:56 -0400

    removed powershell command for now

--- taskt/Core/Automation/Commands/ScriptCommand.cs
+++ taskt/Core/Automation/Commands/ScriptCommand.cs
@@ -46,7 +46,6 @@ namespace taskt.Core.Automation.Commands
     [XmlInclude(typeof(AddVariableCommand))]
     [XmlInclude(typeof(VariableCommand))]
     [XmlInclude(typeof(RunScriptCommand))]
-    [XmlInclude(typeof(RunPowerShellScriptCommand))]
     [XmlInclude(typeof(CloseWindowCommand))]
     [XmlInclude(typeof(SetWindowStateCommand))]
     [XmlInclude(typeof(BeginExcelDatasetLoopCommand))]
