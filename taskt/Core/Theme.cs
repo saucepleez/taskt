75287f6dafeacfd9521f0f37f86ff2c0376880f5
Miguel Rios <mrios0495@gmail.com>
2020-06-02 11:03:36 -0400

    Refactors Theme.cs to use auto properties. (#18)

--- taskt/Core/Theme.cs
+++ taskt/Core/Theme.cs
@@ -1,34 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace taskt.Core
 {
     public class Theme
     {
-
-        Color _BgGradientStartColor = Color.FromArgb(20, 136, 204);
-        public Color BgGradientStartColor
-        {
-            get { return _BgGradientStartColor; }
-            set { _BgGradientStartColor = value; }
-        }
-
-        Color _BgGradientEndColor = Color.FromArgb(43, 50, 178);
-        public Color BgGradientEndColor
-        {
-            get { return _BgGradientEndColor; }
-            set { _BgGradientEndColor = value; }
-        }
+        public Color BgGradientStartColor { get; set; } = Color.FromArgb(20, 136, 204);
+        public Color BgGradientEndColor { get; set; } = Color.FromArgb(43, 50, 178);
 
         public LinearGradientBrush CreateGradient(Rectangle rect)
         {
-            return new LinearGradientBrush(rect, _BgGradientStartColor, _BgGradientEndColor, 180);
+            return new LinearGradientBrush(rect, BgGradientStartColor, BgGradientEndColor, 180);
         }
-
     }
 }
