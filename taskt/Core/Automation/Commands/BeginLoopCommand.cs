0d247a3ce4affa7b3f5f847910b8bdc6d36f4ebe
rcktrncn <rcktrncn@hotmail.com>
2021-07-25 14:42:58 +0900

    Add BeginLoop boolean

--- taskt/Core/Automation/Commands/BeginLoopCommand.cs
+++ taskt/Core/Automation/Commands/BeginLoopCommand.cs
@@ -34,6 +34,7 @@ namespace taskt.Core.Automation.Commands
         [Attributes.PropertyAttributes.PropertyUISelectionOption("GUI Element Exists")]
         [Attributes.PropertyAttributes.PropertyUISelectionOption("Error Occured")]
         [Attributes.PropertyAttributes.PropertyUISelectionOption("Error Did Not Occur")]
+        [Attributes.PropertyAttributes.PropertyUISelectionOption("Boolean")]
         [Attributes.PropertyAttributes.InputSpecification("Select the necessary comparison type.")]
         [Attributes.PropertyAttributes.SampleUsage("Select **Value**, **Window Name Exists**, **Active Window Name Is**, **File Exists**, **Folder Exists**, **Web Element Exists**, **Error Occured**")]
         [Attributes.PropertyAttributes.Remarks("")]
@@ -135,19 +136,33 @@ namespace taskt.Core.Automation.Commands
 
                 value1 = value1.ConvertToUserVariable(sender);
                 value2 = value2.ConvertToUserVariable(sender);
-
-
+                bool tempBool;
+                bool isBoolCompare = (bool.TryParse(value1, out tempBool) && bool.TryParse(value2, out tempBool));
 
                 decimal cdecValue1, cdecValue2;
 
                 switch (operand)
                 {
                     case "is equal to":
-                        loopResult = (value1 == value2);
+                        if (isBoolCompare)
+                        {
+                            loopResult = (bool.Parse(value1) == bool.Parse(value2));
+                        }
+                        else
+                        {
+                            loopResult = (value1 == value2);
+                        }
                         break;
 
                     case "is not equal to":
-                        loopResult = (value1 != value2);
+                        if (isBoolCompare)
+                        {
+                            loopResult = (bool.Parse(value1) != bool.Parse(value2));
+                        }
+                        else
+                        {
+                            loopResult = (value1 != value2);
+                        }
                         break;
 
                     case "is greater than":
@@ -512,6 +527,30 @@ namespace taskt.Core.Automation.Commands
 
 
             }
+            else if (v_LoopActionType == "Boolean")
+            {
+                string value = ((from rw in v_LoopActionParameterTable.AsEnumerable()
+                                 where rw.Field<string>("Parameter Name") == "Variable Name"
+                                 select rw.Field<string>("Parameter Value")).FirstOrDefault().ConvertToUserVariable(sender));
+
+                string compare = ((from rw in v_LoopActionParameterTable.AsEnumerable()
+                                   where rw.Field<string>("Parameter Name") == "Value Is"
+                                   select rw.Field<string>("Parameter Value")).FirstOrDefault().ConvertToUserVariable(sender));
+
+                bool vValue = bool.Parse(value);
+                switch (compare.ToLower())
+                {
+                    case "true":
+                        loopResult = vValue;
+                        break;
+                    case "false":
+                        loopResult = !vValue;
+                        break;
+                    default:
+                        throw new Exception("Value Is " + compare + " is not support.");
+                        break;
+                }
+            }
             else
             {
                 throw new Exception("Loop type not recognized!");
@@ -813,6 +852,23 @@ namespace taskt.Core.Automation.Commands
 
                     break;
 
+                case "Boolean":
+                    loopActionParameterBox.Visible = true;
+                    if (sender != null)
+                    {
+                        actionParameters.Rows.Add("Variable Name", "");
+                        actionParameters.Rows.Add("Value Is", "True");
+                        loopActionParameterBox.DataSource = actionParameters;
+                    }
+                    //assign cell as a combobox
+                    var booleanParam = new DataGridViewComboBoxCell();
+                    booleanParam.Items.Add("True");
+                    booleanParam.Items.Add("False");
+                    loopActionParameterBox.Rows[1].Cells[1] = booleanParam;
+
+                    RecorderControl.Show();
+                    break;
+
                 default:
                     break;
             }
@@ -958,6 +1014,15 @@ namespace taskt.Core.Automation.Commands
 
                     return "Loop While GUI Element Exists [Find " + guiSearch + " Element In " + guiWindowName + "]";
 
+                case "Boolean":
+                    string booleanVariable = ((from rw in v_LoopActionParameterTable.AsEnumerable()
+                                               where rw.Field<string>("Parameter Name") == "Variable Name"
+                                               select rw.Field<string>("Parameter Value")).FirstOrDefault());
+
+                    string compareTo = ((from rw in v_LoopActionParameterTable.AsEnumerable()
+                                         where rw.Field<string>("Parameter Name") == "Value Is"
+                                         select rw.Field<string>("Parameter Value")).FirstOrDefault());
+                    return "If [Boolean] " + booleanVariable + " is " + compareTo;
 
                 default:
 
@@ -1058,6 +1123,10 @@ namespace taskt.Core.Automation.Commands
 
                     case "Error Did Not Occur":
                         ErrorValidate();
+                        break;
+
+                    case "Boolean":
+                        BooleanValidate();
                         break;
 
                     default:
@@ -1212,6 +1281,19 @@ namespace taskt.Core.Automation.Commands
                         this.IsValid = false;
                     }
                 }
+            }
+        }
+
+        private void BooleanValidate()
+        {
+            string variable = ((from rw in v_LoopActionParameterTable.AsEnumerable()
+                                where rw.Field<string>("Parameter Name") == "Variable Name"
+                                select rw.Field<string>("Parameter Value")).FirstOrDefault());
+
+            if (String.IsNullOrEmpty(variable))
+            {
+                this.validationResult += "Variable is empty.\n";
+                this.IsValid = false;
             }
         }
     }
