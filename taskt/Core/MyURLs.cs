a0abb57f56da5a4671a69f1af3f56cd19f666ff3
rcktrncn <rcktrncn@hotmail.com>
2021-08-26 20:32:28 +0900

    class url list

---
+++ taskt/Core/MyURLs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskt.Core
+{
+    public class MyURLs
+    {
+        public const string GitProjectURL = "https://github.com/saucepleez/taskt";
+        public const string GitReleaseURL = "https://github.com/saucepleez/taskt/releases";
+        public const string GitIssueURL = "https://github.com/saucepleez/taskt/issues/new";
+        public const string WikiURL = "https://github.com/saucepleez/taskt-wiki/blob/master/home.md";
+        public const string OfficialSiteURL = "http://www.taskt.net/";
+    }
+}
