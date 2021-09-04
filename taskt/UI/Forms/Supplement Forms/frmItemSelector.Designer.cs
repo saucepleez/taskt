259c4d64ca3e09f08315014ac35d63b64b9fbb3c
rcktrncn <rcktrncn@hotmail.com>
2021-09-04 21:42:14 +0900

    use tableLayoutPanel

--- taskt/UI/Forms/Supplement Forms/frmItemSelector.Designer.cs
+++ taskt/UI/Forms/Supplement Forms/frmItemSelector.Designer.cs
@@ -29,25 +29,37 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmItemSelector));
-            this.lstVariables = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.lblHeader = new System.Windows.Forms.Label();
+            this.lstVariables = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.uiBtnOk = new taskt.UI.CustomControls.UIPictureButton();
             this.uiBtnCancel = new taskt.UI.CustomControls.UIPictureButton();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.uiBtnOk)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.uiBtnCancel)).BeginInit();
             this.SuspendLayout();
             // 
-            // lstVariables
+            // tableLayoutPanel1
             // 
-            this.lstVariables.Font = new System.Drawing.Font("Segoe UI Semilight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lstVariables.FormattingEnabled = true;
-            this.lstVariables.ItemHeight = 21;
-            this.lstVariables.Location = new System.Drawing.Point(12, 38);
-            this.lstVariables.Name = "lstVariables";
-            this.lstVariables.Size = new System.Drawing.Size(328, 130);
-            this.lstVariables.TabIndex = 0;
-            this.lstVariables.SelectedIndexChanged += new System.EventHandler(this.lstVariables_SelectedIndexChanged);
-            this.lstVariables.DoubleClick += new System.EventHandler(this.lstVariables_DoubleClick);
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.lblHeader, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lstVariables, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Segoe UI", 11.5F);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(353, 318);
+            this.tableLayoutPanel1.TabIndex = 20;
             // 
             // lblHeader
             // 
@@ -55,11 +67,35 @@
             this.lblHeader.BackColor = System.Drawing.Color.Transparent;
             this.lblHeader.Font = new System.Drawing.Font("Segoe UI Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblHeader.ForeColor = System.Drawing.Color.White;
-            this.lblHeader.Location = new System.Drawing.Point(7, 4);
+            this.lblHeader.Location = new System.Drawing.Point(3, 0);
             this.lblHeader.Name = "lblHeader";
             this.lblHeader.Size = new System.Drawing.Size(301, 32);
-            this.lblHeader.TabIndex = 1;
+            this.lblHeader.TabIndex = 0;
             this.lblHeader.Text = "Insert a variable from the list";
+            // 
+            // lstVariables
+            // 
+            this.lstVariables.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstVariables.Font = new System.Drawing.Font("Segoe UI Semilight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstVariables.FormattingEnabled = true;
+            this.lstVariables.ItemHeight = 21;
+            this.lstVariables.Location = new System.Drawing.Point(3, 43);
+            this.lstVariables.Name = "lstVariables";
+            this.lstVariables.Size = new System.Drawing.Size(347, 217);
+            this.lstVariables.TabIndex = 1;
+            this.lstVariables.SelectedIndexChanged += new System.EventHandler(this.lstVariables_SelectedIndexChanged);
+            this.lstVariables.DoubleClick += new System.EventHandler(this.lstVariables_DoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.uiBtnOk);
+            this.panel1.Controls.Add(this.uiBtnCancel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 263);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(353, 55);
+            this.panel1.TabIndex = 2;
             // 
             // uiBtnOk
             // 
@@ -70,11 +106,11 @@
             this.uiBtnOk.Font = new System.Drawing.Font("Segoe UI", 8F);
             this.uiBtnOk.Image = global::taskt.Properties.Resources.various_ok_button;
             this.uiBtnOk.IsMouseOver = false;
-            this.uiBtnOk.Location = new System.Drawing.Point(12, 189);
+            this.uiBtnOk.Location = new System.Drawing.Point(6, 5);
             this.uiBtnOk.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.uiBtnOk.Name = "uiBtnOk";
             this.uiBtnOk.Size = new System.Drawing.Size(88, 45);
-            this.uiBtnOk.TabIndex = 18;
+            this.uiBtnOk.TabIndex = 25;
             this.uiBtnOk.TabStop = false;
             this.uiBtnOk.Text = "Ok";
             this.uiBtnOk.Click += new System.EventHandler(this.uiBtnOk_Click);
@@ -88,11 +124,11 @@
             this.uiBtnCancel.Font = new System.Drawing.Font("Segoe UI", 8F);
             this.uiBtnCancel.Image = global::taskt.Properties.Resources.various_cancel_button;
             this.uiBtnCancel.IsMouseOver = false;
-            this.uiBtnCancel.Location = new System.Drawing.Point(106, 189);
+            this.uiBtnCancel.Location = new System.Drawing.Point(100, 5);
             this.uiBtnCancel.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.uiBtnCancel.Name = "uiBtnCancel";
             this.uiBtnCancel.Size = new System.Drawing.Size(88, 45);
-            this.uiBtnCancel.TabIndex = 19;
+            this.uiBtnCancel.TabIndex = 26;
             this.uiBtnCancel.TabStop = false;
             this.uiBtnCancel.Text = "Cancel";
             this.uiBtnCancel.Click += new System.EventHandler(this.uiBtnCancel_Click);
@@ -101,26 +137,28 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(353, 242);
-            this.Controls.Add(this.uiBtnOk);
-            this.Controls.Add(this.uiBtnCancel);
-            this.Controls.Add(this.lblHeader);
-            this.Controls.Add(this.lstVariables);
+            this.ClientSize = new System.Drawing.Size(353, 318);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmItemSelector";
             this.Text = "Insert a Variable";
             this.Load += new System.EventHandler(this.frmVariableSelector_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.uiBtnOk)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.uiBtnCancel)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.ListBox lstVariables;
+        private System.Windows.Forms.Panel panel1;
         private CustomControls.UIPictureButton uiBtnOk;
         private CustomControls.UIPictureButton uiBtnCancel;
-        private System.Windows.Forms.ListBox lstVariables;
-        private System.Windows.Forms.Label lblHeader;
     }
 }
