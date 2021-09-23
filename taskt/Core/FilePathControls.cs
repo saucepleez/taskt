a152b1b57e7d005fd78e33d78df4c0e7e893d1fd
rcktrncn <rcktrncn@hotmail.com>
2021-09-23 22:17:15 +0900

    add FilePathControls

---
+++ taskt/Core/FilePathControls.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskt.Core
+{
+    public class FilePathControls
+    {
+        public static bool hasFolderPath(string path)
+        {
+            return (path != System.IO.Path.GetFileName(path));
+        }
+
+        public static bool hasExtension(string path)
+        {
+            string file = System.IO.Path.GetFileName(path);
+            return (file != System.IO.Path.GetExtension(path));
+        }
+
+        public static string formatFilePath(string path, Core.Automation.Engine.AutomationEngineInstance engine)
+        {
+            if (hasFolderPath(path))
+            {
+                return path;
+            }
+            else
+            {
+                //return engine.engineSettings.;
+                return System.IO.Path.GetDirectoryName(engine.FileName) + "\\" + path;
+            }
+        }
+    }
+}
