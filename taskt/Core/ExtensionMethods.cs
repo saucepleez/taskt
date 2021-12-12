3920192d75936da23767bea80a9d419d8e05561b
rcktrncn <rcktrncn@hotmail.com>
2021-12-12 15:19:05 +0900

    add data type get method

--- taskt/Core/ExtensionMethods.cs
+++ taskt/Core/ExtensionMethods.cs
@@ -948,6 +948,45 @@ namespace taskt.Core
             }
         }
 
+        public static List<string> GetListVariable(this string variableName, Core.Automation.Engine.AutomationEngineInstance engine)
+        {
+            Script.ScriptVariable v = variableName.GetRawVariable(engine);
+            if (v.VariableValue is List<string>)
+            {
+                return (List<string>)v.VariableValue;
+            }
+            else
+            {
+                throw new Exception("Variable " + variableName + " is not supported List");
+            }
+        }
+
+        public static Dictionary<string, string> GetDictionaryVariable(this string variableName, Core.Automation.Engine.AutomationEngineInstance engine)
+        {
+            Script.ScriptVariable v = variableName.GetRawVariable(engine);
+            if (v.VariableValue is Dictionary<string, string>)
+            {
+                return (Dictionary<string, string>)v.VariableValue;
+            }
+            else
+            {
+                throw new Exception("Variable " + variableName + " is not Dictionary");
+            }
+        }
+
+        public static DataTable GetDataTableVariable(this string variableName, Core.Automation.Engine.AutomationEngineInstance engine)
+        {
+            Script.ScriptVariable v = variableName.GetRawVariable(engine);
+            if (v.VariableValue is DataTable)
+            {
+                return (DataTable)v.VariableValue;
+            }
+            else
+            {
+                throw new Exception("Variable " + variableName + " is not DataTable");
+            }
+        }
+
         private static Script.ScriptVariable lookupVariable(string variableName, Core.Automation.Engine.AutomationEngineInstance engine)
         {
             //search for the variable
