627c797ba5504ea34f23434c9e73cf3e6f8b8439
rcktrncn <rcktrncn@hotmail.com>
2022-01-10 21:52:17 +0900

    add new command

---
+++ taskt/Core/Automation/Commands/Excel/ExcelGetColumnValuesAsDictionaryCommand.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using taskt.UI.CustomControls;
+using taskt.UI.Forms;
+
+namespace taskt.Core.Automation.Commands
+{
+    [Serializable]
+    [Attributes.ClassAttributes.Group("Excel Commands")]
+    [Attributes.ClassAttributes.SubGruop("Column")]
+    [Attributes.ClassAttributes.Description("This command get Column values as Dictionary.")]
+    [Attributes.ClassAttributes.UsesDescription("Use this command when you want to get Column values as Dictionary.")]
+    [Attributes.ClassAttributes.ImplementationDescription("")]
+    public class ExcelGetColumnValuesAsDictionaryCommand : ScriptCommand
+    {
+        [XmlAttribute]
+        [Attributes.PropertyAttributes.PropertyDescription("Please Enter the instance name")]
+        [Attributes.PropertyAttributes.InputSpecification("Enter the unique instance name that was specified in the **Create Excel** command")]
+        [Attributes.PropertyAttributes.SampleUsage("**myInstance** or **{{{vInstance}}}**")]
+        [Attributes.PropertyAttributes.Remarks("Failure to enter the correct instance name or failure to first call **Create Excel** command will cause an error")]
+        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [Attributes.PropertyAttributes.PropertyShowSampleUsageInDescription(true)]
+        [Attributes.PropertyAttributes.PropertyInstanceType(Attributes.PropertyAttributes.PropertyInstanceType.InstanceType.Excel)]
+        [Attributes.PropertyAttributes.PropertyRecommendedUIControl(Attributes.PropertyAttributes.PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        [Attributes.PropertyAttributes.PropertyValidationRule("Instance Name", Attributes.PropertyAttributes.PropertyValidationRule.ValidationRuleFlags.Empty)]
+        [Attributes.PropertyAttributes.PropertyFirstValue("%kwd_default_excel_instance%")]
+        public string v_InstanceName { get; set; }
+        [XmlAttribute]
+        [Attributes.PropertyAttributes.PropertyDescription("Please Specify Column Type")]
+        [Attributes.PropertyAttributes.InputSpecification("")]
+        [Attributes.PropertyAttributes.SampleUsage("**Range** or **RC**")]
+        [Attributes.PropertyAttributes.Remarks("")]
+        [Attributes.PropertyAttributes.PropertyIsOptional(true, "Range")]
+        [Attributes.PropertyAttributes.PropertyRecommendedUIControl(Attributes.PropertyAttributes.PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        [Attributes.PropertyAttributes.PropertyUISelectionOption("Range")]
+        [Attributes.PropertyAttributes.PropertyUISelectionOption("RC")]
+        [Attributes.PropertyAttributes.PropertyValueSensitive(false)]
+        public string v_ColumnType { get; set; }
+        [XmlAttribute]
+        [Attributes.PropertyAttributes.PropertyDescription("Please Enter the Column Location or Index")]
+        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [Attributes.PropertyAttributes.InputSpecification("")]
+        [Attributes.PropertyAttributes.SampleUsage("**A** or **1** or **{{{vColumn}}}**")]
+        [Attributes.PropertyAttributes.Remarks("")]
+        [Attributes.PropertyAttributes.PropertyTextBoxSetting(1, false)]
+        [Attributes.PropertyAttributes.PropertyShowSampleUsageInDescription(true)]
+        [Attributes.PropertyAttributes.PropertyValidationRule("Column", Attributes.PropertyAttributes.PropertyValidationRule.ValidationRuleFlags.Empty | Attributes.PropertyAttributes.PropertyValidationRule.ValidationRuleFlags.LessThanZero | Attributes.PropertyAttributes.PropertyValidationRule.ValidationRuleFlags.EqualsZero)]
+        public string v_ColumnIndex { get; set; }
+        [XmlAttribute]
+        [Attributes.PropertyAttributes.PropertyDescription("Please Enter the Start Row Index")]
+        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [Attributes.PropertyAttributes.InputSpecification("")]
+        [Attributes.PropertyAttributes.SampleUsage("**1** or **2** or **{{{vRow}}}**")]
+        [Attributes.PropertyAttributes.Remarks("")]
+        [Attributes.PropertyAttributes.PropertyTextBoxSetting(1, false)]
+        [Attributes.PropertyAttributes.PropertyShowSampleUsageInDescription(true)]
+        [Attributes.PropertyAttributes.PropertyValidationRule("Start Row", Attributes.PropertyAttributes.PropertyValidationRule.ValidationRuleFlags.Empty)]
+        public string v_RowStart { get; set; }
+        [XmlAttribute]
+        [Attributes.PropertyAttributes.PropertyDescription("Please Enter the End Row Index")]
+        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [Attributes.PropertyAttributes.InputSpecification("")]
+        [Attributes.PropertyAttributes.SampleUsage("**1** or **2** or **{{{vRow}}}**")]
+        [Attributes.PropertyAttributes.Remarks("")]
+        [Attributes.PropertyAttributes.PropertyTextBoxSetting(1, false)]
+        [Attributes.PropertyAttributes.PropertyShowSampleUsageInDescription(true)]
+        [Attributes.PropertyAttributes.PropertyValidationRule("End Row", Attributes.PropertyAttributes.PropertyValidationRule.ValidationRuleFlags.Empty)]
+        public string v_RowEnd { get; set; }
+        [XmlAttribute]
+        [Attributes.PropertyAttributes.PropertyDescription("Please specify the Dictionary Variable Name to store results")]
+        [Attributes.PropertyAttributes.InputSpecification("Select or provide a variable from the variable list")]
+        [Attributes.PropertyAttributes.SampleUsage("**vSomeVariable**")]
+        [Attributes.PropertyAttributes.Remarks("If you have enabled the setting **Create Missing Variables at Runtime** then you are not required to pre-define your variables, however, it is highly recommended.")]
+        [Attributes.PropertyAttributes.PropertyRecommendedUIControl(Attributes.PropertyAttributes.PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        [Attributes.PropertyAttributes.PropertyIsVariablesList(true)]
+        [Attributes.PropertyAttributes.PropertyParameterDirection(Attributes.PropertyAttributes.PropertyParameterDirection.ParameterDirection.Output)]
+        [Attributes.PropertyAttributes.PropertyInstanceType(Attributes.PropertyAttributes.PropertyInstanceType.InstanceType.Dictionary)]
+        [Attributes.PropertyAttributes.PropertyValidationRule("List", Attributes.PropertyAttributes.PropertyValidationRule.ValidationRuleFlags.Empty)]
+        public string v_userVariableName { get; set; }
+
+        [XmlAttribute]
+        [Attributes.PropertyAttributes.PropertyDescription("Please specify the Value type to get")]
+        [Attributes.PropertyAttributes.InputSpecification("")]
+        [Attributes.PropertyAttributes.SampleUsage("**Cell** or **Formula** or **Format** or **Color** or **Comment**")]
+        [Attributes.PropertyAttributes.Remarks("")]
+        [Attributes.PropertyAttributes.PropertyUISelectionOption("Cell")]
+        [Attributes.PropertyAttributes.PropertyUISelectionOption("Formula")]
+        [Attributes.PropertyAttributes.PropertyUISelectionOption("Format")]
+        [Attributes.PropertyAttributes.PropertyUISelectionOption("Font Color")]
+        [Attributes.PropertyAttributes.PropertyUISelectionOption("Back Color")]
+        [Attributes.PropertyAttributes.PropertyRecommendedUIControl(Attributes.PropertyAttributes.PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        [Attributes.PropertyAttributes.PropertyIsOptional(true, "Cell")]
+        [Attributes.PropertyAttributes.PropertyValueSensitive(false)]
+        public string v_ValueType { get; set; }
+
+        public ExcelGetColumnValuesAsDictionaryCommand()
+        {
+            this.CommandName = "ExcelGetColumnValuesAsDictionaryCommand";
+            this.SelectionName = "Get Column Values As Dictionary";
+            this.CommandEnabled = true;
+            this.CustomRendering = true;
+        }
+
+        public override void RunCommand(object sender)
+        {
+            var engine = (Core.Automation.Engine.AutomationEngineInstance)sender;
+
+            var excelInstance = ExcelControls.getExcelInstance(engine, v_InstanceName.ConvertToUserVariable(engine));
+            var excelSheet = (Microsoft.Office.Interop.Excel.Worksheet)excelInstance.ActiveSheet;
+
+            int columnIndex = 0;
+            switch (v_ColumnType.GetUISelectionValue("v_ColumnType", this, engine))
+            {
+                case "range":
+                    columnIndex = ExcelControls.getColumnIndex(excelSheet, v_ColumnIndex.ConvertToUserVariable(engine));
+                    break;
+                case "rc":
+                    columnIndex = int.Parse(v_ColumnIndex.ConvertToUserVariable(engine));
+                    break;
+            }
+
+            if (columnIndex < 1)
+            {
+                throw new Exception("Column index is less than 1");
+            }
+
+            int rowStart = int.Parse(v_RowStart.ConvertToUserVariable(engine));
+            int rowEnd = int.Parse(v_RowEnd.ConvertToUserVariable(engine));
+
+            if (rowStart > rowEnd)
+            {
+                int t = rowStart;
+                rowStart = rowEnd;
+                rowEnd = t;
+            }
+
+            Func<Microsoft.Office.Interop.Excel.Worksheet, int, int, string> getFunc = ExcelControls.getCellValueFunction(v_ValueType.GetUISelectionValue("v_ValueType", this, engine));
+
+            Dictionary<string, string> newDic = new Dictionary<string, string>();
+
+            for (int i = rowStart; i <= rowEnd; i++)
+            {
+                string address = ExcelControls.getAddress(excelSheet, i, columnIndex);
+                newDic.Add(address, getFunc(excelSheet, columnIndex, i));
+            }
+
+            newDic.StoreInUserVariable(engine, v_userVariableName);
+        }
+        public override List<Control> Render(frmCommandEditor editor)
+        {
+            base.Render(editor);
+
+            var ctls = CommandControls.MultiCreateInferenceDefaultControlGroupFor(this, editor);
+            RenderedControls.AddRange(ctls);
+
+            return RenderedControls;
+        }
+
+        public override string GetDisplayValue()
+        {
+            return base.GetDisplayValue() + " [Get " + v_ValueType + " Values From '" + v_RowStart + "' to '" + v_RowEnd + "' Column '" + v_ColumnIndex + "' as Dictionary '" + v_userVariableName + "', Instance Name: '" + v_InstanceName + "']";
+        }
+    }
+}
