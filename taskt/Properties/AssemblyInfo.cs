c9484e27911c33b96aec69a89af495bb9b7b8e5e
rcktrncn <rcktrncn@hotmail.com>
2022-04-10 16:28:44 +0900

    change copyright year

--- taskt/Properties/AssemblyInfo.cs
+++ taskt/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("taskt")]
-[assembly: AssemblyCopyright("Copyright ©2021")]
+[assembly: AssemblyCopyright("Copyright ©2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
