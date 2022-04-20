e6bbabe9f71995fa025ed88706d7197867f3e8d2
rcktrncn <rcktrncn@hotmail.com>
2022-04-20 21:40:01 +0900

    use WindowNameControls method

--- taskt/Core/Automation/Commands/Window/GetWindowPositionCommand.cs
+++ taskt/Core/Automation/Commands/Window/GetWindowPositionCommand.cs
@@ -5,7 +5,7 @@ using System.Xml.Serialization;
 using taskt.Core.Automation.User32;
 using taskt.UI.Forms;
 using taskt.UI.CustomControls;
-using System.Linq;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
 {
@@ -14,65 +14,66 @@ namespace taskt.Core.Automation.Commands
     [Attributes.ClassAttributes.SubGruop("Window State")]
     [Attributes.ClassAttributes.Description("This command returns window position.")]
     [Attributes.ClassAttributes.UsesDescription("Use this command when you want window position.")]
-    [Attributes.ClassAttributes.ImplementationDescription("This command implements 'FindWindowNative', 'SetForegroundWindow', 'ShowWindow' from user32.dll to achieve automation.")]
+    [Attributes.ClassAttributes.ImplementationDescription("")]
     public class GetWindowPositionCommand : ScriptCommand
     {
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Please enter or select the window position that you want to.")]
-        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [Attributes.PropertyAttributes.InputSpecification("Input or Type the name of the window name that you want to.")]
-        [Attributes.PropertyAttributes.SampleUsage("**Untitled - Notepad** or **%kwd_current_window%** or **{{{vWindow}}}**")]
-        [Attributes.PropertyAttributes.Remarks("")]
-        [Attributes.PropertyAttributes.PropertyRecommendedUIControl(Attributes.PropertyAttributes.PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
-        [Attributes.PropertyAttributes.PropertyIsWindowNamesList(true)]
-        [Attributes.PropertyAttributes.PropertyShowSampleUsageInDescription(true)]
+        [PropertyDescription("Please enter or select the window position that you want to.")]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [InputSpecification("Input or Type the name of the window name that you want to.")]
+        [SampleUsage("**Untitled - Notepad** or **%kwd_current_window%** or **{{{vWindow}}}**")]
+        [Remarks("")]
+        [PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        [PropertyIsWindowNamesList(true)]
+        [PropertyShowSampleUsageInDescription(true)]
+        [PropertyValidationRule("Window Name", PropertyValidationRule.ValidationRuleFlags.Empty)]
         public string v_WindowName { get; set; }
 
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Window title search method (Default is Contains)")]
-        [Attributes.PropertyAttributes.InputSpecification("")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("Contains")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("Starts with")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("Ends with")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("Exact match")]
-        [Attributes.PropertyAttributes.SampleUsage("**Contains** or **Starts with** or **Ends with** or **Exact match**")]
-        [Attributes.PropertyAttributes.Remarks("")]
-        [Attributes.PropertyAttributes.PropertyRecommendedUIControl(Attributes.PropertyAttributes.PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
-        [Attributes.PropertyAttributes.PropertyIsOptional(true)]
+        [PropertyDescription("Window title search method")]
+        [InputSpecification("")]
+        [PropertyUISelectionOption("Contains")]
+        [PropertyUISelectionOption("Starts with")]
+        [PropertyUISelectionOption("Ends with")]
+        [PropertyUISelectionOption("Exact match")]
+        [SampleUsage("**Contains** or **Starts with** or **Ends with** or **Exact match**")]
+        [Remarks("")]
+        [PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        [PropertyIsOptional(true, "Contains")]
         public string v_SearchMethod { get; set; }
 
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Specify the variable to recieve the window position X")]
-        [Attributes.PropertyAttributes.InputSpecification("")]
-        [Attributes.PropertyAttributes.SampleUsage("**vSomeVariable**")]
-        [Attributes.PropertyAttributes.Remarks("If you have enabled the setting **Create Missing Variables at Runtime** then you are not required to pre-define your variables, however, it is highly recommended.")]
-        [Attributes.PropertyAttributes.PropertyRecommendedUIControl(Attributes.PropertyAttributes.PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
-        [Attributes.PropertyAttributes.PropertyIsVariablesList(true)]
-        [Attributes.PropertyAttributes.PropertyIsOptional(true)]
+        [PropertyDescription("Specify the variable to recieve the window position X")]
+        [InputSpecification("")]
+        [SampleUsage("**vSomeVariable**")]
+        [Remarks("If you have enabled the setting **Create Missing Variables at Runtime** then you are not required to pre-define your variables, however, it is highly recommended.")]
+        [PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        [PropertyIsVariablesList(true)]
+        [PropertyIsOptional(true)]
         public string v_VariablePositionX { get; set; }
 
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Specify the variable to recieve the window position Y")]
-        [Attributes.PropertyAttributes.InputSpecification("")]
-        [Attributes.PropertyAttributes.SampleUsage("**vSomeVariable**")]
-        [Attributes.PropertyAttributes.Remarks("If you have enabled the setting **Create Missing Variables at Runtime** then you are not required to pre-define your variables, however, it is highly recommended.")]
-        [Attributes.PropertyAttributes.PropertyRecommendedUIControl(Attributes.PropertyAttributes.PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
-        [Attributes.PropertyAttributes.PropertyIsVariablesList(true)]
-        [Attributes.PropertyAttributes.PropertyIsOptional(true)]
+        [PropertyDescription("Specify the variable to recieve the window position Y")]
+        [InputSpecification("")]
+        [SampleUsage("**vSomeVariable**")]
+        [Remarks("If you have enabled the setting **Create Missing Variables at Runtime** then you are not required to pre-define your variables, however, it is highly recommended.")]
+        [PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        [PropertyIsVariablesList(true)]
+        [PropertyIsOptional(true)]
         public string v_VariablePositionY { get; set; }
 
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Base position (Default is Top Left)")]
-        [Attributes.PropertyAttributes.InputSpecification("")]
-        [Attributes.PropertyAttributes.SampleUsage("")]
-        [Attributes.PropertyAttributes.Remarks("")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("Top Left")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("Bottom Right")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("Top Right")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("Bottom Left")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("Center")]
-        [Attributes.PropertyAttributes.PropertyRecommendedUIControl(Attributes.PropertyAttributes.PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
-        [Attributes.PropertyAttributes.PropertyIsOptional(true)]
+        [PropertyDescription("Base position")]
+        [InputSpecification("")]
+        [SampleUsage("")]
+        [Remarks("")]
+        [PropertyUISelectionOption("Top Left")]
+        [PropertyUISelectionOption("Bottom Right")]
+        [PropertyUISelectionOption("Top Right")]
+        [PropertyUISelectionOption("Bottom Left")]
+        [PropertyUISelectionOption("Center")]
+        [PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        [PropertyIsOptional(true, "Top Left")]
         public string v_PositionBase { get; set; }
 
         [XmlIgnore]
@@ -88,108 +89,154 @@ namespace taskt.Core.Automation.Commands
         }
         public override void RunCommand(object sender)
         {
-            var engine = (Automation.Engine.AutomationEngineInstance)sender;
+            //var engine = (Engine.AutomationEngineInstance)sender;
+
+            //string windowName = v_WindowName.ConvertToUserVariable(sender);
+            //string searchMethod = v_SearchMethod.ConvertToUserVariable(sender);
+            //if (String.IsNullOrEmpty(searchMethod))
+            //{
+            //    searchMethod = "Contains";
+            //}
+
+            //bool targetIsCurrentWindow = windowName == engine.engineSettings.CurrentWindowKeyword;
+            //var targetWindows = User32Functions.FindTargetWindows(windowName, targetIsCurrentWindow, true);
+
+            //User32Functions.RECT pos = new User32Functions.RECT();
+            //bool isWindowsFound = false;
+
+            ////loop each window
+            //if (searchMethod == "Contains" || targetIsCurrentWindow)
+            //{
+            //    foreach (var targetedWindow in targetWindows)
+            //    {
+            //        pos = User32Functions.GetWindowPosition(targetedWindow);
+            //        isWindowsFound = true;
+            //    }
+            //}
+            //else
+            //{
+            //    Func<string, bool> searchFunc;
+            //    switch(searchMethod)
+            //    {
+            //        case "Starts with":
+            //            searchFunc = (s) => s.StartsWith(windowName);
+            //            break;
+
+            //        case "Ends with":
+            //            searchFunc = (s) => s.EndsWith(windowName);
+            //            break;
+
+            //        case "Exact match":
+            //            searchFunc = (s) => (s == windowName);
+            //            break;
+
+            //        default:
+            //            throw new Exception("Search method " + searchMethod + " is not support.");
+            //            break;
+            //    }
+
+            //    foreach (var targetedWindow in targetWindows)
+            //    {
+            //        if (searchFunc(User32Functions.GetWindowTitle(targetedWindow)))
+            //        {
+            //            pos = User32Functions.GetWindowPosition(targetedWindow);
+            //            isWindowsFound = true;
+            //        }
+            //    }
+            //}
+
+            //if (!isWindowsFound)
+            //{
+            //    throw new Exception("Window name " + windowName + " is not found.");
+            //    return;
+            //}
+
+            //var basePosition = v_PositionBase.ConvertToUserVariable(sender);
+            //if (String.IsNullOrEmpty(basePosition))
+            //{
+            //    basePosition = "Top Left";
+            //}
+            //int x, y;
+            //switch (basePosition)
+            //{
+            //    case "Top Left":
+            //        x = pos.left;
+            //        y = pos.top;
+            //        break;
+            //    case "Bottom Right":
+            //        x = pos.right;
+            //        y = pos.bottom;
+            //        break;
+            //    case "Top Right":
+            //        x = pos.right;
+            //        y = pos.top;
+            //        break;
+            //    case "Bottom Left":
+            //        x = pos.left;
+            //        y = pos.bottom;
+            //        break;
+            //    case "Center":
+            //        x = (pos.right + pos.left) / 2;
+            //        y = (pos.top + pos.bottom) / 2;
+            //        break;
+            //    default:
+            //        throw new Exception("Base Position " + basePosition + " is not supported.");
+            //        break;
+            //}
+
+            //if (!String.IsNullOrEmpty(v_VariablePositionX))
+            //{
+            //    x.ToString().StoreInUserVariable(sender, v_VariablePositionX);
+            //}
+            //if (!String.IsNullOrEmpty(v_VariablePositionY))
+            //{
+            //    y.ToString().StoreInUserVariable(sender, v_VariablePositionY);
+            //}
+
+            var engine = (Engine.AutomationEngineInstance)sender;
 
             string windowName = v_WindowName.ConvertToUserVariable(sender);
-            string searchMethod = v_SearchMethod.ConvertToUserVariable(sender);
-            if (String.IsNullOrEmpty(searchMethod))
+            string searchMethod = v_SearchMethod.GetUISelectionValue("v_SearchMethod", this, engine);
+
+            if (windowName == engine.engineSettings.CurrentWindowKeyword)
             {
-                searchMethod = "Contains";
+                windowName = WindowNameControls.GetCurrentWindowName();
             }
+            IntPtr hWnd = WindowNameControls.FindWindow(windowName, searchMethod, engine);
 
-            bool targetIsCurrentWindow = windowName == engine.engineSettings.CurrentWindowKeyword;
-            var targetWindows = User32Functions.FindTargetWindows(windowName, targetIsCurrentWindow, true);
+            User32Functions.RECT pos = User32Functions.GetWindowPosition(hWnd);
 
-            User32Functions.RECT pos = new User32Functions.RECT();
-            bool isWindowsFound = false;
-
-            //loop each window
-            if (searchMethod == "Contains" || targetIsCurrentWindow)
+            int x = 0, y = 0;
+            switch(v_PositionBase.GetUISelectionValue("v_PositionBase", this, engine))
             {
-                foreach (var targetedWindow in targetWindows)
-                {
-                    pos = User32Functions.GetWindowPosition(targetedWindow);
-                    isWindowsFound = true;
-                }
-            }
-            else
-            {
-                Func<string, bool> searchFunc;
-                switch(searchMethod)
-                {
-                    case "Starts with":
-                        searchFunc = (s) => s.StartsWith(windowName);
-                        break;
-
-                    case "Ends with":
-                        searchFunc = (s) => s.EndsWith(windowName);
-                        break;
-
-                    case "Exact match":
-                        searchFunc = (s) => (s == windowName);
-                        break;
-
-                    default:
-                        throw new Exception("Search method " + searchMethod + " is not support.");
-                        break;
-                }
-
-                foreach (var targetedWindow in targetWindows)
-                {
-                    if (searchFunc(User32Functions.GetWindowTitle(targetedWindow)))
-                    {
-                        pos = User32Functions.GetWindowPosition(targetedWindow);
-                        isWindowsFound = true;
-                    }
-                }
-            }
-
-            if (!isWindowsFound)
-            {
-                throw new Exception("Window name " + windowName + " is not found.");
-                return;
-            }
-
-            var basePosition = v_PositionBase.ConvertToUserVariable(sender);
-            if (String.IsNullOrEmpty(basePosition))
-            {
-                basePosition = "Top Left";
-            }
-            int x, y;
-            switch (basePosition)
-            {
-                case "Top Left":
+                case "top left":
                     x = pos.left;
                     y = pos.top;
                     break;
-                case "Bottom Right":
+                case "bottom right":
                     x = pos.right;
                     y = pos.bottom;
                     break;
-                case "Top Right":
+                case "top right":
                     x = pos.right;
                     y = pos.top;
                     break;
-                case "Bottom Left":
+                case "bottom left":
                     x = pos.left;
                     y = pos.bottom;
                     break;
-                case "Center":
+                case "center":
                     x = (pos.right + pos.left) / 2;
                     y = (pos.top + pos.bottom) / 2;
                     break;
-                default:
-                    throw new Exception("Base Position " + basePosition + " is not supported.");
-                    break;
             }
-            
             if (!String.IsNullOrEmpty(v_VariablePositionX))
             {
-                x.ToString().StoreInUserVariable(sender, v_VariablePositionX);
+                x.ToString().StoreInUserVariable(engine, v_VariablePositionX);
             }
             if (!String.IsNullOrEmpty(v_VariablePositionY))
             {
-                y.ToString().StoreInUserVariable(sender, v_VariablePositionY);
+                y.ToString().StoreInUserVariable(engine, v_VariablePositionY);
             }
         }
 
@@ -214,20 +261,20 @@ namespace taskt.Core.Automation.Commands
             return base.GetDisplayValue() + " [Target Window: " + v_WindowName + "', Result(X) In: '" + v_VariablePositionX + "', Result(Y) In: '" + v_VariablePositionY + "']";
         }
 
-        public override bool IsValidate(frmCommandEditor editor)
-        {
-            base.IsValidate(editor);
+        //public override bool IsValidate(frmCommandEditor editor)
+        //{
+        //    base.IsValidate(editor);
 
-            if (String.IsNullOrEmpty(this.v_WindowName))
-            {
-                this.validationResult += "Window is empty.\n";
-                this.IsValid = false;
-            }
+        //    if (String.IsNullOrEmpty(this.v_WindowName))
+        //    {
+        //        this.validationResult += "Window is empty.\n";
+        //        this.IsValid = false;
+        //    }
 
-            return this.IsValid;
-        }
+        //    return this.IsValid;
+        //}
 
-        public override void convertToIntermediate(EngineSettings settings, List<Core.Script.ScriptVariable> variables)
+        public override void convertToIntermediate(EngineSettings settings, List<Script.ScriptVariable> variables)
         {
             var cnv = new Dictionary<string, string>();
             cnv.Add("v_WindowName", "convertToIntermediateWindowName");
