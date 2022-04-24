7e253a3ce545cecfe4290b68056b2c3e4e1d5e3c
rcktrncn <rcktrncn@hotmail.com>
2022-04-24 14:17:50 +0900

    comment out unused commands

--- taskt/Core/Automation/Commands/BeginExcelDatasetLoopCommand.cs
+++ taskt/Core/Automation/Commands/BeginExcelDatasetLoopCommand.cs
@@ -1,89 +1,89 @@
-﻿using System;
-using System.Linq;
-using System.Xml.Serialization;
-using System.Data;
-using System.Windows.Forms;
-using System.Collections.Generic;
-using taskt.UI.Forms;
-using taskt.UI.CustomControls;
+﻿//using System;
+//using System.Linq;
+//using System.Xml.Serialization;
+//using System.Data;
+//using System.Windows.Forms;
+//using System.Collections.Generic;
+//using taskt.UI.Forms;
+//using taskt.UI.CustomControls;
 
-namespace taskt.Core.Automation.Commands
-{
-    [Serializable]
-    [Attributes.ClassAttributes.Group("Loop Commands")]
-    [Attributes.ClassAttributes.Description("This command allows you to loop through an Excel Dataset")]
-    [Attributes.ClassAttributes.UsesDescription("Use this command when you want to iterate over a series of Excel cells.")]
-    [Attributes.ClassAttributes.ImplementationDescription("This command attempts to loop through a known Excel DataSet")]
-    public class BeginExcelDatasetLoopCommand : ScriptCommand
-    {
-        [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Please indicate the Excel DataSet Name")]
-        [Attributes.PropertyAttributes.InputSpecification("Enter a unique dataset name that will be used later to traverse over the data")]
-        [Attributes.PropertyAttributes.SampleUsage("**myData**")]
-        [Attributes.PropertyAttributes.Remarks("")]
-        public string v_DataSetName { get; set; }
+//namespace taskt.Core.Automation.Commands
+//{
+//    [Serializable]
+//    [Attributes.ClassAttributes.Group("Loop Commands")]
+//    [Attributes.ClassAttributes.Description("This command allows you to loop through an Excel Dataset")]
+//    [Attributes.ClassAttributes.UsesDescription("Use this command when you want to iterate over a series of Excel cells.")]
+//    [Attributes.ClassAttributes.ImplementationDescription("This command attempts to loop through a known Excel DataSet")]
+//    public class BeginExcelDatasetLoopCommand : ScriptCommand
+//    {
+//        [XmlAttribute]
+//        [Attributes.PropertyAttributes.PropertyDescription("Please indicate the Excel DataSet Name")]
+//        [Attributes.PropertyAttributes.InputSpecification("Enter a unique dataset name that will be used later to traverse over the data")]
+//        [Attributes.PropertyAttributes.SampleUsage("**myData**")]
+//        [Attributes.PropertyAttributes.Remarks("")]
+//        public string v_DataSetName { get; set; }
 
-        public BeginExcelDatasetLoopCommand()
-        {
-            this.CommandName = "BeginExcelDataSetLoopCommand";
-            this.SelectionName = "Loop Excel Dataset";
-            //this command is no longer required as list loop can successfully loop.
-            this.CommandEnabled = false;
-            this.CustomRendering = true;
-        }
+//        public BeginExcelDatasetLoopCommand()
+//        {
+//            this.CommandName = "BeginExcelDataSetLoopCommand";
+//            this.SelectionName = "Loop Excel Dataset";
+//            //this command is no longer required as list loop can successfully loop.
+//            this.CommandEnabled = false;
+//            this.CustomRendering = true;
+//        }
 
-        public override void RunCommand(object sender, Core.Script.ScriptAction parentCommand)
-        {
+//        public override void RunCommand(object sender, Core.Script.ScriptAction parentCommand)
+//        {
 
-            Core.Automation.Commands.BeginExcelDatasetLoopCommand loopCommand = (Core.Automation.Commands.BeginExcelDatasetLoopCommand)parentCommand.ScriptCommand;
-            var engine = (Core.Automation.Engine.AutomationEngineInstance)sender;
+//            Core.Automation.Commands.BeginExcelDatasetLoopCommand loopCommand = (Core.Automation.Commands.BeginExcelDatasetLoopCommand)parentCommand.ScriptCommand;
+//            var engine = (Core.Automation.Engine.AutomationEngineInstance)sender;
 
-            var dataSetVariable = engine.VariableList.Where(f => f.VariableName == v_DataSetName).FirstOrDefault();
+//            var dataSetVariable = engine.VariableList.Where(f => f.VariableName == v_DataSetName).FirstOrDefault();
 
-            if (dataSetVariable == null)
-                throw new Exception("DataSet Name Not Found - " + v_DataSetName);
+//            if (dataSetVariable == null)
+//                throw new Exception("DataSet Name Not Found - " + v_DataSetName);
 
-            if (!engine.VariableList.Any(f => f.VariableName == "Loop.CurrentIndex"))
-            {
-                engine.VariableList.Add(new Script.ScriptVariable() { VariableName = "Loop.CurrentIndex", VariableValue = "0" });
-            }
-
-
-            DataTable excelTable = (DataTable)dataSetVariable.VariableValue;
+//            if (!engine.VariableList.Any(f => f.VariableName == "Loop.CurrentIndex"))
+//            {
+//                engine.VariableList.Add(new Script.ScriptVariable() { VariableName = "Loop.CurrentIndex", VariableValue = "0" });
+//            }
 
 
-            var loopTimes = excelTable.Rows.Count;
+//            DataTable excelTable = (DataTable)dataSetVariable.VariableValue;
 
-            for (int i = 0; i < excelTable.Rows.Count; i++)
-            {
-                dataSetVariable.CurrentPosition = i;
 
-                (i + 1).ToString().StoreInUserVariable(engine, "Loop.CurrentIndex");
+//            var loopTimes = excelTable.Rows.Count;
 
-                foreach (var cmd in parentCommand.AdditionalScriptCommands)
-                {
-                    //bgw.ReportProgress(0, new object[] { loopCommand.LineNumber, "Starting Loop Number " + (i + 1) + "/" + loopTimes + " From Line " + loopCommand.LineNumber });
+//            for (int i = 0; i < excelTable.Rows.Count; i++)
+//            {
+//                dataSetVariable.CurrentPosition = i;
 
-                    if (engine.IsCancellationPending)
-                        return;
-                    engine.ExecuteCommand(cmd);
-                    // bgw.ReportProgress(0, new object[] { loopCommand.LineNumber, "Finished Loop From Line " + loopCommand.LineNumber });
-                }
-            }
+//                (i + 1).ToString().StoreInUserVariable(engine, "Loop.CurrentIndex");
 
-        }
-        public override List<Control> Render(frmCommandEditor editor)
-        {
-            base.Render(editor);
+//                foreach (var cmd in parentCommand.AdditionalScriptCommands)
+//                {
+//                    //bgw.ReportProgress(0, new object[] { loopCommand.LineNumber, "Starting Loop Number " + (i + 1) + "/" + loopTimes + " From Line " + loopCommand.LineNumber });
 
-            RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_DataSetName", this, editor));
+//                    if (engine.IsCancellationPending)
+//                        return;
+//                    engine.ExecuteCommand(cmd);
+//                    // bgw.ReportProgress(0, new object[] { loopCommand.LineNumber, "Finished Loop From Line " + loopCommand.LineNumber });
+//                }
+//            }
 
-            return RenderedControls;
-        }
+//        }
+//        public override List<Control> Render(frmCommandEditor editor)
+//        {
+//            base.Render(editor);
 
-        public override string GetDisplayValue()
-        {
-            return base.GetDisplayValue();
-        }
-    }
-}
+//            RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_DataSetName", this, editor));
+
+//            return RenderedControls;
+//        }
+
+//        public override string GetDisplayValue()
+//        {
+//            return base.GetDisplayValue();
+//        }
+//    }
+//}
