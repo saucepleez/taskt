9a6acc2ae224688af8bbfd0834305d4c3fa31c36
rcktrncn <rcktrncn@hotmail.com>
2022-05-01 19:46:36 +0900

    Add InspectToolParser Form

---
+++ taskt/UI/Forms/Supplement Forms/frmInspectParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taskt.UI.Forms.Supplement_Forms
+{
+    public partial class frmInspectParser : ThemedForm
+    {
+        public frmInspectParser()
+        {
+            InitializeComponent();
+        }
+
+        private void uiBtnAdd_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void uiBtnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        public string inspectResult
+        {
+            get
+            {
+                return this.txtInspectResult.Text.Trim();
+            }
+        }
+    }
+}
