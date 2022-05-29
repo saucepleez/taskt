c67ba28ec3871b8e1497cca01fc23d91b695532f
rcktrncn <rcktrncn@hotmail.com>
2022-05-29 22:08:38 +0900

    move MathCalculation and fix Attributes

--- taskt/Core/Automation/Commands/Numerical/MathCalculationCommand.cs
+++ taskt/Core/Automation/Commands/Numerical/MathCalculationCommand.cs
@@ -6,50 +6,58 @@ using System.Windows.Forms;
 using taskt.UI.Forms;
 using taskt.UI.CustomControls;
 using System.Data;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
 {
     [Serializable]
-    [Attributes.ClassAttributes.Group("Data Commands")]
+    [Attributes.ClassAttributes.Group("Numerical Commands")]
     [Attributes.ClassAttributes.Description("This command allows you to perform a math calculation and apply it to a variable.")]
     [Attributes.ClassAttributes.UsesDescription("Use this command when you want to perform a math calculation.")]
     [Attributes.ClassAttributes.ImplementationDescription("")]
     public class MathCalculationCommand : ScriptCommand
     {
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Please supply the input to be computed")]
-        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [Attributes.PropertyAttributes.InputSpecification("Specify either text or a variable that contains valid math.")]
-        [Attributes.PropertyAttributes.SampleUsage("**2+1** or **{{{vNum}}}+1**")]
-        [Attributes.PropertyAttributes.Remarks("You can use known numbers or variables.")]
-        [Attributes.PropertyAttributes.PropertyShowSampleUsageInDescription(true)]
+        [PropertyDescription("Please supply the input to be computed")]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [InputSpecification("Specify either text or a variable that contains valid math.")]
+        [SampleUsage("**2+1** or **{{{vNum}}}+1**")]
+        [Remarks("You can use known numbers or variables.")]
+        [PropertyShowSampleUsageInDescription(true)]
+        [PropertyTextBoxSetting(2, false)]
+        [PropertyValidationRule("Compute", PropertyValidationRule.ValidationRuleFlags.Empty)]
         public string v_InputValue { get; set; }
 
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Indicate Thousand Seperator")]
-        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [Attributes.PropertyAttributes.InputSpecification("Enter the seperator used to identify decimal places")]
-        [Attributes.PropertyAttributes.SampleUsage("")]
-        [Attributes.PropertyAttributes.Remarks("Typically a comma or a decimal point (period)")]
-        [Attributes.PropertyAttributes.PropertyIsOptional(true)]
+        [PropertyDescription("Indicate Thousand Seperator")]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [InputSpecification("Enter the seperator used to identify decimal places")]
+        [SampleUsage("")]
+        [Remarks("Typically a comma or a decimal point (period)")]
+        [PropertyIsOptional(true)]
+        [PropertyFirstValue("")]
+        [PropertyTextBoxSetting(1, false)]
         public string v_ThousandSeperator { get; set; }
 
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Optional - Indicate Decimal Seperator")]
-        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [Attributes.PropertyAttributes.InputSpecification("Enter the seperator used to identify decimal places")]
-        [Attributes.PropertyAttributes.SampleUsage("")]
-        [Attributes.PropertyAttributes.Remarks("Typically a comma or a decimal point (period)")]
-        [Attributes.PropertyAttributes.PropertyIsOptional(true)]
+        [PropertyDescription("Indicate Decimal Seperator")]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [InputSpecification("Enter the seperator used to identify decimal places")]
+        [SampleUsage("")]
+        [Remarks("Typically a comma or a decimal point (period)")]
+        [PropertyIsOptional(true, ".")]
+        [PropertyFirstValue(".")]
+        [PropertyTextBoxSetting(1, false)]
         public string v_DecimalSeperator { get; set; }
 
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Please select the variable to receive the math calculation")]
-        [Attributes.PropertyAttributes.InputSpecification("Select or provide a variable from the variable list")]
-        [Attributes.PropertyAttributes.SampleUsage("**vSomeVariable**")]
-        [Attributes.PropertyAttributes.Remarks("If you have enabled the setting **Create Missing Variables at Runtime** then you are not required to pre-define your variables, however, it is highly recommended.")]
-        [Attributes.PropertyAttributes.PropertyRecommendedUIControl(Attributes.PropertyAttributes.PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
-        [Attributes.PropertyAttributes.PropertyIsVariablesList(true)]
+        [PropertyDescription("Please select the variable to receive the math calculation")]
+        [InputSpecification("Select or provide a variable from the variable list")]
+        [SampleUsage("**vSomeVariable**")]
+        [Remarks("If you have enabled the setting **Create Missing Variables at Runtime** then you are not required to pre-define your variables, however, it is highly recommended.")]
+        [PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        [PropertyIsVariablesList(true)]
+        [PropertyValidationRule("Variable", PropertyValidationRule.ValidationRuleFlags.Empty)]
         public string v_applyToVariableName { get; set; }
 
         public MathCalculationCommand()
@@ -59,9 +67,9 @@ namespace taskt.Core.Automation.Commands
             this.CommandEnabled = true;
             this.CustomRendering = true;
 
-            this.v_InputValue = "(2 + 5) * 3";
-            this.v_DecimalSeperator = ".";
-            this.v_ThousandSeperator = ",";
+            //this.v_InputValue = "(2 + 5) * 3";
+            //this.v_DecimalSeperator = ".";
+            //this.v_ThousandSeperator = ",";
         }
 
         public override void RunCommand(object sender)
@@ -136,22 +144,22 @@ namespace taskt.Core.Automation.Commands
             return base.GetDisplayValue() + $" [Apply Calculation Result to '{v_applyToVariableName}']";
         }
 
-        public override bool IsValidate(frmCommandEditor editor)
-        {
-            base.IsValidate(editor);
+        //public override bool IsValidate(frmCommandEditor editor)
+        //{
+        //    base.IsValidate(editor);
 
-            if (String.IsNullOrEmpty(this.v_InputValue))
-            {
-                this.validationResult += "Compute is empty.\n";
-                this.IsValid = false;
-            }
-            if (String.IsNullOrEmpty(this.v_applyToVariableName))
-            {
-                this.validationResult += "Variable is empty.\n";
-                this.IsValid = false;
-            }
+        //    if (String.IsNullOrEmpty(this.v_InputValue))
+        //    {
+        //        this.validationResult += "Compute is empty.\n";
+        //        this.IsValid = false;
+        //    }
+        //    if (String.IsNullOrEmpty(this.v_applyToVariableName))
+        //    {
+        //        this.validationResult += "Variable is empty.\n";
+        //        this.IsValid = false;
+        //    }
 
-            return this.IsValid;
-        }
+        //    return this.IsValid;
+        //}
     }
 }
