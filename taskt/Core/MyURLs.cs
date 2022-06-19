9e87f486bc11db58124c97999c2b2ebdd8581a52
KondouMasaki <rcktrncn@hotmail.com>
2022-06-19 17:12:02 +0900

    fix uob

--- taskt/Core/MyURLs.cs
+++ taskt/Core/MyURLs.cs
@@ -8,13 +8,13 @@ namespace taskt.Core
 {
     public class MyURLs
     {
-        public const string GitProjectURL = "https://github.com/saucepleez/taskt";
-        public const string GitReleaseURL = "https://github.com/saucepleez/taskt/releases";
-        public const string GitIssueURL = "https://github.com/saucepleez/taskt/issues/new";
-        public const string WikiURL = "https://github.com/saucepleez/taskt-wiki/blob/master/home.md";
+        public const string GitProjectURL = "https://github.com/rcktrncn/taskt";
+        public const string GitReleaseURL = "https://github.com/rcktrncn/taskt/releases";
+        public const string GitIssueURL = "https://github.com/rcktrncn/taskt/issues/new";
+        public const string WikiURL = "https://github.com/rcktrncn/taskt-wiki/blob/master/home.md";
         public const string OfficialSiteURL = "http://www.taskt.net/";
         public const string GitterURL = "https://gitter.im/taskt-rpa/Lobby";
-        public const string WikiBaseURL = "https://github.com/saucepleez/taskt-wiki/blob/master/";
+        public const string WikiBaseURL = "https://github.com/rcktrncn/taskt-wiki/blob/master/";
 
         public static string GetWikiURL(string commandName, string groupName)
         {
--- taskt/Properties/AssemblyInfo.cs
+++ taskt/Properties/AssemblyInfo.cs
@@ -18,11 +18,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("taskt")]
+[assembly: AssemblyTitle("taskt uob")]
 [assembly: AssemblyDescription("Build and Execute Automation Scripts")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("taskt")]
+[assembly: AssemblyProduct("taskt uob")]
 [assembly: AssemblyCopyright("Copyright ©2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -45,6 +45,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.0.*")]
-[assembly: AssemblyFileVersion("3.6.0.0")]
+[assembly: AssemblyFileVersion("3.5.0.88")]
 //[assembly: AssemblyVersion("0.0.0.2")]
 //[assembly: AssemblyFileVersion("0.0.0.2")]
