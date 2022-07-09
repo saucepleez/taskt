fdb19e7045c1d656040852dfa4233499f13b60c7
KondouMasaki <rcktrncn@hotmail.com>
2022-07-09 14:41:46 +0900

    add textbox property
    don't allow enter

--- taskt/Core/Automation/Commands/UIAutomation/UIAutomationGetElementFromElementByXPathCommand.cs
+++ taskt/Core/Automation/Commands/UIAutomation/UIAutomationGetElementFromElementByXPathCommand.cs
@@ -41,6 +41,7 @@ namespace taskt.Core.Automation.Commands
         [SampleUsage("**//Button[@Name=\"OK\"]** or **{{{vXPath}}}**")]
         [Remarks("XPath does not support to use parent, following-sibling, and preceding-sibling for root element.")]
         [PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.TextBox)]
+        [PropertyTextBoxSetting(1, false)]
         [PropertyValidationRule("XPath", PropertyValidationRule.ValidationRuleFlags.Empty)]
         public string v_SearchXPath { get; set; }
 
