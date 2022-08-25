f71465c0aefecd8aa5911c91c49e3a91cbcea4c6
rcktrncn <rcktrncn@hotmail.com>
2022-08-25 21:46:48 +0900

    add frmFormatChecker(beta)

---
+++ taskt/UI/Forms/Supplement Forms/frmFormatChecker.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace taskt.UI.Forms.Supplement_Forms
+{
+    partial class frmFormatChecker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.panelFooter = new System.Windows.Forms.Panel();
+            this.uiBtnAdd = new taskt.UI.CustomControls.UIPictureButton();
+            this.uiBtnCancel = new taskt.UI.CustomControls.UIPictureButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.txtFormat = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panelHeader.SuspendLayout();
+            this.panelFooter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.uiBtnAdd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uiBtnCancel)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panelHeader, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panelFooter, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 57F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(495, 366);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.Controls.Add(this.label1);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Margin = new System.Windows.Forms.Padding(0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(495, 40);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // panelFooter
+            // 
+            this.panelFooter.Controls.Add(this.uiBtnAdd);
+            this.panelFooter.Controls.Add(this.uiBtnCancel);
+            this.panelFooter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelFooter.Location = new System.Drawing.Point(0, 309);
+            this.panelFooter.Margin = new System.Windows.Forms.Padding(0);
+            this.panelFooter.Name = "panelFooter";
+            this.panelFooter.Size = new System.Drawing.Size(495, 57);
+            this.panelFooter.TabIndex = 1;
+            // 
+            // uiBtnAdd
+            // 
+            this.uiBtnAdd.BackColor = System.Drawing.Color.Transparent;
+            this.uiBtnAdd.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.uiBtnAdd.DisplayText = "Ok";
+            this.uiBtnAdd.DisplayTextBrush = System.Drawing.Color.White;
+            this.uiBtnAdd.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.uiBtnAdd.Image = global::taskt.Properties.Resources.various_ok_button;
+            this.uiBtnAdd.IsMouseOver = false;
+            this.uiBtnAdd.Location = new System.Drawing.Point(6, 5);
+            this.uiBtnAdd.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.uiBtnAdd.Name = "uiBtnAdd";
+            this.uiBtnAdd.Size = new System.Drawing.Size(88, 49);
+            this.uiBtnAdd.TabIndex = 20;
+            this.uiBtnAdd.TabStop = false;
+            this.uiBtnAdd.Text = "Ok";
+            // 
+            // uiBtnCancel
+            // 
+            this.uiBtnCancel.BackColor = System.Drawing.Color.Transparent;
+            this.uiBtnCancel.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.uiBtnCancel.DisplayText = "Cancel";
+            this.uiBtnCancel.DisplayTextBrush = System.Drawing.Color.White;
+            this.uiBtnCancel.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.uiBtnCancel.Image = global::taskt.Properties.Resources.various_cancel_button;
+            this.uiBtnCancel.IsMouseOver = false;
+            this.uiBtnCancel.Location = new System.Drawing.Point(106, 5);
+            this.uiBtnCancel.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.uiBtnCancel.Name = "uiBtnCancel";
+            this.uiBtnCancel.Size = new System.Drawing.Size(88, 49);
+            this.uiBtnCancel.TabIndex = 21;
+            this.uiBtnCancel.TabStop = false;
+            this.uiBtnCancel.Text = "Cancel";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Emoji", 16F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Format Checker";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(59)))), ((int)(((byte)(59)))));
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.cmbType);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtResult);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtFormat);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtValue);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 40);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(495, 269);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "&Value";
+            // 
+            // txtValue
+            // 
+            this.txtValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtValue.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtValue.Location = new System.Drawing.Point(16, 36);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(467, 29);
+            this.txtValue.TabIndex = 1;
+            // 
+            // txtFormat
+            // 
+            this.txtFormat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFormat.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtFormat.Location = new System.Drawing.Point(16, 167);
+            this.txtFormat.Name = "txtFormat";
+            this.txtFormat.Size = new System.Drawing.Size(467, 29);
+            this.txtFormat.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "&Format";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtResult.Location = new System.Drawing.Point(16, 231);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(467, 29);
+            this.txtResult.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "&Result";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(12, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "&Type";
+            // 
+            // cmbType
+            // 
+            this.cmbType.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbType.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Items.AddRange(new object[] {
+            "Number",
+            "DateTime",
+            "File Folder"});
+            this.cmbType.Location = new System.Drawing.Point(16, 104);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(338, 28);
+            this.cmbType.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.button1.Location = new System.Drawing.Point(95, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "File";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.button2.Location = new System.Drawing.Point(164, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.button3.Location = new System.Drawing.Point(234, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(51, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Now";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // frmFormatChecker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 366);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmFormatChecker";
+            this.Text = "Format Checker";
+            this.Load += new System.EventHandler(this.frmFormatChecker_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.panelFooter.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.uiBtnAdd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uiBtnCancel)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Panel panelFooter;
+        private CustomControls.UIPictureButton uiBtnAdd;
+        private CustomControls.UIPictureButton uiBtnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFormat;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ taskt/UI/Forms/Supplement Forms/frmFormatChecker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taskt.UI.Forms.Supplement_Forms
+{
+    public partial class frmFormatChecker : ThemedForm
+    {
+        public frmFormatChecker()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFormatChecker_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
