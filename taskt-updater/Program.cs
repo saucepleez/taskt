ab7855c191d5a302f931ed7ce081bc6bd81ddea3
rcktrncn <rcktrncn@hotmail.com>
2022-08-31 12:43:28 +0900

    remove debug code

--- taskt-updater/Program.cs
+++ taskt-updater/Program.cs
@@ -18,16 +18,6 @@ namespace taskt_updater
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            // Debug
-            //if (args.Length == 0)
-            //{
-            //    //args[0] = "https://github.com/rcktrncn/taskt-up-test/releases/download/v3.5.1.5/taskt-uob_v3.5.1.5.zip";
-            //    string[] newArg = new string[2];
-            //    newArg[0] = "/r";
-            //    newArg[1] = "\"C:\\Users\\rr\\Documents\\GitHub\\taskt\\taskt-updater\\bin\\Debug\"";
-            //    args = newArg;
-            //}
-
             if (args.Count() == 0)
             {
                 MessageBox.Show("Update Tool requires a package argument!");
