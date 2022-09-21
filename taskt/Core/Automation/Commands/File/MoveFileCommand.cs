105961cf9a9f62051c2c96458e25bfc25207c8a4
rcktrncn <rcktrncn@hotmail.com>
2022-09-21 22:55:00 +0900

    change attributes to render, displayText, validate

--- taskt/Core/Automation/Commands/File/MoveFileCommand.cs
+++ taskt/Core/Automation/Commands/File/MoveFileCommand.cs
@@ -5,64 +5,75 @@ using System.Windows.Forms;
 using System.Collections.Generic;
 using taskt.UI.Forms;
 using taskt.UI.CustomControls;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
 {
-
-
- 
     [Serializable]
     [Attributes.ClassAttributes.Group("File Operation Commands")]
     [Attributes.ClassAttributes.Description("This command moves a file to a specified destination")]
     [Attributes.ClassAttributes.UsesDescription("Use this command to move a file to a new destination.")]
     [Attributes.ClassAttributes.ImplementationDescription("This command implements '' to achieve automation.")]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
     public class MoveFileCommand : ScriptCommand
     {
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Optional - Indicate whether to move or copy the file (default is Move File)")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("Move File")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("Copy File")]
-        [Attributes.PropertyAttributes.InputSpecification("Specify whether you intend to move the file or copy the file.  Moving will remove the file from the original path while Copying will not.")]
-        [Attributes.PropertyAttributes.SampleUsage("Select either **Move File** or **Copy File**")]
-        [Attributes.PropertyAttributes.Remarks("")]
+        [PropertyDescription("Indicate whether to move or copy the file")]
+        [PropertyUISelectionOption("Move File")]
+        [PropertyUISelectionOption("Copy File")]
+        [InputSpecification("Specify whether you intend to move the file or copy the file.  Moving will remove the file from the original path while Copying will not.")]
+        [SampleUsage("Select either **Move File** or **Copy File**")]
+        [Remarks("")]
+        [PropertyIsOptional(true, "Move File")]
+        [PropertyDisplayText(true, "Operation")]
         public string v_OperationType { get; set; }
 
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Please indicate the path to the source file (ex. C:\\temp\\myfile.txt, {{{vFilePath}}})")]
-        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowFileSelectionHelper)]
-        [Attributes.PropertyAttributes.InputSpecification("Enter or Select the path to the file.")]
-        [Attributes.PropertyAttributes.SampleUsage("**C:\\temp\\myfile.txt** or **{{{vTextFilePath}}}**")]
-        [Attributes.PropertyAttributes.Remarks("")]
+        [PropertyDescription("Please indicate the path to the source file")]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowFileSelectionHelper)]
+        [InputSpecification("Enter or Select the path to the file.")]
+        [SampleUsage("**C:\\temp\\myfile.txt** or **{{{vTextFilePath}}}**")]
+        [Remarks("")]
+        [PropertyShowSampleUsageInDescription(true)]
+        [PropertyTextBoxSetting(1, false)]
+        [PropertyValidationRule("Source File", PropertyValidationRule.ValidationRuleFlags.Empty)]
+        [PropertyDisplayText(true, "File")]
         public string v_SourceFilePath { get; set; }
 
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Please indicate the directory to move/copy to (ex. C:\\temp\\new_folder, {{{vFolderPath}}})")]
-        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowFolderSelectionHelper)]
-        [Attributes.PropertyAttributes.InputSpecification("Enter or Select the new path to the file.")]
-        [Attributes.PropertyAttributes.SampleUsage("**C:\\temp\\new path\\** or **{{{vTextFolderPath}}}**")]
-        [Attributes.PropertyAttributes.Remarks("")]
+        [PropertyDescription("Please indicate the directory to move/copy to")]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowFolderSelectionHelper)]
+        [InputSpecification("Enter or Select the new path to the file.")]
+        [SampleUsage("**C:\\temp\\new path\\** or **{{{vTextFolderPath}}}**")]
+        [Remarks("")]
+        [PropertyShowSampleUsageInDescription(true)]
+        [PropertyTextBoxSetting(1, false)]
+        [PropertyValidationRule("Directory", PropertyValidationRule.ValidationRuleFlags.Empty)]
+        [PropertyDisplayText(true, "To")]
         public string v_DestinationDirectory { get; set; }
 
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Optional - Create folder if destination does not exist (default is No)")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("Yes")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("No")]
-        [Attributes.PropertyAttributes.InputSpecification("Specify whether the directory should be created if it does not already exist.")]
-        [Attributes.PropertyAttributes.SampleUsage("Select **Yes** or **No**")]
-        [Attributes.PropertyAttributes.Remarks("")]
+        [PropertyDescription("Create folder if destination does not exist")]
+        [PropertyUISelectionOption("Yes")]
+        [PropertyUISelectionOption("No")]
+        [InputSpecification("Specify whether the directory should be created if it does not already exist.")]
+        [SampleUsage("Select **Yes** or **No**")]
+        [Remarks("")]
+        [PropertyIsOptional(true, "No")]
         public string v_CreateDirectory { get; set; }
 
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Optional - Delete file if it already exists (default is No)")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("Yes")]
-        [Attributes.PropertyAttributes.PropertyUISelectionOption("No")]
-        [Attributes.PropertyAttributes.InputSpecification("Specify whether the file should be deleted first if it is already found to exist.")]
-        [Attributes.PropertyAttributes.SampleUsage("Select **Yes** or **No**")]
-        [Attributes.PropertyAttributes.Remarks("")]
+        [PropertyDescription("Delete file if it already exists")]
+        [PropertyUISelectionOption("Yes")]
+        [PropertyUISelectionOption("No")]
+        [InputSpecification("Specify whether the file should be deleted first if it is already found to exist.")]
+        [SampleUsage("Select **Yes** or **No**")]
+        [Remarks("")]
+        [PropertyIsOptional(true, "No")]
         public string v_DeleteExisting { get; set; }
-
 
         public MoveFileCommand()
         {
@@ -79,7 +90,7 @@ namespace taskt.Core.Automation.Commands
             var sourceFile = v_SourceFilePath.ConvertToUserVariable(sender);
             var destinationFolder = v_DestinationDirectory.ConvertToUserVariable(sender);
 
-            if (!System.IO.Directory.Exists(destinationFolder))
+            if (!Directory.Exists(destinationFolder))
             {
                 var vCreateDirectroy = v_CreateDirectory.ConvertToUserVariable(sender);
                 if (String.IsNullOrEmpty(vCreateDirectroy))
@@ -88,7 +99,7 @@ namespace taskt.Core.Automation.Commands
                 }
                 if (vCreateDirectroy.ToLower() == "yes")
                 {
-                    System.IO.Directory.CreateDirectory(destinationFolder);
+                    Directory.CreateDirectory(destinationFolder);
                 }
                 else
                 {
@@ -97,16 +108,16 @@ namespace taskt.Core.Automation.Commands
             }
 
             //get source file name and info
-            System.IO.FileInfo sourceFileInfo = new FileInfo(sourceFile);
+            FileInfo sourceFileInfo = new FileInfo(sourceFile);
 
             //create destination
-            var destinationPath = System.IO.Path.Combine(destinationFolder, sourceFileInfo.Name);
+            var destinationPath = Path.Combine(destinationFolder, sourceFileInfo.Name);
 
             //delete if it already exists per user
             var vDeleteExistsint = v_DeleteExisting.ConvertToUserVariable(sender);
             if (vDeleteExistsint.ToLower() == "yes")
             {
-                System.IO.File.Delete(destinationPath);
+                File.Delete(destinationPath);
             }
 
             var vOperationType = v_OperationType.ConvertToUserVariable(sender);
@@ -117,47 +128,47 @@ namespace taskt.Core.Automation.Commands
             if (vOperationType == "Move File")
             {
                 //move file
-                System.IO.File.Move(sourceFile, destinationPath);
+                File.Move(sourceFile, destinationPath);
             }
             else
             {
                 //copy file
-                System.IO.File.Copy(sourceFile, destinationPath);
+                File.Copy(sourceFile, destinationPath);
             }
         }
-        public override List<Control> Render(frmCommandEditor editor)
-        {
-            base.Render(editor);
+        //public override List<Control> Render(frmCommandEditor editor)
+        //{
+        //    base.Render(editor);
 
-            RenderedControls.AddRange(CommandControls.CreateDefaultDropdownGroupFor("v_OperationType", this, editor));
-            RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_SourceFilePath", this, editor));
-            RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_DestinationDirectory", this, editor));
-            RenderedControls.AddRange(CommandControls.CreateDefaultDropdownGroupFor("v_CreateDirectory", this, editor));
-            RenderedControls.AddRange(CommandControls.CreateDefaultDropdownGroupFor("v_DeleteExisting", this, editor));
-            return RenderedControls;
-        }
+        //    RenderedControls.AddRange(CommandControls.CreateDefaultDropdownGroupFor("v_OperationType", this, editor));
+        //    RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_SourceFilePath", this, editor));
+        //    RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_DestinationDirectory", this, editor));
+        //    RenderedControls.AddRange(CommandControls.CreateDefaultDropdownGroupFor("v_CreateDirectory", this, editor));
+        //    RenderedControls.AddRange(CommandControls.CreateDefaultDropdownGroupFor("v_DeleteExisting", this, editor));
+        //    return RenderedControls;
+        //}
 
-        public override string GetDisplayValue()
-        {
-            return base.GetDisplayValue() + " [" + v_OperationType + " from '" + v_SourceFilePath + "' to '" + v_DestinationDirectory + "']";
-        }
+        //public override string GetDisplayValue()
+        //{
+        //    return base.GetDisplayValue() + " [" + v_OperationType + " from '" + v_SourceFilePath + "' to '" + v_DestinationDirectory + "']";
+        //}
 
-        public override bool IsValidate(frmCommandEditor editor)
-        {
-            base.IsValidate(editor);
+        //public override bool IsValidate(frmCommandEditor editor)
+        //{
+        //    base.IsValidate(editor);
 
-            if (String.IsNullOrEmpty(v_SourceFilePath))
-            {
-                this.validationResult += "Source file is empty.\n";
-                this.IsValid = false;
-            }
-            if (String.IsNullOrEmpty(v_SourceFilePath))
-            {
-                this.validationResult += "Move/copy directory is empty.\n";
-                this.IsValid = false;
-            }
+        //    if (String.IsNullOrEmpty(v_SourceFilePath))
+        //    {
+        //        this.validationResult += "Source file is empty.\n";
+        //        this.IsValid = false;
+        //    }
+        //    if (String.IsNullOrEmpty(v_SourceFilePath))
+        //    {
+        //        this.validationResult += "Move/copy directory is empty.\n";
+        //        this.IsValid = false;
+        //    }
 
-            return this.IsValid;
-        }
+        //    return this.IsValid;
+        //}
     }
 }
--- taskt/Core/Automation/Commands/File/RenameFileCommand.cs
+++ taskt/Core/Automation/Commands/File/RenameFileCommand.cs
@@ -5,33 +5,42 @@ using taskt.UI.CustomControls;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using taskt.UI.Forms;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
 {
-
     [Serializable]
     [Attributes.ClassAttributes.Group("File Operation Commands")]
     [Attributes.ClassAttributes.Description("This command renames a file at a specified destination")]
     [Attributes.ClassAttributes.UsesDescription("Use this command to rename an existing file.")]
     [Attributes.ClassAttributes.ImplementationDescription("This command implements '' to achieve automation.")]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
     public class RenameFileCommand : ScriptCommand
     {
-
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Please indicate the path to the source file (ex. C:\\temp\\myfile.txt, {{{vFilePath}}})")]
-        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowFileSelectionHelper)]
-        [Attributes.PropertyAttributes.InputSpecification("Enter or Select the path to the file.")]
-        [Attributes.PropertyAttributes.SampleUsage("**C:\\temp\\myfile.txt** or **{{{vTextFilePath}}}**")]
-        [Attributes.PropertyAttributes.Remarks("")]
+        [PropertyDescription("Please indicate the path to the source file")]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowFileSelectionHelper)]
+        [InputSpecification("Enter or Select the path to the file.")]
+        [SampleUsage("**C:\\temp\\myfile.txt** or **{{{vTextFilePath}}}**")]
+        [Remarks("")]
+        [PropertyShowSampleUsageInDescription(true)]
+        [PropertyTextBoxSetting(1, false)]
+        [PropertyValidationRule("File", PropertyValidationRule.ValidationRuleFlags.Empty)]
+        [PropertyDisplayText(true, "File")]
         public string v_SourceFilePath { get; set; }
 
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Please indicate the new file name (with extension) (ex. newfilename.txt, {{{vFileName}}})")]
-        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [Attributes.PropertyAttributes.InputSpecification("Specify the new file name including the extension.")]
-        [Attributes.PropertyAttributes.SampleUsage("**newfile.txt** or **{{{vNewFileName}}}**")]
-        [Attributes.PropertyAttributes.Remarks("Changing the file extension will not automatically convert files.")]
+        [PropertyDescription("Please indicate the new file name (with extension)")]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [InputSpecification("Specify the new file name including the extension.")]
+        [SampleUsage("**newfile.txt** or **{{{vNewFileName}}}**")]
+        [Remarks("Changing the file extension will not automatically convert files.")]
+        [PropertyShowSampleUsageInDescription(true)]
+        [PropertyTextBoxSetting(1, false)]
+        [PropertyValidationRule("New FileName", PropertyValidationRule.ValidationRuleFlags.Empty)]
+        [PropertyDisplayText(true, "New FileName")]
         public string v_NewName { get; set; }
 
         public RenameFileCommand()
@@ -44,51 +53,50 @@ namespace taskt.Core.Automation.Commands
 
         public override void RunCommand(object sender)
         {
-
             //apply variable logic
             var sourceFile = v_SourceFilePath.ConvertToUserVariable(sender);
             var newFileName = v_NewName.ConvertToUserVariable(sender);
 
             //get source file name and info
-            System.IO.FileInfo sourceFileInfo = new FileInfo(sourceFile);
+            FileInfo sourceFileInfo = new FileInfo(sourceFile);
 
             //create destination
-            var destinationPath = System.IO.Path.Combine(sourceFileInfo.DirectoryName, newFileName);
+            var destinationPath = Path.Combine(sourceFileInfo.DirectoryName, newFileName);
 
             //rename file
-            System.IO.File.Move(sourceFile, destinationPath);
-
-        }
-        public override List<Control> Render(frmCommandEditor editor)
-        {
-            base.Render(editor);
-
-            RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_SourceFilePath", this, editor));
-            RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_NewName", this, editor));
-
-            return RenderedControls;
-        }
-        public override string GetDisplayValue()
-        {
-            return base.GetDisplayValue() + " [rename " + v_SourceFilePath + " to '" + v_NewName + "']";
+            File.Move(sourceFile, destinationPath);
         }
 
-        public override bool IsValidate(frmCommandEditor editor)
-        {
-            base.IsValidate(editor);
+        //public override List<Control> Render(frmCommandEditor editor)
+        //{
+        //    base.Render(editor);
 
-            if (String.IsNullOrEmpty(this.v_SourceFilePath))
-            {
-                this.validationResult += "Source file is empty.\n";
-                this.IsValid = false;
-            }
-            if (String.IsNullOrEmpty(this.v_NewName))
-            {
-                this.validationResult += "New file name is empty.\n";
-                this.IsValid = false;
-            }
+        //    RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_SourceFilePath", this, editor));
+        //    RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_NewName", this, editor));
 
-            return this.IsValid;
-        }
+        //    return RenderedControls;
+        //}
+        //public override string GetDisplayValue()
+        //{
+        //    return base.GetDisplayValue() + " [rename " + v_SourceFilePath + " to '" + v_NewName + "']";
+        //}
+
+        //public override bool IsValidate(frmCommandEditor editor)
+        //{
+        //    base.IsValidate(editor);
+
+        //    if (String.IsNullOrEmpty(this.v_SourceFilePath))
+        //    {
+        //        this.validationResult += "Source file is empty.\n";
+        //        this.IsValid = false;
+        //    }
+        //    if (String.IsNullOrEmpty(this.v_NewName))
+        //    {
+        //        this.validationResult += "New file name is empty.\n";
+        //        this.IsValid = false;
+        //    }
+
+        //    return this.IsValid;
+        //}
     }
 }
