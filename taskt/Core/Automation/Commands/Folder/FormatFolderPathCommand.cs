5a938b433dfaff95d7bfb6dad6e2883b35810a6e
rcktrncn <rcktrncn@hotmail.com>
2022-09-25 12:32:04 +0900

    add path format commands

---
+++ taskt/Core/Automation/Commands/Folder/FormatFolderPathCommand.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using taskt.UI.CustomControls;
+using taskt.UI.Forms;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+
+    [Serializable]
+    [Attributes.ClassAttributes.Group("Folder Operation Commands")]
+    [Attributes.ClassAttributes.Description("This command allows you to format folder path.")]
+    [Attributes.ClassAttributes.UsesDescription("Use this command when you want to format folder path.")]
+    [Attributes.ClassAttributes.ImplementationDescription("")]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    public class FormatFolderPathCommnad : ScriptCommand
+    {
+        [XmlAttribute]
+        [PropertyDescription("Please indicate the Folder Path to Delete.")]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowFolderSelectionHelper)]
+        [InputSpecification("Enter or Select the path to the folder.")]
+        [SampleUsage("**C:\\temp\\myfolder** or **{{{vTextFolderPath}}}**")]
+        [Remarks("")]
+        [PropertyShowSampleUsageInDescription(true)]
+        [PropertyTextBoxSetting(1, false)]
+        [PropertyValidationRule("Folder Path", PropertyValidationRule.ValidationRuleFlags.Empty)]
+        [PropertyDisplayText(true, "Folder")]
+        public string v_SourceFolderPath { get; set; }
+
+        [XmlAttribute]
+        [PropertyDescription("Please specify File Path Format.")]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [PropertyCustomUIHelper("Format Checker", "lnkFormatChecker_Click")]
+        [InputSpecification("")]
+        [SampleUsage("**Folder** or **DriveName** etc")]
+        [Remarks("")]
+        [PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        [PropertyUISelectionOption("Folder")]
+        [PropertyUISelectionOption("DriveName")]
+        [PropertyValidationRule("Format", PropertyValidationRule.ValidationRuleFlags.Empty)]
+        [PropertyDisplayText(true, "Format")]
+        public string v_Format { get; set; }
+
+        [XmlAttribute]
+        [PropertyDescription("Please specify Variable Name to store Result.")]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [InputSpecification("")]
+        [SampleUsage("")]
+        [Remarks("")]
+        [PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        [PropertyIsVariablesList(true)]
+        [PropertyValidationRule("Result", PropertyValidationRule.ValidationRuleFlags.Empty)]
+        [PropertyDisplayText(true, "Store")]
+        public string v_Result { get; set; }
+
+        public FormatFolderPathCommnad()
+        {
+            this.CommandName = "Format Folder PathCommand";
+            this.SelectionName = "Format Folder Path";
+            this.CommandEnabled = true;
+            this.CustomRendering = true;
+        }
+
+        public override void RunCommand(object sender)
+        {
+            var engine = (taskt.Core.Automation.Engine.AutomationEngineInstance)sender;
+
+            string filePath = v_SourceFolderPath.ConvertToUserVariable(engine);
+            string format = v_Format.ConvertToUserVariable(engine);
+
+            string result = FilePathControls.formatFileFolderPath(filePath, format);
+            result.StoreInUserVariable(engine, v_Result);
+        }
+
+        private void lnkFormatChecker_Click(object sender, EventArgs e)
+        {
+            ComboBox cmb = (ComboBox)((CommandItemControl)sender).Tag;
+            UI.Forms.Supplement_Forms.frmFormatChecker.ShowFormatCheckerFormLinkClicked(cmb, "File Folder");
+        }
+    }
+}
