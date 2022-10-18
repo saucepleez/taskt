d62bb88d182d2f56d9f00cb71e4b42d7db05c544
rcktrncn <rcktrncn@hotmail.com>
2022-10-18 22:33:12 +0900

    remove using

--- taskt/Core/Automation/Commands/NumberControls.cs
+++ taskt/Core/Automation/Commands/NumberControls.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
