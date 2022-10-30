c5e8fbd2992529233018d9f70b2d19f8c0871aa6
rcktrncn <rcktrncn@hotmail.com>
2022-10-30 18:07:25 +0900

    fix property name

--- taskt/Core/Automation/Commands/Excel/ExcelGetCellRCCommand.cs
+++ taskt/Core/Automation/Commands/Excel/ExcelGetCellRCCommand.cs
@@ -92,7 +92,7 @@ namespace taskt.Core.Automation.Commands
             (var excelInstance, var excelSheet) = v_InstanceName.GetExcelInstanceAndWorksheet(engine);
 
             //var rg = ((v_ExcelCellRow, nameof(v_ExcelCellRow)), (v_ExcelCellColumn, nameof(v_ExcelCellColumn))).ConvertToExcelRange(engine, excelInstance, excelSheet, this);
-            var rg = this.ConvertToExcelRange(nameof(v_CellRow), nameof(v_CellColumn), engine, excelInstance, excelSheet);
+            var rg = this.ConvertToExcelRange(nameof(v_ExcelCellRow), nameof(v_ExcelCellColumn), engine, excelInstance, excelSheet);
 
             //var valueType = v_ValueType.GetUISelectionValue("v_ValueType", this, engine);
             //var valueType = new PropertyConvertTag(v_ValueType, nameof(v_ValueType), "Value Type").GetUISelectionValue(this, engine);
