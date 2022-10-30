7e316b76aa1dfa8e951c6a03b206cccc9cda5978
rcktrncn <rcktrncn@hotmail.com>
2022-10-30 21:37:55 +0900

    Add ColorControls

---
+++ taskt/Core/Automation/Commands/ColorControls.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace taskt.Core.Automation.Commands
+{
+    internal static class ColorControls
+    {
+        public static System.Drawing.Color GetColorVariable(this string variableName, Core.Automation.Engine.AutomationEngineInstance engine)
+        {
+            Script.ScriptVariable v = variableName.GetRawVariable(engine);
+            if (v.VariableValue is System.Drawing.Color)
+            {
+                return (System.Drawing.Color)v.VariableValue;
+            }
+            else
+            {
+                throw new Exception("Variable " + variableName + " is not Color");
+            }
+        }
+
+        public static void StoreInUserVariable(this System.Drawing.Color value, Core.Automation.Engine.AutomationEngineInstance sender, string targetVariable)
+        {
+            ExtensionMethods.StoreInUserVariable(targetVariable, value, sender);
+        }
+    }
+}
