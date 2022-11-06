372e20211f14a7e6160860dd556c5939c52bca68
rcktrncn <rcktrncn@hotmail.com>
2022-11-06 21:51:16 +0900

    move Dictionary methods

---
+++ taskt/Core/Automation/Commands/DictionaryControls.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskt.Core.Automation.Commands
+{
+    internal static class DictionaryControls
+    {
+        /// <summary>
+        /// get Dictionary&lt;string, string&gt; Variable from variable name
+        /// </summary>
+        /// <param name="variableName"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static Dictionary<string, string> GetDictionaryVariable(this string variableName, Core.Automation.Engine.AutomationEngineInstance engine)
+        {
+            Script.ScriptVariable v = variableName.GetRawVariable(engine);
+            if (v.VariableValue is Dictionary<string, string> dictionary)
+            {
+                return dictionary;
+            }
+            else
+            {
+                throw new Exception("Variable " + variableName + " is not Dictionary");
+            }
+        }
+
+        public static void StoreInUserVariable(this Dictionary<string, string> value, Core.Automation.Engine.AutomationEngineInstance sender, string targetVariable)
+        {
+            ExtensionMethods.StoreInUserVariable(targetVariable, value, sender, false);
+        }
+    }
+}
--- taskt/Core/ExtensionMethods.cs
+++ taskt/Core/ExtensionMethods.cs
@@ -1000,10 +1000,6 @@ namespace taskt.Core
             StoreInUserVariable(targetVariable, value ? "TRUE" : "FALSE", sender, false);
         }
 
-        public static void StoreInUserVariable(this Dictionary<string, string> value, Core.Automation.Engine.AutomationEngineInstance sender, string targetVariable)
-        {
-            StoreInUserVariable(targetVariable, value, sender, false);
-        }
 
         public static void StoreInUserVariable(this DataTable value, Core.Automation.Engine.AutomationEngineInstance sender, string targetVariable)
         {
@@ -1088,19 +1084,6 @@ namespace taskt.Core
             }
         }
 
-
-        public static Dictionary<string, string> GetDictionaryVariable(this string variableName, Core.Automation.Engine.AutomationEngineInstance engine)
-        {
-            Script.ScriptVariable v = variableName.GetRawVariable(engine);
-            if (v.VariableValue is Dictionary<string, string>)
-            {
-                return (Dictionary<string, string>)v.VariableValue;
-            }
-            else
-            {
-                throw new Exception("Variable " + variableName + " is not Dictionary");
-            }
-        }
 
         public static DataTable GetDataTableVariable(this string variableName, Core.Automation.Engine.AutomationEngineInstance engine)
         {
