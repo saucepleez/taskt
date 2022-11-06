949e0ecb90acf28de5374846169a3d3295ad9922
rcktrncn <rcktrncn@hotmail.com>
2022-11-06 20:58:01 +0900

    remove using

--- taskt/Core/Automation/Commands/ConditionControls.cs
+++ taskt/Core/Automation/Commands/ConditionControls.cs
@@ -3,9 +3,6 @@ using System.Data;
 using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime;
 
 namespace taskt.Core.Automation.Commands
 {
