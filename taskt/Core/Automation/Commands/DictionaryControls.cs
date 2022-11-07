5fb6eb0c192c2ffc24fd8946162359f62824f0f3
rcktrncn <rcktrncn@hotmail.com>
2022-11-07 21:54:03 +0900

    add dictionary, key method

--- taskt/Core/Automation/Commands/DictionaryControls.cs
+++ taskt/Core/Automation/Commands/DictionaryControls.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Word;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,48 @@ namespace taskt.Core.Automation.Commands
             else
             {
                 throw new Exception("Variable " + variableName + " is not Dictionary");
+            }
+        }
+
+        /// <summary>
+        /// get Dictionary&lt;string, string&gt; and key name from property names. It supports current position to key.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="dictionaryName"></param>
+        /// <param name="keyName"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static (Dictionary<string, string>, string) GetDictionaryVariableAndKey(this ScriptCommand command, string dictionaryName, string keyName, Core.Automation.Engine.AutomationEngineInstance engine)
+        {
+            string dicVariable = command.ConvertToUserVariable(dictionaryName, "Dictionary", engine);
+            var v = dicVariable.GetRawVariable(engine);
+            if (v.VariableValue is Dictionary<string, string> dictionary)
+            {
+                string keyVariable = command.ConvertToUserVariable(keyName, "Key", engine);
+                string key;
+                if (String.IsNullOrEmpty(keyVariable))
+                {
+                    int pos = v.CurrentPosition;
+                    string[] keys = dictionary.Keys.ToArray();
+                    if ((pos >= 0) && (pos < keys.Length))
+                    {
+                        key = keys[pos];
+                    }
+                    else
+                    {
+                        throw new Exception("Strange Current Position in Dictionary " + pos);
+                    }
+                }
+                else
+                {
+                    key = keyVariable.ConvertToUserVariable(engine);
+                }
+                return (dictionary, key);
+            }
+            else
+            {
+                throw new Exception("Variable " + dicVariable + " is not Dictionary");
             }
         }
 
