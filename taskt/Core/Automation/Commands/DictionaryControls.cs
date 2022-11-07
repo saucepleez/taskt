58c5b393b8193cb33dcb434492ee2de7f3ab60e9
rcktrncn <rcktrncn@hotmail.com>
2022-11-07 22:17:25 +0900

    remove using

--- taskt/Core/Automation/Commands/DictionaryControls.cs
+++ taskt/Core/Automation/Commands/DictionaryControls.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.Office.Interop.Word;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace taskt.Core.Automation.Commands
 {
