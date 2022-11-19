e88a1edec49b2ad50e8cc2c7f0d6e70a50c539c8
rcktrncn <rcktrncn@hotmail.com>
2022-11-19 17:22:54 +0900

    use WaitControls method

--- taskt/Core/Automation/Commands/Folder/WaitForFolderToExistCommand.cs
+++ taskt/Core/Automation/Commands/Folder/WaitForFolderToExistCommand.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
 using System.Xml.Serialization;
-using taskt.UI.CustomControls;
-using taskt.UI.Forms;
 using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
@@ -54,50 +50,42 @@ namespace taskt.Core.Automation.Commands
         {
             var engine = (Engine.AutomationEngineInstance)sender;
 
-            //convert items to variables
             var folder = v_FolderName.ConvertToUserVariable(sender);
-            int pauseTime = v_WaitTime.ConvertToUserVariableAsInteger("Wait Time", engine);
 
-            //determine when to stop waiting based on user config
-            var stopWaiting = DateTime.Now.AddSeconds(pauseTime);
+            //int pauseTime = v_WaitTime.ConvertToUserVariableAsInteger("Wait Time", engine);
 
-            //initialize flag for file found
-            var folderFound = false;
+            ////determine when to stop waiting based on user config
+            //var stopWaiting = DateTime.Now.AddSeconds(pauseTime);
 
-            //while file has not been found
-            while (!folderFound)
+            ////initialize flag for file found
+            //var folderFound = false;
+
+            ////while file has not been found
+            //while (!folderFound)
+            //{
+            //    //if file exists at the folder path
+            //    if (System.IO.Directory.Exists(folder))
+            //    {
+            //        folderFound = true;
+            //    }
+
+            //    //test if we should exit and throw exception
+            //    if (DateTime.Now > stopWaiting)
+            //    {
+            //        throw new Exception("Folder was not found in time!");
+            //    }
+
+            //    //put thread to sleep before iterating
+            //    engine.ReportProgress("Folder Not Yet Found... " + (int)((stopWaiting - DateTime.Now).TotalSeconds) + "s remain");
+            //    System.Threading.Thread.Sleep(1000);
+            //}
+
+            Func<bool> checkFolderFunc = new Func<bool>(() =>
             {
-                //if file exists at the folder path
-                if (System.IO.Directory.Exists(folder))
-                {
-                    folderFound = true;
-                }
+                return System.IO.Directory.Exists(folder);
+            });
 
-                //test if we should exit and throw exception
-                if (DateTime.Now > stopWaiting)
-                {
-                    throw new Exception("Folder was not found in time!");
-                }
-
-                //put thread to sleep before iterating
-                engine.ReportProgress("Folder Not Yet Found... " + (int)((stopWaiting - DateTime.Now).TotalSeconds) + "s remain");
-                System.Threading.Thread.Sleep(1000);
-            }
+            this.WaitProcess(nameof(v_WaitTime), "Folder", checkFolderFunc, engine);
         }
-
-        //public override List<Control> Render(frmCommandEditor editor)
-        //{
-        //    base.Render(editor);
-
-        //    var ctrls = CommandControls.MultiCreateInferenceDefaultControlGroupFor(this, editor);
-        //    RenderedControls.AddRange(ctrls);
-            
-        //    return RenderedControls;
-        //}
-
-        //public override string GetDisplayValue()
-        //{
-        //    return base.GetDisplayValue() + " [Folder: " + v_FolderName + ", Wait " + v_WaitTime + "s]";
-        //}
     }
 }
