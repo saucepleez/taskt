6183f9af25c972c7da146d63f510bb87448a6225
rcktrncn <rcktrncn@hotmail.com>
2022-12-14 21:42:28 +0900

    add new classes

---
+++ taskt/Core/Automation/Commands/DisplayTextControls.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskt.Core.Automation.Commands
+{
+    internal static class DisplayTextControls
+    {
+        public static string GetPropertyDisplayValue(PropertyInfo prop, ScriptCommand command)
+        {
+            object value = prop.GetValue(command);
+
+            Attributes.PropertyAttributes.PropertyDisplayText dispProp = (Attributes.PropertyAttributes.PropertyDisplayText)prop.GetCustomAttribute(typeof(Attributes.PropertyAttributes.PropertyDisplayText));
+            if ((dispProp == null) || (!dispProp.parameterDisplay))
+            {
+                return "";
+            }
+            else
+            {
+                string dispValue;
+                if (value == null)
+                {
+                    dispValue = "''";
+                }
+                else if (value is System.Data.DataTable)
+                {
+                    dispValue = ((System.Data.DataTable)value).Rows.Count + " items";
+                }
+                else if (!(value is string))
+                {
+                    dispValue = "'" + value.ToString() + "'";
+                }
+                else
+                {
+                    dispValue = "'" + value + "'";
+                }
+
+                if (dispProp.afterText != "")
+                {
+                    return dispProp.parameterName + ": " + dispValue + " " + dispProp.afterText + ", ";
+                }
+                else
+                {
+                    return dispProp.parameterName + ": " + dispValue + ", ";
+                }
+            }
+        }
+    }
+}
---
+++ taskt/Core/Automation/Commands/VirtualPropertyControls.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    internal static class VirtualPropertyControls
+    {
+        /// <summary>
+        /// get PropertyInfo from VirtualProperty
+        /// </summary>
+        /// <param name="propInfo"></param>
+        /// <returns></returns>
+        public static PropertyInfo GetVirtualProperty(PropertyInfo propInfo)
+        {
+            var attrVP = propInfo.GetCustomAttribute<PropertyVirtualProperty>();
+            if (attrVP == null)
+            {
+                return null;
+            }
+            var tp = Type.GetType("taskt.Core.Automation.Commands." + attrVP.className);
+            return tp.GetProperty(attrVP.propertyName, BindingFlags.Public | BindingFlags.Static);
+        }
+    }
+}
