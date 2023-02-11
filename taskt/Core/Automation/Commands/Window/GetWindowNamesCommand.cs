c1eaf3cf9ecc4063dbef45064800afef32df38e2
rcktrncn <rcktrncn@hotmail.com>
2023-02-11 21:49:14 +0900

    remove comment out codes, change method name

--- taskt/Core/Automation/Commands/Window/GetWindowNamesCommand.cs
+++ taskt/Core/Automation/Commands/Window/GetWindowNamesCommand.cs
@@ -140,7 +140,7 @@ namespace taskt.Core.Automation.Commands
             string windowName = v_WindowName.ConvertToUserVariable(sender);
             string searchMethod = v_SearchMethod.GetUISelectionValue("v_SearchMethod", this, engine);
 
-            var searchFunc = WindowNameControls.getWindowSearchMethod(searchMethod);
+            var searchFunc = WindowNameControls.GetWindowSearchMethod(searchMethod);
             var list = WindowNameControls.GetAllWindowTitles();
 
             var ret = new List<string>();
--- taskt/Core/Automation/Commands/WindowNameControls.cs
+++ taskt/Core/Automation/Commands/WindowNameControls.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 
 namespace taskt.Core.Automation.Commands
 {
-    internal class WindowNameControls
+    /// <summary>
+    /// for window name methods
+    /// </summary>
+    internal static class WindowNameControls
     {
 
-        public static Func<string, string, bool> getWindowSearchMethod(string searchMethod)
+        public static Func<string, string, bool> GetWindowSearchMethod(string searchMethod)
         {
             Func<string, string, bool> ret;
             switch (searchMethod.ToLower())
@@ -44,49 +47,6 @@ namespace taskt.Core.Automation.Commands
             return User32.User32Functions.GetActiveWindow();
         }
 
-        //public static IntPtr FindWindow(string windowName, string searchMethod)
-        //{
-        //    var searchFunc = getWindowSearchMethod(searchMethod);
-
-        //    if (windowName.StartsWith("Windows Explorer -"))
-        //    {
-        //        var windowLocationName = windowName.Split('-')[1].Trim();
-
-        //        SHDocVw.ShellWindows shellWindows = new SHDocVw.ShellWindows();
-
-        //        foreach (SHDocVw.InternetExplorer window in shellWindows)
-        //        {
-        //            //if (window.LocationName.Contains(windowLocationName))
-        //            if (searchFunc(window.LocationName, windowName))
-        //            {
-        //                return new IntPtr(window.HWND);
-        //            }
-        //        }
-
-        //        return IntPtr.Zero;
-        //    }
-        //    else
-        //    {
-        //        //try to find exact window name
-        //        IntPtr hWnd = User32.User32Functions.FindWindowNative(null, windowName);
-        //        if (hWnd == IntPtr.Zero)
-        //        {
-        //            //potentially wait for some additional initialization
-        //            System.Threading.Thread.Sleep(1000);
-        //            hWnd = User32.User32Functions.FindWindowNative(null, windowName);
-        //        }
-
-        //        //if exact window was not found, try partial match
-        //        if (hWnd == IntPtr.Zero)
-        //        {
-        //            var potentialWindow = System.Diagnostics.Process.GetProcesses().Where(prc => prc.MainWindowTitle.Contains(windowName)).FirstOrDefault();
-        //            if (potentialWindow != null)
-        //                hWnd = potentialWindow.MainWindowHandle;
-        //        }
-        //        //return hwnd
-        //        return hWnd;
-        //    }
-        //}
         public static IntPtr FindWindow(string windowName, string searchMethod, Automation.Engine.AutomationEngineInstance engine)
         {
             if (windowName == engine.engineSettings.CurrentWindowKeyword)
@@ -96,7 +56,7 @@ namespace taskt.Core.Automation.Commands
             else
             {
                 var windows = GetAllWindows();
-                var method = getWindowSearchMethod(searchMethod);
+                var method = GetWindowSearchMethod(searchMethod);
                 foreach(var win in windows)
                 {
                     if (method(win.Value, windowName))
@@ -108,89 +68,18 @@ namespace taskt.Core.Automation.Commands
             // not found
             throw new Exception("Window Name '" + windowName + "' not found");
         }
-        //public static List<IntPtr> FindWindows(string windowName, string searchMethod)
-        //{
-        //    var searchFunc = getWindowSearchMethod(searchMethod);
-
-        //    List<IntPtr> ret = new List<IntPtr>();
-        //    if (windowName.Contains("Windows Explorer -"))
-        //    {
-        //        var windowLocationName = windowName.Split('-')[1].Trim();
-
-        //        SHDocVw.ShellWindows shellWindows = new SHDocVw.ShellWindows();
-
-        //        foreach (SHDocVw.InternetExplorer window in shellWindows)
-        //        {
-        //            //if (window.LocationName.Contains(windowLocationName))
-        //            if (searchFunc(window.LocationName, windowName))
-        //            {
-        //                ret.Add((IntPtr)window.HWND);
-        //            }
-        //        }
-        //        return ret;
-        //    }
-        //    else
-        //    {
-        //        //try to find exact window name
-        //        IntPtr hWnd = User32.User32Functions.FindWindowNative(null, windowName);
-
-        //        if (hWnd == IntPtr.Zero)
-        //        {
-        //            //potentially wait for some additional initialization
-        //            System.Threading.Thread.Sleep(1000);
-        //            hWnd = User32.User32Functions.FindWindowNative(null, windowName);
-        //            if (hWnd != IntPtr.Zero)
-        //            {
-        //                ret.Add(hWnd);
-        //            }
-        //        }
-        //        else
-        //        {
-        //            ret.Add(hWnd);
-        //        }
-        //        //if exact window was not found, try partial match
-        //        var potentialWindows = System.Diagnostics.Process.GetProcesses().Where(prc => prc.MainWindowTitle.Contains(windowName)).ToList();
-        //        foreach (var potentialWindow in potentialWindows)
-        //        {
-        //            ret.Add(potentialWindow.MainWindowHandle);
-        //        }
-        //        //return hwnd
-        //        return ret;
-        //    }
-        //}
+        
         public static List<IntPtr> FindWindows(string windowName, string searchMethod, Engine.AutomationEngineInstance engine)
         {
             List<IntPtr> ret = new List<IntPtr>();
 
             if (windowName == engine.engineSettings.CurrentWindowKeyword)
             {
-                //ret.Add(User32.User32Functions.GetActiveWindow());
-                //return ret;
                 windowName = GetCurrentWindowName();
             }
-            //else
-            //{
-            //    var windows = GetAllWindows();
-            //    var method = getWindowSearchMethod(searchMethod);
-            //    foreach (var win in windows)
-            //    {
-            //        if (method(win.Value, windowName))
-            //        {
-            //            ret.Add(win.Key);
-            //        }
-            //    }
-            //    if (ret.Count > 0)
-            //    {
-            //        return ret;
-            //    }
-            //    else
-            //    {
-            //        // not found
-            //        throw new Exception("Window Name '" + windowName + "' not found");
-            //    }
-            //}
+
             var windows = GetAllWindows();
-            var method = getWindowSearchMethod(searchMethod);
+            var method = GetWindowSearchMethod(searchMethod);
             foreach (var win in windows)
             {
                 if (method(win.Value, windowName))
@@ -255,7 +144,7 @@ namespace taskt.Core.Automation.Commands
             else
             {
                 var windows = GetAllWindows();
-                var method = getWindowSearchMethod(searchMethod);
+                var method = GetWindowSearchMethod(searchMethod);
                 foreach (var win in windows)
                 {
                     if (method(win.Value, windowName))
