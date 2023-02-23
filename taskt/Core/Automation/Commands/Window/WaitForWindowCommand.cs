f276d3a03df1b74bd04261b36b6b32dac7a69495
rcktrncn <rcktrncn@hotmail.com>
2023-02-23 14:05:49 +0900

    use virtualproperty, new methods

--- taskt/Core/Automation/Commands/Window/WaitForWindowCommand.cs
+++ taskt/Core/Automation/Commands/Window/WaitForWindowCommand.cs
@@ -14,40 +14,47 @@ namespace taskt.Core.Automation.Commands
     [Attributes.ClassAttributes.Description("This command waits for a window to exist.")]
     [Attributes.ClassAttributes.UsesDescription("Use this command when you want to explicitly wait for a window to exist before continuing script execution.")]
     [Attributes.ClassAttributes.ImplementationDescription("This command implements 'FindWindowNative', 'ShowWindow' from user32.dll to achieve automation.")]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
     public class WaitForWindowCommand : ScriptCommand
     {
         [XmlAttribute]
-        [PropertyDescription("Please enter or select the window name that you are waiting for to exist.")]
-        [InputSpecification("Input or Type the name of the window that you want to wait to exist.")]
-        [SampleUsage("**Untitled - Notepad** or **%kwd_current_window%** or **{{{vWindow}}}**")]
-        [Remarks("")]
-        [PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
-        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [PropertyCustomUIHelper("Up-to-date", nameof(lnkUpToDate_Click))]
-        [PropertyIsWindowNamesList(true)]
-        [PropertyShowSampleUsageInDescription(true)]
-        [PropertyValidationRule("Window Name", PropertyValidationRule.ValidationRuleFlags.Empty)]
+        //[PropertyDescription("Please enter or select the window name that you are waiting for to exist.")]
+        //[InputSpecification("Input or Type the name of the window that you want to wait to exist.")]
+        //[SampleUsage("**Untitled - Notepad** or **%kwd_current_window%** or **{{{vWindow}}}**")]
+        //[Remarks("")]
+        //[PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        //[PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        //[PropertyCustomUIHelper("Up-to-date", nameof(lnkUpToDate_Click))]
+        //[PropertyIsWindowNamesList(true)]
+        //[PropertyShowSampleUsageInDescription(true)]
+        //[PropertyValidationRule("Window Name", PropertyValidationRule.ValidationRuleFlags.Empty)]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WindowName))]
         public string v_WindowName { get; set; }
+
         [XmlAttribute]
-        [PropertyDescription("Window title search method")]
-        [InputSpecification("")]
-        [PropertyUISelectionOption("Contains")]
-        [PropertyUISelectionOption("Starts with")]
-        [PropertyUISelectionOption("Ends with")]
-        [PropertyUISelectionOption("Exact match")]
-        [SampleUsage("**Contains** or **Starts with** or **Ends with** or **Exact match**")]
-        [Remarks("")]
-        [PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
-        [PropertyIsOptional(true, "Contains")]
+        //[PropertyDescription("Window title search method")]
+        //[InputSpecification("")]
+        //[PropertyUISelectionOption("Contains")]
+        //[PropertyUISelectionOption("Starts with")]
+        //[PropertyUISelectionOption("Ends with")]
+        //[PropertyUISelectionOption("Exact match")]
+        //[SampleUsage("**Contains** or **Starts with** or **Ends with** or **Exact match**")]
+        //[Remarks("")]
+        //[PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        //[PropertyIsOptional(true, "Contains")]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_CompareMethod))]
         public string v_SearchMethod { get; set; }
+
         [XmlAttribute]
-        [PropertyDescription("Indicate how many seconds to wait before an error should be raised.")]
-        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [InputSpecification("Specify how many seconds to wait before an error should be invoked")]
-        [SampleUsage("**5** or **{{{vWaitTime}}}**")]
-        [Remarks("")]
-        [PropertyShowSampleUsageInDescription(true)]
-        [PropertyValidationRule("Wait Time", PropertyValidationRule.ValidationRuleFlags.Empty | PropertyValidationRule.ValidationRuleFlags.EqualsZero | PropertyValidationRule.ValidationRuleFlags.LessThanZero)]
+        //[PropertyDescription("Indicate how many seconds to wait before an error should be raised.")]
+        //[PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        //[InputSpecification("Specify how many seconds to wait before an error should be invoked")]
+        //[SampleUsage("**5** or **{{{vWaitTime}}}**")]
+        //[Remarks("")]
+        //[PropertyShowSampleUsageInDescription(true)]
+        //[PropertyValidationRule("Wait Time", PropertyValidationRule.ValidationRuleFlags.Empty | PropertyValidationRule.ValidationRuleFlags.EqualsZero | PropertyValidationRule.ValidationRuleFlags.LessThanZero)]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WaitTime))]
         public string v_LengthToWait { get; set; }
 
         [XmlIgnore]
@@ -66,57 +73,14 @@ namespace taskt.Core.Automation.Commands
         {
             var engine = (Engine.AutomationEngineInstance)sender;
 
-            string windowName = v_WindowName.ConvertToUserVariable(sender);
-            string searchMethod = v_SearchMethod.GetUISelectionValue("v_SearchMethod", this, engine);
-
-            //var waitUntil = v_LengthToWait.ConvertToUserVariableAsInteger("Length to Wait", engine);
-            //var endDateTime = DateTime.Now.AddSeconds(waitUntil);
-
-            //bool isFind = false;
-            //while (DateTime.Now < endDateTime)
-            //{
-            //    try
-            //    {
-            //        IntPtr wHnd = WindowNameControls.FindWindow(windowName, searchMethod, engine);
-            //        isFind = true;
-            //        break;
-            //    }
-            //    catch
-            //    {
-
-            //    }
-            //    engine.ReportProgress("Window Not Yet Found... " + (int)((endDateTime - DateTime.Now).TotalSeconds) + "s remain");
-            //    System.Threading.Thread.Sleep(1000);
-            //}
-
-            //if (!isFind)
-            //{
-            //    throw new Exception("Window was not found in the allowed time!");
-            //}
-
-            var windowWaitFunc = new Func<(bool, object)>(() =>
+            try
             {
-                try
-                {
-                    IntPtr wHnd = WindowNameControls.FindWindowHandle(windowName, searchMethod, engine);
-                    return (true, null);
-                }
-                catch
-                {
-                    return (false, null);
-                }
-            });
-            this.WaitProcess(nameof(v_LengthToWait), "Window", windowWaitFunc, engine);
-        }
-
-        public override List<Control> Render(frmCommandEditor editor)
-        {
-            base.Render(editor);
-
-            RenderedControls.AddRange(CommandControls.MultiCreateInferenceDefaultControlGroupFor(this, editor));
-
-            return RenderedControls;
-
+                WindowNameControls.FindWindows(this, nameof(v_WindowName), nameof(v_SearchMethod), nameof(v_LengthToWait), engine);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public override void Refresh(frmCommandEditor editor)
@@ -124,16 +88,27 @@ namespace taskt.Core.Automation.Commands
             base.Refresh();
             WindowNameControl.AddWindowNames();
         }
-        public override string GetDisplayValue()
-        {
-            return base.GetDisplayValue() + " [Target Window: '" + v_WindowName + "', Wait Up To " + v_LengthToWait + " seconds]";
-        }
 
-        private void lnkUpToDate_Click(object sender, EventArgs e)
-        {
-            ComboBox cmb = (ComboBox)((CommandItemControl)sender).Tag;
-            WindowNameControls.UpdateWindowTitleCombobox(cmb);
-        }
+        //public override List<Control> Render(frmCommandEditor editor)
+        //{
+        //    base.Render(editor);
+
+        //    RenderedControls.AddRange(CommandControls.MultiCreateInferenceDefaultControlGroupFor(this, editor));
+
+        //    return RenderedControls;
+
+        //}
+
+        //public override string GetDisplayValue()
+        //{
+        //    return base.GetDisplayValue() + " [Target Window: '" + v_WindowName + "', Wait Up To " + v_LengthToWait + " seconds]";
+        //}
+
+        //private void lnkUpToDate_Click(object sender, EventArgs e)
+        //{
+        //    ComboBox cmb = (ComboBox)((CommandItemControl)sender).Tag;
+        //    WindowNameControls.UpdateWindowTitleCombobox(cmb);
+        //}
 
         public override void ConvertToIntermediate(EngineSettings settings, List<Script.ScriptVariable> variables)
         {
