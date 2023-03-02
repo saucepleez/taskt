3e2f9ab414f9552788cac53039ffae02326b60c3
rcktrncn <rcktrncn@hotmail.com>
2023-03-02 21:55:15 +0900

    change command name

--- taskt/Core/Automation/Commands/Dictionary/AddDictionaryCommand.cs
+++ taskt/Core/Automation/Commands/Dictionary/AddDictionaryCommand.cs
@@ -10,12 +10,13 @@ namespace taskt.Core.Automation.Commands
     [Serializable]
     [Attributes.ClassAttributes.Group("Dictionary Commands")]
     [Attributes.ClassAttributes.SubGruop("Dictionary Item")]
+    [Attributes.ClassAttributes.CommandSettings("Add Dictionary Item")]
     [Attributes.ClassAttributes.Description("This command Adds a key and value to a existing Dictionary")]
     [Attributes.ClassAttributes.UsesDescription("Use this command when you want to add to a dictionary")]
     [Attributes.ClassAttributes.ImplementationDescription("")]
     [Attributes.ClassAttributes.EnableAutomateRender(true)]
     [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
-    public class AddDictionaryCommand : ScriptCommand
+    public class AddDictionaryItemCommand : ScriptCommand
     {
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(DictionaryControls), nameof(DictionaryControls.v_BothDictionaryName))]
@@ -25,12 +26,12 @@ namespace taskt.Core.Automation.Commands
         [PropertyVirtualProperty(nameof(DictionaryControls), nameof(DictionaryControls.v_KeyAndValue))]
         public DataTable v_ColumnNameDataTable { get; set; }
 
-        public AddDictionaryCommand()
+        public AddDictionaryItemCommand()
         {
-            this.CommandName = "AddDictionaryItemCommand";
-            this.SelectionName = "Add Dictionary Item";
-            this.CommandEnabled = true;
-            this.CustomRendering = true;
+            //this.CommandName = "AddDictionaryItemCommand";
+            //this.SelectionName = "Add Dictionary Item";
+            //this.CommandEnabled = true;
+            //this.CustomRendering = true;
         }
 
         public override void RunCommand(object sender)
