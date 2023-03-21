205fa233000a96c25f35a38a9a2e3a902cc78ef3
rcktrncn <rcktrncn@hotmail.com>
2023-03-21 18:52:40 +0900

    remove comment out codes

--- taskt/Core/Automation/Commands/ProgramProcess/RunScriptCommand.cs
+++ taskt/Core/Automation/Commands/ProgramProcess/RunScriptCommand.cs
@@ -1,54 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Windows.Forms;
 using System.Xml.Serialization;
-using taskt.UI.CustomControls;
-using taskt.UI.Forms;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
 {
     [Serializable]
     [Attributes.ClassAttributes.Group("Programs/Process Commands")]
+    [Attributes.ClassAttributes.CommandSettings("Run Script")]
     [Attributes.ClassAttributes.Description("This command allows you to run a script or program and wait for it to exit before proceeding.")]
     [Attributes.ClassAttributes.UsesDescription("Use this command when you want to run a script (such as vbScript, javascript, or executable) but wait for it to close before taskt continues executing.")]
     [Attributes.ClassAttributes.ImplementationDescription("This command implements 'Process.Start' and waits for the script/program to exit before proceeding.")]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
     public class RunScriptCommand : ScriptCommand
     {
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Enter the path to the script (ex. C:\\temp\\myscript.vbs, {{{vScriptPath}}})")]
-        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowFileSelectionHelper)]
-        [Attributes.PropertyAttributes.InputSpecification("Enter a fully qualified path to the script, including the script extension.")]
-        [Attributes.PropertyAttributes.SampleUsage("**C:\\temp\\myscript.vbs** or **{{{vScriptPath}}}**")]
-        [Attributes.PropertyAttributes.Remarks("This command differs from **Start Process** because this command blocks execution until the script has completed.  If you do not want to stop while the script executes, consider using **Start Process** instead.If file does not contain extensin, supplement extensions supported by cmd.\nIf file does not contain folder path, file will be opened in the same folder as script file.")]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_DisallowNewLine_OneLineTextBox))]
+        [PropertyDescription("Path to the Batch Script File")]
+        [InputSpecification("Path", true)]
+        [PropertyDetailSampleUsage("**C:\\temp\\myscript.bat**", PropertyDetailSampleUsage.ValueType.Value, "Script File")]
+        [PropertyDetailSampleUsage("**C:\\temp\\myscript.vbs**", PropertyDetailSampleUsage.ValueType.Value, "Script File")]
+        [PropertyDetailSampleUsage("**C:\\temp\\myscript.js**", PropertyDetailSampleUsage.ValueType.Value, "Script File")]
+        [PropertyDetailSampleUsage("**{{{vScriptPath}}}**", PropertyDetailSampleUsage.ValueType.VariableValue, "Script File")]
+        [Remarks("This command differs from **Start Process** because this command blocks execution until the script has completed. If you do not want to stop while the script executes, consider using **Start Process** instead.\nIf file does not contain extensin, supplement ps1 or bat extension.\nIf file does not contain folder path, file will be opened in the same folder as script file.")]
+        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowFileSelectionHelper)]
+        [PropertyValidationRule("Path", PropertyValidationRule.ValidationRuleFlags.Empty)]
+        [PropertyDisplayText(true, "Path")]
         public string v_ScriptPath { get; set; }
 
         public RunScriptCommand()
         {
-            this.CommandName = "RunScriptCommand";
-            this.SelectionName = "Run Script";
-            this.CommandEnabled = true;
-            this.CustomRendering = true;
+            //this.CommandName = "RunScriptCommand";
+            //this.SelectionName = "Run Script";
+            //this.CommandEnabled = true;
+            //this.CustomRendering = true;
         }
 
         public override void RunCommand(object sender)
         {
             var engine = (Engine.AutomationEngineInstance)sender;
 
-            //var scriptPath = v_ScriptPath.ConvertToUserVariable(sender);
-            //scriptPath = Core.FilePathControls.formatFilePath(scriptPath, (Engine.AutomationEngineInstance)sender);
-            //if (!System.IO.File.Exists(scriptPath) && !Core.FilePathControls.hasExtension(scriptPath))
-            //{
-            //    string[] exts = new string[] { ".bat", ".vbs", ".js", ".wsf" };
-            //    foreach(string ext in exts)
-            //    {
-            //        if (System.IO.File.Exists(scriptPath + ext))
-            //        {
-            //            scriptPath += ext;
-            //            break;
-            //        }
-            //    }
-            //}
             string scriptPath = FilePathControls.formatFilePath_NoFileCounter(v_ScriptPath, engine, new List<string>() { "bat", "vbs", "js", "wsf" }, true);
 
             System.Diagnostics.Process scriptProc = new System.Diagnostics.Process();
@@ -58,33 +50,6 @@ namespace taskt.Core.Automation.Commands
             scriptProc.WaitForExit();
 
             scriptProc.Close();
-        }
-
-        public override List<Control> Render(frmCommandEditor editor)
-        {
-            base.Render(editor);
-
-            RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_ScriptPath", this, editor));
-
-            return RenderedControls;
-        }
-
-        public override string GetDisplayValue()
-        {
-            return base.GetDisplayValue() + " [Script Path: " + v_ScriptPath + "]";
-        }
-
-        public override bool IsValidate(frmCommandEditor editor)
-        {
-            base.IsValidate(editor);
-
-            if (String.IsNullOrEmpty(this.v_ScriptPath))
-            {
-                this.validationResult += "Script is empty.\n";
-                this.IsValid = false;
-            }
-
-            return this.IsValid;
         }
     }
 }
