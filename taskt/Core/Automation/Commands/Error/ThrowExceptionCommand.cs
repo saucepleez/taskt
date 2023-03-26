ff022fc47749e0e7fe7f3c3b50dc0fdb086b2052
rcktrncn <rcktrncn@hotmail.com>
2023-03-26 16:11:13 +0900

    add message parameter, add attribute

--- taskt/Core/Automation/Commands/Error/ThrowExceptionCommand.cs
+++ taskt/Core/Automation/Commands/Error/ThrowExceptionCommand.cs
@@ -1,42 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using taskt.UI.CustomControls;
-using taskt.UI.Forms;
+using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
 {
     [Serializable]
     [Attributes.ClassAttributes.Group("Error Handling Commands")]
+    [Attributes.ClassAttributes.CommandSettings("Throw Exception")]
     [Attributes.ClassAttributes.Description("This command allows you to throw an exception error.")]
     [Attributes.ClassAttributes.UsesDescription("Use this command when you want to throw an exception error")]
     [Attributes.ClassAttributes.ImplementationDescription("")]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
     public class ThrowExceptionCommand : ScriptCommand
     {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_MultiLinesTextBox))]
+        [PropertyDescription("Exception Message")]
+        [InputSpecification("Message", true)]
+        [PropertyIsOptional(true)]
+        [PropertyValidationRule("Message", PropertyValidationRule.ValidationRuleFlags.None)]
+        [PropertyDisplayText(true, "Message")]
+        public string v_Message { get; set; }
+
         public string ErrorMessage { get; set; }
         public string StackTrace { get; set; }
 
         public ThrowExceptionCommand()
         {
-            this.CommandName = "ThrowExceptionCommand";
-            this.SelectionName = "Throw Exception";
-            this.CommandEnabled = true;
-            this.CustomRendering = true;
+            //this.CommandName = "ThrowExceptionCommand";
+            //this.SelectionName = "Throw Exception";
+            //this.CommandEnabled = true;
+            //this.CustomRendering = true;
         }
+
         public override void RunCommand(object sender)
         {
-            throw new System.Exception();
+            var engine = (Engine.AutomationEngineInstance)sender;
+
+            string message;
+            if (String.IsNullOrEmpty(v_Message))
+            {
+                message = "A Error has Occured.";
+            }
+            else
+            {
+                message = v_Message.ConvertToUserVariable(engine);
+            }
+
+            throw new Exception(message);
         }
-        public override List<Control> Render(frmCommandEditor editor)
-        {
-            base.Render(editor);
-            //RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_Comment", this, editor));
-            RenderedControls.AddRange(CommandControls.MultiCreateInferenceDefaultControlGroupFor(new List<string>() { nameof(v_Comment) }, this, editor));
-            return RenderedControls;
-        }
-        public override string GetDisplayValue()
-        {
-            return base.GetDisplayValue();
-        }
+
+        //public override List<Control> Render(frmCommandEditor editor)
+        //{
+        //    base.Render(editor);
+        //    //RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_Comment", this, editor));
+        //    RenderedControls.AddRange(CommandControls.MultiCreateInferenceDefaultControlGroupFor(new List<string>() { nameof(v_Comment) }, this, editor));
+        //    return RenderedControls;
+        //}
+        //public override string GetDisplayValue()
+        //{
+        //    return base.GetDisplayValue();
+        //}
     }
 }
