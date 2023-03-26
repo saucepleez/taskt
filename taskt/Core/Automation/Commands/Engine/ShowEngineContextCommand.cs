fc0f9bffc3dea9d7cf7823d865204bfd2e4759f0
rcktrncn <rcktrncn@hotmail.com>
2023-03-26 15:35:51 +0900

    use virtualproperty, new method

--- taskt/Core/Automation/Commands/Engine/ShowEngineContextCommand.cs
+++ taskt/Core/Automation/Commands/Engine/ShowEngineContextCommand.cs
@@ -1,56 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Xml.Serialization;
-using taskt.UI.CustomControls;
-using taskt.UI.Forms;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
 {
     [Serializable]
     [Attributes.ClassAttributes.Group("Engine Commands")]
+    [Attributes.ClassAttributes.CommandSettings("Show Engine Context")]
     [Attributes.ClassAttributes.Description("This command allows you to show a message to the user.")]
     [Attributes.ClassAttributes.UsesDescription("Use this command when you want to present or display a value on screen to the user.")]
     [Attributes.ClassAttributes.ImplementationDescription("This command implements 'MessageBox' and invokes VariableCommand to find variable data.")]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
     public class ShowEngineContextCommand : ScriptCommand
     {
-
         [XmlAttribute]
-        [Attributes.PropertyAttributes.PropertyDescription("Close After X (Seconds) - 0 to bypass")]
-        [Attributes.PropertyAttributes.InputSpecification("Specify how many seconds to display on screen. After the amount of seconds passes, the message box will be automatically closed and script will resume execution.")]
-        [Attributes.PropertyAttributes.SampleUsage("**0** to remain open indefinitely or **5** to stay open for 5 seconds.")]
-        [Attributes.PropertyAttributes.Remarks("")]
-        [Attributes.PropertyAttributes.PropertyUIHelper(Attributes.PropertyAttributes.PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_DisallowNewLine_OneLineTextBox))]
+        [PropertyDescription("Close After X Seconds - 0 to bypass")]
+        [InputSpecification("Close Time", true)]
+        [PropertyDetailSampleUsage("**0**", "Show Indefinitely")]
+        [PropertyDetailSampleUsage("**5**", PropertyDetailSampleUsage.ValueType.Value, "Close Time")]
+        [PropertyDetailSampleUsage("**{{{vTime}}}**", PropertyDetailSampleUsage.ValueType.VariableValue, "Close Time")]
+        [PropertyValidationRule("Close Time", PropertyValidationRule.ValidationRuleFlags.LessThanZero)]
+        [PropertyFirstValue("0")]
+        [PropertyIsOptional(true, "0")]
+        [PropertyDisplayText(true, "Close After", "Seconds")]
         public string v_AutoCloseAfter { get; set; }
+
         public ShowEngineContextCommand()
         {
-            this.CommandName = "ShowEngineContextCommand";
-            this.SelectionName = "Show Engine Context";
-            this.CommandEnabled = true;
-            this.v_AutoCloseAfter = "0";
-            this.CustomRendering = true;
+            //this.CommandName = "ShowEngineContextCommand";
+            //this.SelectionName = "Show Engine Context";
+            //this.CommandEnabled = true;
+            //this.v_AutoCloseAfter = "0";
+            //this.CustomRendering = true;
         }
 
         public override void RunCommand(object sender)
         {
-            var engine = (Core.Automation.Engine.AutomationEngineInstance)sender;
+            var engine = (Engine.AutomationEngineInstance)sender;
 
+            //if (engine.tasktEngineUI == null)
+            //{           
+            //    return;
+            //}
 
-            if (engine.tasktEngineUI == null)
-            {           
-                return;
-            }
+            //v_AutoCloseAfter = v_AutoCloseAfter.ConvertToUserVariable(sender);
+            //int closeValue;
+            //if (!int.TryParse(v_AutoCloseAfter, out closeValue))
+            //{
+            //    closeValue = 10;
+            //}
 
-            v_AutoCloseAfter = v_AutoCloseAfter.ConvertToUserVariable(sender);
-            int closeValue;
-            if (!int.TryParse(v_AutoCloseAfter, out closeValue))
-            {
-                closeValue = 10;
-            }
+            var closeValue = this.ConvertToUserVariableAsInteger(nameof(v_AutoCloseAfter), engine);
 
             //automatically close messageboxes for server requests
             if (engine.serverExecution && closeValue <= 0)
@@ -63,51 +65,50 @@ namespace taskt.Core.Automation.Commands
                     engine.tasktEngineUI.ShowEngineContext(engine.GetEngineContext(), closeValue);
                 }
             ));
-
-        }
-        public override List<Control> Render(frmCommandEditor editor)
-        {
-            base.Render(editor);
-
-
-            //create auto close control set
-            var autocloseControlSet = CommandControls.CreateDefaultInputGroupFor("v_AutoCloseAfter", this, editor);
-            RenderedControls.AddRange(autocloseControlSet);
-
-
-            return RenderedControls;
         }
 
-        public override string GetDisplayValue()
-        {
-            return base.GetDisplayValue();
-        }
+        //public override List<Control> Render(frmCommandEditor editor)
+        //{
+        //    base.Render(editor);
 
-        public override bool IsValidate(frmCommandEditor editor)
-        {
-            this.IsValid = true;
-            this.validationResult = "";
 
-            if (String.IsNullOrEmpty(v_AutoCloseAfter))
-            {
-                this.validationResult += "Close time is empty.\n";
-                this.IsValid = true;
-            }
-            else
-            {
-                int v;
-                if (!int.TryParse(v_AutoCloseAfter, out v))
-                {
-                    if (v < 0)
-                    {
-                        this.validationResult += "Close time less than zero.\n";
-                        this.IsValid = true;
-                    }
-                }
-            }
+        //    //create auto close control set
+        //    var autocloseControlSet = CommandControls.CreateDefaultInputGroupFor("v_AutoCloseAfter", this, editor);
+        //    RenderedControls.AddRange(autocloseControlSet);
 
-            return this.IsValid;
-        }
 
+        //    return RenderedControls;
+        //}
+
+        //public override string GetDisplayValue()
+        //{
+        //    return base.GetDisplayValue();
+        //}
+
+        //public override bool IsValidate(frmCommandEditor editor)
+        //{
+        //    this.IsValid = true;
+        //    this.validationResult = "";
+
+        //    if (String.IsNullOrEmpty(v_AutoCloseAfter))
+        //    {
+        //        this.validationResult += "Close time is empty.\n";
+        //        this.IsValid = true;
+        //    }
+        //    else
+        //    {
+        //        int v;
+        //        if (!int.TryParse(v_AutoCloseAfter, out v))
+        //        {
+        //            if (v < 0)
+        //            {
+        //                this.validationResult += "Close time less than zero.\n";
+        //                this.IsValid = true;
+        //            }
+        //        }
+        //    }
+
+        //    return this.IsValid;
+        //}
     }
 }
