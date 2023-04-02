2ff957e7b92674491ae6655ca018d5fbb3123a54
rcktrncn <rcktrncn@hotmail.com>
2023-04-02 15:36:49 +0900

    v3.5.1.38

--- taskt/Properties/AssemblyInfo.cs
+++ taskt/Properties/AssemblyInfo.cs
@@ -45,6 +45,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.0.*")]
-[assembly: AssemblyFileVersion("3.5.1.37")]
+[assembly: AssemblyFileVersion("3.5.1.38")]
 //[assembly: AssemblyVersion("0.0.0.2")]
 //[assembly: AssemblyFileVersion("0.0.0.2")]
