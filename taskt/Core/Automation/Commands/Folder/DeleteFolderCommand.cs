c538bc8f2ec5aca4fa482d8ff274bb68d02da70c
rcktrncn <rcktrncn@hotmail.com>
2023-04-02 20:17:09 +0900

    use virtualproperty, new methods

--- taskt/Core/Automation/Commands/Folder/DeleteFolderCommand.cs
+++ taskt/Core/Automation/Commands/Folder/DeleteFolderCommand.cs
@@ -7,6 +7,7 @@ namespace taskt.Core.Automation.Commands
 
     [Serializable]
     [Attributes.ClassAttributes.Group("Folder Operation Commands")]
+    [Attributes.ClassAttributes.CommandSettings("Delete Folder")]
     [Attributes.ClassAttributes.Description("This command deletes a folder from a specified destination")]
     [Attributes.ClassAttributes.UsesDescription("Use this command to delete a folder from a specific location.")]
     [Attributes.ClassAttributes.ImplementationDescription("This command implements '' to achieve automation.")]
@@ -15,30 +16,38 @@ namespace taskt.Core.Automation.Commands
     public class DeleteFolderCommand : ScriptCommand
     {
         [XmlAttribute]
-        [PropertyDescription("Please indicate the path to the source folder")]
-        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowFolderSelectionHelper)]
-        [InputSpecification("Enter or Select the path to the folder.")]
-        [SampleUsage("**C:\\temp\\myfolder** or **{{{vTextFolderPath}}}**")]
-        [Remarks("")]
-        [PropertyShowSampleUsageInDescription(true)]
-        [PropertyTextBoxSetting(1, false)]
-        [PropertyValidationRule("Folder Path", PropertyValidationRule.ValidationRuleFlags.Empty)]
-        [PropertyDisplayText(true, "Folder")]
+        //[PropertyDescription("Please indicate the path to the source folder")]
+        //[PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        //[PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowFolderSelectionHelper)]
+        //[InputSpecification("Enter or Select the path to the folder.")]
+        //[SampleUsage("**C:\\temp\\myfolder** or **{{{vTextFolderPath}}}**")]
+        //[Remarks("")]
+        //[PropertyShowSampleUsageInDescription(true)]
+        //[PropertyTextBoxSetting(1, false)]
+        //[PropertyValidationRule("Folder Path", PropertyValidationRule.ValidationRuleFlags.Empty)]
+        //[PropertyDisplayText(true, "Folder")]
+        [PropertyVirtualProperty(nameof(FolderPathControls), nameof(FolderPathControls.v_FolderPath))]
         public string v_SourceFolderPath { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(FolderPathControls), nameof(FolderPathControls.v_WaitTime))]
+        public string v_WaitForFolder { get; set; }
 
         public DeleteFolderCommand()
         {
-            this.CommandName = "DeleteFolderCommand";
-            this.SelectionName = "Delete Folder";
-            this.CommandEnabled = true;
-            this.CustomRendering = true;
+            //this.CommandName = "DeleteFolderCommand";
+            //this.SelectionName = "Delete Folder";
+            //this.CommandEnabled = true;
+            //this.CustomRendering = true;
         }
 
         public override void RunCommand(object sender)
         {
+            var engine = (Engine.AutomationEngineInstance)sender;
+
             //apply variable logic
-            var sourceFolder = v_SourceFolderPath.ConvertToUserVariable(sender);
+            //var sourceFolder = v_SourceFolderPath.ConvertToUserVariable(sender);
+            var sourceFolder = FolderPathControls.WaitForFolder(this, nameof(v_SourceFolderPath), nameof(v_WaitForFolder), engine);
 
             //delete folder
             System.IO.Directory.Delete(sourceFolder, true);
