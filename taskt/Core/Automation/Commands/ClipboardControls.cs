1e8da6c055a32439f43975766df27fa10188ac03
rcktrncn <rcktrncn@hotmail.com>
2023-04-29 18:50:42 +0900

    move clipboard methods to ClipbardControls

---
+++ taskt/Core/Automation/Commands/ClipboardControls.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for clipboard methods
+    /// </summary>
+    static internal class ClipboardControls
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetClipboardData(uint uFormat);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetClipboardData(uint uFormat, IntPtr data);
+
+        [DllImport("user32.dll")]
+        private static extern bool IsClipboardFormatAvailable(uint format);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool OpenClipboard(IntPtr hWndNewOwner);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool CloseClipboard();
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GlobalLock(IntPtr hMem);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool GlobalUnlock(IntPtr hMem);
+
+        private const uint CF_UNICODETEXT = 13;
+
+        public static void SetClipboardText(string textToSet)
+        {
+            OpenClipboard(IntPtr.Zero);
+            var ptr = Marshal.StringToHGlobalUni(textToSet);
+            SetClipboardData(13, ptr);
+            CloseClipboard();
+        }
+
+        public static string GetClipboardText()
+        {
+            if (!IsClipboardFormatAvailable(CF_UNICODETEXT))
+            {
+                return null;
+            }
+                
+            if (!OpenClipboard(IntPtr.Zero))
+            {
+                return null;
+            }
+
+            string data = null;
+            var hGlobal = GetClipboardData(CF_UNICODETEXT);
+            if (hGlobal != IntPtr.Zero)
+            {
+                var lpwcstr = GlobalLock(hGlobal);
+                if (lpwcstr != IntPtr.Zero)
+                {
+                    data = Marshal.PtrToStringUni(lpwcstr);
+                    GlobalUnlock(lpwcstr);
+                }
+            }
+            CloseClipboard();
+
+            return data;
+        }
+    }
+}
--- taskt/Core/Automation/User32/User32Functionas.cs
+++ taskt/Core/Automation/User32/User32Functionas.cs
@@ -157,53 +157,6 @@ namespace taskt.Core.Automation.User32
             public int left, top, right, bottom;
         }
 
-        
-        [DllImport("user32.dll")]
-        static extern IntPtr GetClipboardData(uint uFormat);
-        [DllImport("user32.dll")]
-        internal static extern bool SetClipboardData(uint uFormat, IntPtr data);
-        [DllImport("user32.dll")]
-        static extern bool IsClipboardFormatAvailable(uint format);
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern bool OpenClipboard(IntPtr hWndNewOwner);
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern bool CloseClipboard();
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GlobalLock(IntPtr hMem);
-        [DllImport("kernel32.dll")]
-        static extern bool GlobalUnlock(IntPtr hMem);
-
-        const uint CF_UNICODETEXT = 13;
-        public static void SetClipboardText(string textToSet)
-        {
-            OpenClipboard(IntPtr.Zero);
-            var ptr = Marshal.StringToHGlobalUni(textToSet);
-            SetClipboardData(13, ptr);
-            CloseClipboard();
-        }
-        public static string GetClipboardText()
-        {
-            if (!IsClipboardFormatAvailable(CF_UNICODETEXT))
-                return null;
-            if (!OpenClipboard(IntPtr.Zero))
-                return null;
-
-            string data = null;
-            var hGlobal = GetClipboardData(CF_UNICODETEXT);
-            if (hGlobal != IntPtr.Zero)
-            {
-                var lpwcstr = GlobalLock(hGlobal);
-                if (lpwcstr != IntPtr.Zero)
-                {
-                    data = Marshal.PtrToStringUni(lpwcstr);
-                    GlobalUnlock(lpwcstr);
-                }
-            }
-            CloseClipboard();
-
-            return data;
-        }
-
         private delegate bool EnumWindowProc(IntPtr hwnd, IntPtr lParam);
 
         [DllImport("user32")]
