eacb556523232e5a47cd5f8239dc7a657442c1a6
rcktrncn <rcktrncn@hotmail.com>
2023-04-29 18:40:36 +0900

    remove using

--- taskt/Core/Automation/User32/User32Functionas.cs
+++ taskt/Core/Automation/User32/User32Functionas.cs
@@ -13,15 +13,10 @@
 //limitations under the License.
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Serialization;
 using taskt.Core.Automation.Commands;
 
 namespace taskt.Core.Automation.User32
