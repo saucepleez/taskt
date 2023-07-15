380933290c6fe976a76d7ec11522ba5a2e15cee1
rcktrncn <rcktrncn@hotmail.com>
2023-07-15 20:08:08 +0900

    change subgroup name

--- taskt/Core/Automation/Commands/UIAutomation/UIAutomationUIElementActionCommand.cs
+++ taskt/Core/Automation/Commands/UIAutomation/UIAutomationUIElementActionCommand.cs
@@ -11,7 +11,7 @@ namespace taskt.Core.Automation.Commands
 
     [Serializable]
     [Attributes.ClassAttributes.Group("UIAutomation Commands")]
-    [Attributes.ClassAttributes.SubGruop("Element Action")]
+    [Attributes.ClassAttributes.SubGruop("UIElement Action")]
     [Attributes.ClassAttributes.CommandSettings("UIElement Action")]
     [Attributes.ClassAttributes.Description("Combined implementation of the ThickAppClick/GetText command but includes an advanced Window Recorder to record the required element.")]
     [Attributes.ClassAttributes.ImplementationDescription("This command implements 'Windows UI Automation' to find elements and invokes a Variable Command to assign data and achieve automation")]
