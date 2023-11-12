da4d9e926372763945d7fe90c7c1ec082287cddd
rcktrncn <rcktrncn@hotmail.com>
2023-11-12 16:20:27 +0900

    add new commands

---
+++ taskt/Core/Automation/Commands/Math/GetArccosCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    [Serializable]
+    [Attributes.ClassAttributes.Group("Math Commands")]
+    [Attributes.ClassAttributes.CommandSettings("Get Arcsin")]
+    [Attributes.ClassAttributes.Description("This command allows you to get arccos.")]
+    [Attributes.ClassAttributes.UsesDescription("Use this command when you want to get arccos.")]
+    [Attributes.ClassAttributes.ImplementationDescription("")]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    public class GetArccosCommand : ScriptCommand
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(NumberControls), nameof(NumberControls.v_Value))]
+        public string v_Value { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_Result))]
+        public string v_Result { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(MathControls), nameof(MathControls.v_AngleType))]
+        public string v_AngleType { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(MathControls), nameof(MathControls.v_WhenValueIsOutOfRange))]
+        public string v_WhenValueIsOutOfRange { get; set; }
+
+        public GetArccosCommand()
+        {
+        }
+
+        public override void RunCommand(Engine.AutomationEngineInstance engine)
+        {
+            MathControls.InverseTrignometicFunctionAction(this, nameof(v_Value), nameof(v_Result), nameof(v_AngleType), nameof(v_WhenValueIsOutOfRange),
+                Math.Acos, new Func<double, bool>(v =>
+                {
+                    return (v >= -1 && v <= 1);
+                }), engine);
+        }
+    }
+}
---
+++ taskt/Core/Automation/Commands/Math/GetArctanCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    [Serializable]
+    [Attributes.ClassAttributes.Group("Math Commands")]
+    [Attributes.ClassAttributes.CommandSettings("Get Arctan")]
+    [Attributes.ClassAttributes.Description("This command allows you to get arctan.")]
+    [Attributes.ClassAttributes.UsesDescription("Use this command when you want to get arctan.")]
+    [Attributes.ClassAttributes.ImplementationDescription("")]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    public class GetArctanCommand : ScriptCommand
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(NumberControls), nameof(NumberControls.v_Value))]
+        public string v_Value { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_Result))]
+        public string v_Result { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(MathControls), nameof(MathControls.v_AngleType))]
+        public string v_AngleType { get; set; }
+
+        public GetArctanCommand()
+        {
+        }
+
+        public override void RunCommand(Engine.AutomationEngineInstance engine)
+        {
+            var v = (double)this.ExpandValueOrUserVariableAsDecimal(nameof(v_Value), engine);
+
+            var r = Math.Atan(v);
+            switch(this.ExpandValueOrUserVariableAsSelectionItem(nameof(v_AngleType), engine))
+            {
+                case "radian":
+                    r.StoreInUserVariable(engine, v_Result);
+                    break;
+                case "degree":
+                    (r * 180.0 / Math.PI).StoreInUserVariable(engine, v_Result);
+                    break;
+            }
+        }
+    }
+}
