475976b90a65ad9819c04eaec3d4b8ad124481b0
rcktrncn <rcktrncn@hotmail.com>
2023-11-12 19:06:29 +0900

    add new command

---
+++ taskt/Core/Automation/Commands/Window/GetWindowSizeCommand.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using taskt.UI.Forms;
+using taskt.UI.CustomControls;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    [Serializable]
+    [Attributes.ClassAttributes.Group("Window Commands")]
+    [Attributes.ClassAttributes.SubGruop("Window State")]
+    [Attributes.ClassAttributes.CommandSettings("Get Window Size")]
+    [Attributes.ClassAttributes.Description("This command returns window size.")]
+    [Attributes.ClassAttributes.UsesDescription("Use this command when you want window size.")]
+    [Attributes.ClassAttributes.ImplementationDescription("")]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    public class GetWindowSizeCommand : ScriptCommand
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WindowName))]
+        public string v_WindowName { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_CompareMethod))]
+        public string v_SearchMethod { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_Result))]
+        [PropertyDescription("Variable Name to Recieve the Window Width")]
+        [PropertyIsOptional(true)]
+        [PropertyDisplayText(false, "")]
+        public string v_With { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_Result))]
+        [PropertyDescription("Variable Name to Recieve the Window Height")]
+        [PropertyIsOptional(true)]
+        [PropertyDisplayText(false, "")]
+        public string v_Height { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_MatchMethod_Single))]
+        [PropertySelectionChangeEvent(nameof(MatchMethodComboBox_SelectionChangeCommitted))]
+        public string v_MatchMethod { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_TargetWindowIndex))]
+        public string v_TargetWindowIndex { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WaitTime))]
+        public string v_WaitTime { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WindowNameResult))]
+        public string v_NameResult { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WindowHandleResult))]
+        public string v_HandleResult { get; set; }
+
+        public GetWindowSizeCommand()
+        {
+        }
+        public override void RunCommand(Engine.AutomationEngineInstance engine)
+        {
+            WindowNameControls.WindowAction(this, engine,
+                new Action<System.Collections.Generic.List<(IntPtr, string)>>(wins =>
+                {
+                    var whnd = wins[0].Item1;
+
+                    var rct = WindowNameControls.GetWindowRect(whnd);
+
+                    if (!string.IsNullOrEmpty(v_With))
+                    {
+                        (rct.right - rct.left).StoreInUserVariable(engine, v_With);
+                    }
+                    if (!string.IsNullOrEmpty(v_Height))
+                    {
+                        (rct.bottom - rct.top).StoreInUserVariable(engine, v_Height);
+                    }
+                })
+            );
+        }
+
+        public override void Refresh(frmCommandEditor editor)
+        {
+            base.Refresh();
+            ControlsList.GetPropertyControl<ComboBox>(nameof(v_WindowName)).AddWindowNames();
+        }
+
+        private void MatchMethodComboBox_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            WindowNameControls.MatchMethodComboBox_SelectionChangeCommitted(ControlsList, (ComboBox)sender, nameof(v_TargetWindowIndex));
+        }
+    }
+}
