645ed1fbb0dfe2842d55918a2da65f6f6327495b
rcktrncn <rcktrncn@hotmail.com>
2023-11-27 22:42:29 +0900

    add new command

---
+++ taskt/Core/Automation/Commands/Window/GetWindowHandleFromWindowName.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using taskt.UI.Forms;
+using taskt.UI.CustomControls;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    [Serializable]
+    [Attributes.ClassAttributes.Group("Window Commands")]
+    [Attributes.ClassAttributes.SubGruop("Window Actions")]
+    [Attributes.ClassAttributes.CommandSettings("Get Window Handle From Window Name")]
+    [Attributes.ClassAttributes.Description("This command allows you to Get Window Handle from Window Name.")]
+    [Attributes.ClassAttributes.UsesDescription("Use this command when you want to Get Window Handle from Window Name.")]
+    [Attributes.ClassAttributes.ImplementationDescription("")]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    public class GetWindowHandleFromWindowNameCommand : ScriptCommand
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WindowName))]
+        public string v_WindowName { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_CompareMethod))]
+        public string v_SearchMethod { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_MatchMethod))]
+        [PropertyUISelectionOption("First")]
+        [PropertyUISelectionOption("Last")]
+        [PropertyUISelectionOption("Index")]
+        [PropertySelectionChangeEvent(nameof(MatchMethodComboBox_SelectionChangeCommitted))]
+        public string v_MatchMethod { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_TargetWindowIndex))]
+        public string v_TargetWindowIndex { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_OutputWindowHandle))]
+        [Remarks("")]
+        [PropertyIsOptional(false)]
+        [PropertyValidationRule("Window Handle", PropertyValidationRule.ValidationRuleFlags.Empty)]
+        public string v_WindowHandle { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WaitTime))]
+        public string v_WaitTime { get; set; }
+
+        public GetWindowHandleFromWindowNameCommand()
+        {
+        }
+
+        public override void RunCommand(Engine.AutomationEngineInstance engine)
+        {
+            WindowNameControls.WindowAction( this, engine,
+                new Action<List<(IntPtr, string)>>(wins =>
+                {
+                    wins[0].Item1.StoreInUserVariable(engine, v_WindowHandle);
+                })
+            );
+        }
+
+        private void MatchMethodComboBox_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            WindowNameControls.MatchMethodComboBox_SelectionChangeCommitted(ControlsList, (ComboBox)sender, nameof(v_TargetWindowIndex));
+        }
+
+        public override void Refresh(frmCommandEditor editor)
+        {
+            ControlsList.GetPropertyControl<ComboBox>(nameof(v_WindowName)).AddWindowNames();
+        }
+    }
+}
