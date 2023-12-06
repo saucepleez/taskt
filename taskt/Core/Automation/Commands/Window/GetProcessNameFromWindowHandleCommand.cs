2edf9dc0c2f517f42ebe943c937cf4ccf1fb841c
rcktrncn <rcktrncn@hotmail.com>
2023-12-06 22:16:08 +0900

    add new commands

---
+++ taskt/Core/Automation/Commands/Window/GetProcessNameFromWindowHandleCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    [Serializable]
+    [Attributes.ClassAttributes.Group("Window Commands")]
+    [Attributes.ClassAttributes.SubGruop("Get From Window Handle")]
+    [Attributes.ClassAttributes.CommandSettings("Get Process Name From Window Handle")]
+    [Attributes.ClassAttributes.Description("This command returns process name.")]
+    [Attributes.ClassAttributes.UsesDescription("Use this command when you want to get process name.")]
+    [Attributes.ClassAttributes.ImplementationDescription("")]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    public class GetProcessNameFromWindowHandleCommand : ScriptCommand
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_InputWindowHandle))]
+        public string v_WindowHandle { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_Result))]
+        [PropertyDescription("Variable Name to Store Process Name")]
+        public string v_Result { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WaitTime))]
+        [PropertyIsOptional(true, "0")]
+        [PropertyFirstValue("0")]
+        public string v_WaitTime { get; set; }
+
+        public GetProcessNameFromWindowHandleCommand()
+        {
+        }
+
+        public override void RunCommand(Engine.AutomationEngineInstance engine)
+        {
+            WindowNameControls.WindowHandleAction(this, engine,
+                new Action<IntPtr>(whnd =>
+                {
+                    var proc = Process.GetProcesses().Where(p => (p.MainWindowHandle == whnd)).First();
+                    proc.ProcessName.StoreInUserVariable(engine, v_Result);
+                })
+            );
+        }
+    }
+}
