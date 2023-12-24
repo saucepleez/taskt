2627c70eb2ab61c1853e08fa76260c020e8d3970
rcktrncn <rcktrncn@hotmail.com>
2023-12-24 20:22:31 +0900

    change to subclass of AMathValueResultCommand

--- taskt/Core/Automation/Commands/Math/ATrignometricCommand.cs
+++ taskt/Core/Automation/Commands/Math/ATrignometricCommand.cs
@@ -8,18 +8,19 @@ namespace taskt.Core.Automation.Commands
     /// Abstract Class for Trignometric Commands
     /// </summary>
     [Serializable]
-    public abstract class ATrignometricCommand : ScriptCommand
+    public abstract class ATrignometricCommand : AMathValueResultCommand
     {
-        [XmlAttribute]
-        [PropertyVirtualProperty(nameof(NumberControls), nameof(NumberControls.v_Value))]
-        public string v_Value { get; set; }
+        //[XmlAttribute]
+        //[PropertyVirtualProperty(nameof(NumberControls), nameof(NumberControls.v_Value))]
+        //public string v_Value { get; set; }
 
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(MathControls), nameof(MathControls.v_AngleType))]
+        [PropertyParameterOrder(5500)]
         public string v_AngleType { get; set; }
 
-        [XmlAttribute]
-        [PropertyVirtualProperty(nameof(NumberControls), nameof(NumberControls.v_OutputNumericalVariableName))]
-        public string v_Result { get; set; }
+        //[XmlAttribute]
+        //[PropertyVirtualProperty(nameof(NumberControls), nameof(NumberControls.v_OutputNumericalVariableName))]
+        //public string v_Result { get; set; }
     }
 }
