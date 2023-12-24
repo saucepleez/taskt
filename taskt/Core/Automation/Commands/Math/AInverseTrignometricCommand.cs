cc8ec11478958cc7cca4539a88f157a9244cc56d
rcktrncn <rcktrncn@hotmail.com>
2023-12-24 21:07:23 +0900

    add AInverseTrignometricCommand

---
+++ taskt/Core/Automation/Commands/Math/AInverseTrignometricCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// abstract class for Inverse Trignometric Commands
+    /// </summary>
+    [Serializable]
+    public class AInverseTrignometricCommand : AMathValueResultCommand
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(MathControls), nameof(MathControls.v_AngleType))]
+        [PropertyParameterOrder(7000)]
+        public string v_AngleType { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(MathControls), nameof(MathControls.v_WhenValueIsOutOfRange))]
+        [PropertyParameterOrder(8000)]
+        public string v_WhenValueIsOutOfRange { get; set; }
+    }
+}
