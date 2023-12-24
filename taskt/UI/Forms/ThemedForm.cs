429bf3c140e89a5e2bb5e49c244014823c041bb9
rcktrncn <rcktrncn@hotmail.com>
2023-12-24 12:30:10 +0900

    remove using

--- taskt/UI/Forms/ThemedForm.cs
+++ taskt/UI/Forms/ThemedForm.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace taskt.UI.Forms
