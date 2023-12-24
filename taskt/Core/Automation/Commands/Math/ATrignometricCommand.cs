cf187994daf99dcd2bc77b968ee4d51cf60b6fb3
rcktrncn <rcktrncn@hotmail.com>
2023-12-24 19:54:48 +0900

    change to subclass of ATRignometricCommand

---
+++ taskt/Core/Automation/Commands/Math/ATrignometricCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// Abstract Class for Trignometric Commands
+    /// </summary>
+    [Serializable]
+    public abstract class ATrignometricCommand : ScriptCommand
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(NumberControls), nameof(NumberControls.v_Value))]
+        public string v_Value { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(MathControls), nameof(MathControls.v_AngleType))]
+        public string v_AngleType { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(NumberControls), nameof(NumberControls.v_OutputNumericalVariableName))]
+        public string v_Result { get; set; }
+    }
+}
