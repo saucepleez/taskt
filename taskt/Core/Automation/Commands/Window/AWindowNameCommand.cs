006f67ea36d50462e80cc674fe71e31af823f775
rcktrncn <rcktrncn@hotmail.com>
2023-12-29 21:57:55 +0900

    add parameter order

--- taskt/Core/Automation/Commands/Window/AWindowNameCommand.cs
+++ taskt/Core/Automation/Commands/Window/AWindowNameCommand.cs
@@ -14,31 +14,38 @@ namespace taskt.Core.Automation.Commands
     {
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WindowName))]
+        [PropertyParameterOrder(5000)]
         public string v_WindowName { get; set; }
 
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_CompareMethod))]
+        [PropertyParameterOrder(6000)]
         public string v_SearchMethod { get; set; }
 
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_MatchMethod))]
         [PropertySelectionChangeEvent(nameof(MatchMethodComboBox_SelectionChangeCommitted))]
+        [PropertyParameterOrder(7000)]
         public string v_MatchMethod { get; set; }
 
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_TargetWindowIndex))]
+        [PropertyParameterOrder(7100)]
         public string v_TargetWindowIndex { get; set; }
 
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WaitTime))]
+        [PropertyParameterOrder(7200)]
         public string v_WaitTime { get; set; }
 
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WindowNameResult))]
+        [PropertyParameterOrder(7300)]
         public string v_NameResult { get; set; }
 
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_OutputWindowHandle))]
+        [PropertyParameterOrder(7400)]
         public string v_HandleResult { get; set; }
 
         public AWindowCommandCommand()
