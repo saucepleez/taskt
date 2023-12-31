028eebd089e67bc86024399ce063cfbcaade3125
rcktrncn <rcktrncn@hotmail.com>
2023-12-31 21:17:45 +0900

    remove empty line, using

--- taskt/Core/Client.cs
+++ taskt/Core/Client.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace taskt.Core
 {
     public static class Client
     {
-
-
         private static bool engineBusy;
         public static bool EngineBusy
         {
