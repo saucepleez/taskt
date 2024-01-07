d21b302a1314f83c9b198dc56c7bc7d947aafce2
rcktrncn <rcktrncn@hotmail.com>
2024-01-07 13:59:59 +0900

    add interfaces

---
+++ taskt/Core/Automation/Commands/Window/IAnyWindowNameProperties.cs
@@ -0,0 +1,33 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// any window name commands properties
+    /// </summary>
+    public interface IAnyWindowNameProperties
+    {
+        /// <summary>
+        /// window name
+        /// </summary>
+        string v_WindowName { get; set; }
+
+        /// <summary>
+        /// search method (contains, starts-with, ...)
+        /// </summary>
+        string v_SearchMethod { get; set; }
+
+        /// <summary>
+        /// wait time for window
+        /// </summary>
+        string v_WaitTime { get; set; }
+
+        /// <summary>
+        /// found window name
+        /// </summary>
+        string v_NameResult { get; set; }
+
+        /// <summary>
+        /// found window handle
+        /// </summary>
+        string v_HandleResult { get; set; }
+    }
+}
---
+++ taskt/Core/Automation/Commands/Window/IWindowNameProperties.cs
@@ -0,0 +1,18 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// window name commands properties
+    /// </summary>
+    public interface IWindowNameProperties : IAnyWindowNameProperties
+    {
+        /// <summary>
+        /// match method (first, last, index)
+        /// </summary>
+        string v_MatchMethod { get; set; }
+
+        /// <summary>
+        /// match method index
+        /// </summary>
+        string v_TargetWindowIndex { get; set; }
+    }
+}
