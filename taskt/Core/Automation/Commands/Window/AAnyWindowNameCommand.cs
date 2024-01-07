6e40cb5725bece043c7c61c5dc3f8bef25aeb975
rcktrncn <rcktrncn@hotmail.com>
2024-01-07 16:21:58 +0900

    add AAnyWindowNameCommand

---
+++ taskt/Core/Automation/Commands/Window/AAnyWindowNameCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using taskt.UI.CustomControls;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// Abstract class for Any Window Name commands 
+    /// </summary>
+    [Serializable]
+    public abstract class AAnyWindowNameCommand : ScriptCommand, IAnyWindowNameProperties
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WindowName))]
+        [PropertyParameterOrder(5000)]
+        public string v_WindowName { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_CompareMethod))]
+        [PropertyParameterOrder(6000)]
+        public string v_SearchMethod { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WaitTime))]
+        [PropertyParameterOrder(8000)]
+        public abstract string v_WaitTime { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WindowNameResult))]
+        [PropertyParameterOrder(8100)]
+        public string v_NameResult { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_OutputWindowHandle))]
+        [PropertyParameterOrder(8200)]
+        public string v_HandleResult { get; set; }
+
+        public AAnyWindowNameCommand()
+        {
+        }
+
+        public override void Refresh(UI.Forms.ScriptBuilder.CommandEditor.frmCommandEditor editor)
+        {
+            ControlsList.GetPropertyControl<ComboBox>(nameof(v_WindowName)).AddWindowNames();
+        }
+    }
+}
