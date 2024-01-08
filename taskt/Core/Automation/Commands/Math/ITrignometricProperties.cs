eb4f536b1e7ab03549fbc07299f773af90bdfcf4
rcktrncn <rcktrncn@hotmail.com>
2024-01-08 18:38:43 +0900

    add ITrignometricProperties

---
+++ taskt/Core/Automation/Commands/Math/ITrignometricProperties.cs
@@ -0,0 +1,20 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    public interface ITrignometricProperties
+    {
+        /// <summary>
+        /// target value to convert
+        /// </summary>
+        string v_Value {  get; set; }
+
+        /// <summary>
+        /// agnle type Radian or Degree
+        /// </summary>
+        string v_AngleType { get; set; }
+
+        /// <summary>
+        /// result variable name
+        /// </summary>
+        string v_Result { get; set; }
+    }
+}
