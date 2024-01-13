9469a0db34d0e0dfe86f83a1a0fd3db36398f196
rcktrncn <rcktrncn@hotmail.com>
2024-01-13 22:38:10 +0900

    add nuget urls

--- taskt/Core/MyURLs.cs
+++ taskt/Core/MyURLs.cs
@@ -24,6 +24,28 @@
         public const string DateTimeFormatURL1 = "https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings";
         public const string DateTimeFormatURL2 = "https://docs.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings";
 
+        // NuGets, etc
+        public const string BouncyCastleURL = "https://www.bouncycastle.org/csharp/";
+        public const string HTMLAgilityPackURL = "https://www.nuget.org/packages/HtmlAgilityPack/";
+        public const string IMAPXURL = "https://github.com/azanov/imapx";
+        public const string JetBrainAnnotationsURL = "https://www.nuget.org/packages/JetBrains.Annotations/";
+        public const string Log4NetURL = "";    //?
+        public const string MailKitURL = "http://www.mimekit.net/";
+        public const string MarkddigURL = "https://github.com/xoofx/markdig";
+        public const string MimeKitURL = "http://www.mimekit.net/";
+        public const string NewtonSoftJSONURL = "https://www.nuget.org/packages/newtonsoft.json/";
+        public const string OneNoteOCRURL = "https://github.com/ignatandrei/OneNoteOCR";
+        public const string RestSharpURL = "https://restsharp.dev/";
+        public const string SeleniumURL = "https://github.com/SeleniumHQ/selenium";
+        public const string SelilogURL = "https://serilog.net/";
+        public const string SelilogFormattingCompactURL = "https://github.com/serilog/serilog-formatting-compact";
+        public const string SelilogSinksFileURL = "https://serilog.net/";
+        public const string SharpCompressURL = "https://github.com/adamhathcock/sharpcompress";
+        public const string SharpSimpleNLGURL = "https://github.com/nickhodge/SharpSimpleNLG";
+        public const string SuperSocketClientEngineURL = "https://github.com/kerryjiang/SuperSocket.ClientEngine";
+        public const string TaskSchedulerURL = "https://github.com/dahall/TaskScheduler";
+        public const string WebSocket4NetURL = "https://github.com/kerryjiang/WebSocket4Net";
+        public const string ZstdSharpPortURL = "https://github.com/oleg-st/ZstdSharp";
 
         public static string GetWikiURL(string commandName, string groupName)
         {
