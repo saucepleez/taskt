4ee2d97088f5a851135cbf9da1eef25b35523261
rcktrncn <rcktrncn@hotmail.com>
2024-02-03 14:57:31 +0900

    add RECT struct

---
+++ taskt/Core/Automation/Commands/_General/RECT.cs
@@ -0,0 +1,13 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// Rect Struct
+    /// </summary>
+    public struct RECT
+    {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+    }
+}
