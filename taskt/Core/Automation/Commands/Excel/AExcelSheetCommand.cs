b1679b4a493bd6c4de9472fe79cd3cb45d4a0c0b
rcktrncn <rcktrncn@hotmail.com>
2024-02-04 11:55:03 +0900

    change interface name

--- taskt/Core/Automation/Commands/Excel/AExcelSheetCommand.cs
+++ taskt/Core/Automation/Commands/Excel/AExcelSheetCommand.cs
@@ -6,7 +6,7 @@ namespace taskt.Core.Automation.Commands.Excel
     /// <summary>
     /// for commands that using Excel Sheet
     /// </summary>
-    public abstract class AExcelSheetCommand : AExcelInstanceCommand, IExcelSheetProperties
+    public abstract class AExcelSheetCommand : AExcelInstanceCommand, IExcelWorksheetProperties
     {
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(ExcelControls), nameof(ExcelControls.v_SheetName))]
