9390019e7aaf76c014c51179428bf51253e041ea
rcktrncn <rcktrncn@hotmail.com>
2024-02-15 21:30:40 +0900

    comment out method
    not used

--- taskt/Core/Automation/Commands/Excel/EM_ExcelRCLocationPropertiesExtensionMethods.cs
+++ taskt/Core/Automation/Commands/Excel/EM_ExcelRCLocationPropertiesExtensionMethods.cs
@@ -51,28 +51,28 @@ namespace taskt.Core.Automation.Commands
             }
         }
 
-        /// <summary>
-        /// expand value or variable as Excel Cell Row, Column, and Range
-        /// </summary>
-        /// <param name="command"></param>
-        /// <param name="engine"></param>
-        /// <returns></returns>
-        /// <exception cref="Exception"></exception>
-        public static (int row, int column, Range rg) ExpandValueOrVariableAsExcelCellRowAndCellColumnAndRange(this IExcelRCLocationProperties command, Engine.AutomationEngineInstance engine)
-        {
-            var row = ((ScriptCommand)command).ExpandValueOrUserVariableAsInteger(nameof(command.v_CellRow), "Cell Row", engine);
-            var column = ((ScriptCommand)command).ExpandValueOrUserVariableAsInteger(nameof(command.v_CellColumn), "Cell Column", engine);
+        ///// <summary>
+        ///// expand value or variable as Excel Cell Row, Column, and Range
+        ///// </summary>
+        ///// <param name="command"></param>
+        ///// <param name="engine"></param>
+        ///// <returns></returns>
+        ///// <exception cref="Exception"></exception>
+        //public static (int row, int column, Range rg) ExpandValueOrVariableAsExcelCellRowAndCellColumnAndRange(this IExcelRCLocationProperties command, Engine.AutomationEngineInstance engine)
+        //{
+        //    var row = ((ScriptCommand)command).ExpandValueOrUserVariableAsInteger(nameof(command.v_CellRow), "Cell Row", engine);
+        //    var column = ((ScriptCommand)command).ExpandValueOrUserVariableAsInteger(nameof(command.v_CellColumn), "Cell Column", engine);
 
-            var excelInstance = command.ExpandValueOrVariableAsExcelInstance(engine);
+        //    var excelInstance = command.ExpandValueOrVariableAsExcelInstance(engine);
 
-            if (excelInstance.RCLocationTryParse(row, column, out Range rg))
-            {
-                return (row, column, rg);
-            }
-            else
-            {
-                throw new Exception($"Invalid Cell Location. Row: '{command.v_CellRow}', Column: '{command.v_CellColumn}', Row Expanded: '{row}', Column Expanded: '{command}'");
-            }
-        }
+        //    if (excelInstance.RCLocationTryParse(row, column, out Range rg))
+        //    {
+        //        return (row, column, rg);
+        //    }
+        //    else
+        //    {
+        //        throw new Exception($"Invalid Cell Location. Row: '{command.v_CellRow}', Column: '{command.v_CellColumn}', Row Expanded: '{row}', Column Expanded: '{command}'");
+        //    }
+        //}
     }
 }
