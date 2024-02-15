f3c0a17a3397ed32da29baf611d70b2d17ce37e8
rcktrncn <rcktrncn@hotmail.com>
2024-02-15 21:25:44 +0900

    add new method

--- taskt/Core/Automation/Commands/Excel/EM_ExcelRCLocationPropertiesExtensionMethods.cs
+++ taskt/Core/Automation/Commands/Excel/EM_ExcelRCLocationPropertiesExtensionMethods.cs
@@ -28,6 +28,30 @@ namespace taskt.Core.Automation.Commands
         }
 
         /// <summary>
+        /// expand value or variable as Excel Cell Location
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static Range ExpandValueOrVariableAsExcelCellLocation(this IExcelRCLocationProperties command, Engine.AutomationEngineInstance engine)
+        {
+            var row = ((ScriptCommand)command).ExpandValueOrUserVariableAsInteger(nameof(command.v_CellRow), "Cell Row", engine);
+            var column = ((ScriptCommand)command).ExpandValueOrUserVariableAsInteger(nameof(command.v_CellColumn), "Cell Column", engine);
+
+            var excelInstance = command.ExpandValueOrVariableAsExcelInstance(engine);
+
+            if (excelInstance.RCLocationTryParse(row, column, out Range rg))
+            {
+                return rg;
+            }
+            else
+            {
+                throw new Exception($"Invalid Cell Location. Row: '{command.v_CellRow}', Column: '{command.v_CellColumn}', Row Expanded: '{row}', Column Expanded: '{command}'");
+            }
+        }
+
+        /// <summary>
         /// expand value or variable as Excel Cell Row, Column, and Range
         /// </summary>
         /// <param name="command"></param>
