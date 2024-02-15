aaa20f97e72d52db26373dbfda72372f8da0d4cb
rcktrncn <rcktrncn@hotmail.com>
2024-02-15 22:54:14 +0900

    add interface

---
+++ taskt/Core/Automation/Commands/Excel/IExcelRowRangeGetProperties.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    public interface IExcelRowRangeGetProperties : IExcelRowRangeProperties
+    {
+        /// <summary>
+        /// variable name to store result
+        /// </summary>
+        string v_Result { get; set; }
+    }
+}
