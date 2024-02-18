db38e86ea38598f3001ed9db32b151ebccfc0200
rcktrncn <rcktrncn@hotmail.com>
2024-02-18 13:20:03 +0900

    change interface

---
+++ taskt/Core/Automation/Commands/Excel/IExcelCellValueTypeProperties.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands.Excel
+{
+    /// <summary>
+    /// for Cell value type properties
+    /// </summary>
+    public interface IExcelCellValueTypeProperties : IExcelValueTypeProperties
+    {
+        // only implements
+    }
+}
---
+++ taskt/Core/Automation/Commands/Excel/IExcelRCValueTypeProperties.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands.Excel
+{
+    /// <summary>
+    /// for Excel RC Value Type properties
+    /// </summary>
+    public interface IExcelRCValueTypeProperties : IExcelValueTypeProperties
+    {
+        // only implements
+    }
+}
