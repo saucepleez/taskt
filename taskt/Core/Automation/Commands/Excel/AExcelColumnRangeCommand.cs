7f075ebc41501aec8ed523aec32b2ed02e7f007f
rcktrncn <rcktrncn@hotmail.com>
2024-02-18 19:46:39 +0900

    add new abstract classes

---
+++ taskt/Core/Automation/Commands/Excel/AExcelColumnRangeCommand.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for Excel column Range commands
+    /// </summary>
+    public abstract class AExcelColumnRangeCommand : AExcelInstanceCommand, IExcelColumnRangeProperties
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(ExcelControls), nameof(ExcelControls.v_ColumnType))]
+        [PropertyParameterOrder(6000)]
+        public virtual string v_ColumnType { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(ExcelControls), nameof(ExcelControls.v_ColumnNameOrIndex))]
+        [PropertyParameterOrder(7000)]
+        public virtual string v_ColumnIndex { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(ExcelControls), nameof(ExcelControls.v_RowStart))]
+        [PropertyParameterOrder(8000)]
+        public virtual string v_RowStart { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(ExcelControls), nameof(ExcelControls.v_RowEnd))]
+        [PropertyParameterOrder(9000)]
+        public virtual string v_RowEnd { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(ExcelControls), nameof(ExcelControls.v_ValueType))]
+        [PropertyParameterOrder(11000)]
+        public virtual string v_ValueType { get; set; }
+    }
+}
