b507296692af32d90a3fbd5f9e5a3a0645c8cd7d
rcktrncn <rcktrncn@hotmail.com>
2024-02-20 22:24:53 +0900

    add interfaces

---
+++ taskt/Core/Automation/Commands/Excel/IExcelColumnRowRangeSetProperties.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for Excel column-row range (2D-Range) set properties
+    /// </summary>
+    public interface IExcelColumnRowRangeSetProperties : IExcelColumnRowRangeProperties, ILExcelRangeSetProperties
+    {
+        // only implements
+    }
+}
