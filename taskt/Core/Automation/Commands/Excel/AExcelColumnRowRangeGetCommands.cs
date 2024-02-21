1f68fa95f71373e73cc2c990631ee27f9fdcf115
rcktrncn <rcktrncn@hotmail.com>
2024-02-21 20:59:39 +0900

    add new classes

---
+++ taskt/Core/Automation/Commands/Excel/AExcelColumnRowRangeGetCommands.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for Excel Column Row Range (2D-Range) Get commands
+    /// </summary>
+    public abstract class AExcelColumnRowRangeGetCommands : AExcelColumnRowRangeCommands, IExcelColumnRowRangeGetProperties
+    {
+        [XmlAttribute]
+        [PropertyParameterOrder(11000)]
+        public abstract string v_Result { get; set; }
+    }
+}
