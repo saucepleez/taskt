486c031ed36647841d8a2a8fb72a9de26cd7cc90
rcktrncn <rcktrncn@hotmail.com>
2024-02-23 19:24:38 +0900

    add Excel Value Set properties

---
+++ taskt/Core/Automation/Commands/Excel/ILExcelValueSetProperties.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    public interface ILExcelValueSetProperties : ILExpandableProperties
+    {
+        /// <summary>
+        /// text to set
+        /// </summary>
+        string v_TextToSet { get; set; }
+    }
+}
