3d45d16cadc182460529fb6d9d09d8da203e12d0
rcktrncn <rcktrncn@hotmail.com>
2024-02-27 23:00:11 +0900

    add interface, classes

---
+++ taskt/Core/Automation/Commands/Excel/EM_ExcelColumnSpecifiedPropertiesExtensionMethods.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.Office.Interop.Excel;
+
+namespace taskt.Core.Automation.Commands
+{
+    public static class EM_ExcelColumnSpecifiedPropertiesExtensionMethods
+    {
+        /// <summary>
+        /// expand value or variable as Excel Column Index
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static int ExpandValueOrVariableAsExcelColumnIndex(this IExcelColumnSpecifiedProperties command, Engine.AutomationEngineInstance engine)
+        {
+            (_, var sheet) = command.ExpandValueOrVariableAsExcelInstanceAndCurrentWorksheet(engine);
+
+            string columnIndexValue = ((ScriptCommand)command).GetRawPropertyValueAsString(nameof(command.v_ColumnIndex), "Column Index");
+            int columnIndex = 0;
+
+            switch (((ScriptCommand)command).ExpandValueOrUserVariableAsSelectionItem(nameof(command.v_ColumnType), "Column Type", engine))
+            {
+                case "range":
+                    if (string.IsNullOrEmpty(columnIndexValue))
+                    {
+                        columnIndexValue = "A";
+                    }
+                    columnIndex = sheet.ToColumnIndex(columnIndexValue);
+                    break;
+                case "rc":
+                    if (string.IsNullOrEmpty(columnIndexValue))
+                    {
+                        columnIndexValue = "1";
+                    }
+                    columnIndex = columnIndexValue.ExpandValueOrUserVariableAsInteger("Column Index", engine);
+                    break;
+            }
+
+            if (!sheet.RCLocationTryParse(1, columnIndex, out _))
+            {
+                throw new Exception($"Invalid Column Index. Type: '{command.v_ColumnType}', Value: '{command.v_ColumnIndex}', Expand Value: '{columnIndex}'");
+            }
+
+            return columnIndex;
+        }
+
+        /// <summary>
+        /// expand value or variable as Excel current worksheet and column index
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        public static (Worksheet, int) ExpandValueOrVariableAsExcelCurrentWorksheetAndColumnIndex(this IExcelColumnSpecifiedProperties command, Engine.AutomationEngineInstance engine)
+        {
+            (_, var sheet) = command.ExpandValueOrVariableAsExcelInstanceAndCurrentWorksheet(engine);
+            var index = command.ExpandValueOrVariableAsExcelColumnIndex(engine);
+
+            return (sheet, index);
+        }
+    }
+}
---
+++ taskt/Core/Automation/Commands/Excel/IExcelColumnSpecifiedProperties.cs
@@ -0,0 +1,13 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// column index/name specified properties
+    /// </summary>
+    public interface IExcelColumnSpecifiedProperties : IExcelRCRangeProperties
+    {
+        /// <summary>
+        /// column index or Name
+        /// </summary>
+        string v_ColumnIndex { get; set; }
+    }
+}
