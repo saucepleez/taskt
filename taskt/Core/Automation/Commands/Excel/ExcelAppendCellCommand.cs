ac51490aa5e98d2f17e50f72d2342d53caead417
rcktrncn <rcktrncn@hotmail.com>
2024-02-27 23:01:04 +0900

    change base class, enable auto rende, display text
    use new methods

--- taskt/Core/Automation/Commands/Excel/ExcelAppendCellCommand.cs
+++ taskt/Core/Automation/Commands/Excel/ExcelAppendCellCommand.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
 using System.Xml.Serialization;
-using System.Linq;
-using taskt.UI.CustomControls;
 using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
@@ -16,7 +12,9 @@ namespace taskt.Core.Automation.Commands
     [Attributes.ClassAttributes.UsesDescription("Use this command when you want to set a value to the last cell.")]
     [Attributes.ClassAttributes.ImplementationDescription("")]
     [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_spreadsheet))]
-    public class ExcelAppendCellCommand : AExcelInstanceCommands
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    public class ExcelAppendCellCommand : AExcelColumnSpecifiedCommands
     {
         //[XmlAttribute]
         //[PropertyDescription("Please Enter the instance name")]
@@ -29,13 +27,25 @@ namespace taskt.Core.Automation.Commands
         //public string v_InstanceName { get; set; }
 
         [XmlAttribute]
-        [PropertyDescription("Please Enter text to set")]
-        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [InputSpecification("Enter the text value that will be set.")]
-        [SampleUsage("Hello World or {vText}")]
-        [Remarks("")]
+        //[PropertyDescription("Please Enter text to set")]
+        //[PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        //[InputSpecification("Enter the text value that will be set.")]
+        //[SampleUsage("Hello World or {vText}")]
+        //[Remarks("")]
+        [PropertyVirtualProperty(nameof(ExcelControls), nameof(ExcelControls.v_ValueToSet))]
         [PropertyParameterOrder(6000)]
         public string v_TextToSet { get; set; }
+
+        //[XmlAttribute]
+        //[PropertyVirtualProperty(nameof(ExcelControls), nameof(ExcelControls.v_ColumnType))]
+        //public string v_ColumnType { get; set; }
+
+        //[XmlAttribute]
+        //[PropertyVirtualProperty(nameof(ExcelControls), nameof(ExcelControls.v_ColumnLocation))]
+        //public string v_ColumnIndex { get; set; }
+
+        //[XmlAttribute]
+        //public string v_ValueType { get; set; }
 
         public ExcelAppendCellCommand()
         {
@@ -50,41 +60,51 @@ namespace taskt.Core.Automation.Commands
         public override void RunCommand(Engine.AutomationEngineInstance engine)
         {
             //(_, var excelSheet) = v_InstanceName.ExpandValueOrUserVariableAsExcelInstanceAndWorksheet(engine);
-            (_, var excelSheet) = this.ExpandValueOrVariableAsExcelInstanceAndCurrentWorksheet(engine);
+            //(_, var excelSheet) = this.ExpandValueOrVariableAsExcelInstanceAndCurrentWorksheet(engine);
 
-            int test = 0;
-            test = excelSheet.Columns.Count;
-            var lastUsedRow = excelSheet.Cells.Find("*", System.Reflection.Missing.Value,
-                                 System.Reflection.Missing.Value, System.Reflection.Missing.Value,
-                                 Microsoft.Office.Interop.Excel.XlSearchOrder.xlByRows, Microsoft.Office.Interop.Excel.XlSearchDirection.xlPrevious,
-                                 false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Row;
-            var targetAddress = "A" + (lastUsedRow + 1);
+            //int test = 0;
+            //test = excelSheet.Columns.Count;
+            //var lastUsedRow = excelSheet.Cells.Find("*", System.Reflection.Missing.Value,
+            //                     System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+            //                     Microsoft.Office.Interop.Excel.XlSearchOrder.xlByRows, Microsoft.Office.Interop.Excel.XlSearchDirection.xlPrevious,
+            //                     false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Row;
+            //var targetAddress = "A" + (lastUsedRow + 1);
+            //var targetText = v_TextToSet.ExpandValueOrUserVariable(engine);
+            //excelSheet.Range[targetAddress].Value = targetText;
+
+            (var sheet, var columnIndex) = this.ExpandValueOrVariableAsExcelCurrentWorksheetAndColumnIndex(engine);
+
+            var rowIndex = sheet.FirstBlankRowIndex(columnIndex, 1, this.ExpandValueOrUserVariableAsSelectionItem(nameof(v_ValueType), "Value Type", engine));
+
             var targetText = v_TextToSet.ExpandValueOrUserVariable(engine);
-            excelSheet.Range[targetAddress].Value = targetText;
+
+            var setFunc = this.ExpandValueOrVaribleAsSetValueAction(engine);
+
+            setFunc(targetText, sheet, columnIndex, rowIndex);
         }
 
-        public override List<Control> Render(UI.Forms.ScriptBuilder.CommandEditor.frmCommandEditor editor)
-        {
-            base.Render(editor);
+        //public override List<Control> Render(UI.Forms.ScriptBuilder.CommandEditor.frmCommandEditor editor)
+        //{
+        //    base.Render(editor);
 
-            //create standard group controls
-            var instanceCtrls = CommandControls.CreateDefaultDropdownGroupFor("v_InstanceName", this, editor);
-            CommandControls.AddInstanceNames((ComboBox)instanceCtrls.Where(t => (t.Name == "v_InstanceName")).FirstOrDefault(), editor, PropertyInstanceType.InstanceType.Excel);
-            RenderedControls.AddRange(instanceCtrls);
-            //RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_InstanceName", this, editor));
-            RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_TextToSet", this, editor));
+        //    //create standard group controls
+        //    var instanceCtrls = CommandControls.CreateDefaultDropdownGroupFor("v_InstanceName", this, editor);
+        //    CommandControls.AddInstanceNames((ComboBox)instanceCtrls.Where(t => (t.Name == "v_InstanceName")).FirstOrDefault(), editor, PropertyInstanceType.InstanceType.Excel);
+        //    RenderedControls.AddRange(instanceCtrls);
+        //    //RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_InstanceName", this, editor));
+        //    RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_TextToSet", this, editor));
 
-            if (editor.creationMode == UI.Forms.ScriptBuilder.CommandEditor.frmCommandEditor.CreationMode.Add)
-            {
-                this.v_InstanceName = editor.appSettings.ClientSettings.DefaultExcelInstanceName;
-            }
+        //    if (editor.creationMode == UI.Forms.ScriptBuilder.CommandEditor.frmCommandEditor.CreationMode.Add)
+        //    {
+        //        this.v_InstanceName = editor.appSettings.ClientSettings.DefaultExcelInstanceName;
+        //    }
 
-            return RenderedControls;
-        }
+        //    return RenderedControls;
+        //}
 
-        public override string GetDisplayValue()
-        {
-            return base.GetDisplayValue() + " [Append last cell to: " + v_TextToSet + ", Instance Name: '" + v_InstanceName + "']";
-        }
+        //public override string GetDisplayValue()
+        //{
+        //    return base.GetDisplayValue() + " [Append last cell to: " + v_TextToSet + ", Instance Name: '" + v_InstanceName + "']";
+        //}
     }
 }
--- taskt/Core/Automation/Commands/Excel/ExcelAppendRowCommand.cs
+++ taskt/Core/Automation/Commands/Excel/ExcelAppendRowCommand.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
 using System.Xml.Serialization;
-using System.Linq;
-using taskt.UI.CustomControls;
 using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
@@ -16,7 +12,9 @@ namespace taskt.Core.Automation.Commands
     [Attributes.ClassAttributes.UsesDescription("Use this command will take in a comma seprerated value and append it to the end of an excel sheet.")]
     [Attributes.ClassAttributes.ImplementationDescription("This command implements Excel Interop to achieve automations.")]
     [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_spreadsheet))]
-    public class ExcelAppendRowCommand : AExcelInstanceCommands
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    public class ExcelAppendRowCommand : AExcelColumnSpecifiedCommands
     {
         //[XmlAttribute]
         //[PropertyDescription("Please Enter the instance name")]
@@ -29,13 +27,40 @@ namespace taskt.Core.Automation.Commands
         //public string v_InstanceName { get; set; }
 
         [XmlAttribute]
-        [PropertyDescription("Please Enter the Row to set")]
-        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [InputSpecification("Enter the text value that will be set (This could be a DataRow).")]
-        [SampleUsage("Hello,world or {vText}")]
-        [Remarks("")]
+        //[PropertyDescription("Please Enter the Row to set")]
+        //[PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        //[InputSpecification("Enter the text value that will be set (This could be a DataRow).")]
+        //[SampleUsage("Hello,world or {vText}")]
+        //[Remarks("")]
+        [PropertyVirtualProperty(nameof(ExcelControls), nameof(ExcelControls.v_ValueToSet))]
+        [PropertyDetailSampleUsageBehavior(MultiAttributesBehavior.Overwrite)]
+        [PropertyDetailSampleUsage("**Hello,World**", "Specified **Hello** and **World**.")]
+        [PropertyDetailSampleUsage("**{{{vText}}}**", PropertyDetailSampleUsage.ValueType.VariableValue, "Value To Set")]
         [PropertyParameterOrder(6000)]
         public string v_TextToSet { get; set; }
+
+        [XmlAttribute]
+        [PropertyDescription("Text Separator")]
+        [InputSpecification("")]
+        [PropertyShowSampleUsageInDescription(true)]
+        [PropertyDetailSampleUsage("**,**", PropertyDetailSampleUsage.ValueType.Value, "Text Separator")]
+        [PropertyDetailSampleUsage("**{{{vSep}}}**", PropertyDetailSampleUsage.ValueType.VariableValue, "Text Separator")]
+        [PropertyTextBoxSetting(1, false)]
+        [PropertyIsOptional(true, ",")]
+        [PropertyFirstValue(",")]
+        [Remarks("")]
+        [PropertyDisplayText(false, "Text Separator")]
+        [PropertyParameterOrder(6500)]
+        public string v_TextSeparator { get; set; }
+
+        //[XmlAttribute]
+        //public string v_ColumnType { get; set; }
+
+        //[XmlAttribute]
+        //public string v_ColumnIndex { get; set; }
+
+        //[XmlAttribute]
+        //public string v_ValueType { get; set; }
 
         public ExcelAppendRowCommand()
         {
@@ -49,64 +74,87 @@ namespace taskt.Core.Automation.Commands
 
         public override void RunCommand(Engine.AutomationEngineInstance engine)
         {
-            var splittext = v_TextToSet.Split(',');
+            //var splittext = v_TextToSet.Split(',');
 
-            //(_, var excelSheet) = v_InstanceName.ExpandValueOrUserVariableAsExcelInstanceAndWorksheet(engine);
-            (_, var excelSheet) = this.ExpandValueOrVariableAsExcelInstanceAndCurrentWorksheet(engine);
+            ////(_, var excelSheet) = v_InstanceName.ExpandValueOrUserVariableAsExcelInstanceAndWorksheet(engine);
+            //(_, var excelSheet) = this.ExpandValueOrVariableAsExcelInstanceAndCurrentWorksheet(engine);
 
-            int i = 1;            
-            int lastUsedRow;
-            try
-            {
-                lastUsedRow = excelSheet.Cells.Find("*", System.Reflection.Missing.Value,
-                                 System.Reflection.Missing.Value, System.Reflection.Missing.Value,
-                                 Microsoft.Office.Interop.Excel.XlSearchOrder.xlByRows, Microsoft.Office.Interop.Excel.XlSearchDirection.xlPrevious,
-                                 false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Row;
-            }
-            catch(Exception)
-            {
-                lastUsedRow = 0;
-            }
+            //int i = 1;            
+            //int lastUsedRow;
+            //try
+            //{
+            //    lastUsedRow = excelSheet.Cells.Find("*", System.Reflection.Missing.Value,
+            //                     System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+            //                     Microsoft.Office.Interop.Excel.XlSearchOrder.xlByRows, Microsoft.Office.Interop.Excel.XlSearchDirection.xlPrevious,
+            //                     false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Row;
+            //}
+            //catch(Exception)
+            //{
+            //    lastUsedRow = 0;
+            //}
+
+            //var targetText = v_TextToSet.ExpandValueOrUserVariable(engine);
+            //splittext = targetText.Split(',');
+
+            //foreach (var item in splittext)
+            //{
+            //    string output = item;
+            //    if (item.Contains("[") || item.Contains("]"))
+            //        output = item.Trim('[', ']');
+            //    output = output.Trim('"');
+            //    if (output=="null")
+            //    {
+            //        output = string.Empty;
+            //    }
+            //    excelSheet.Cells[lastUsedRow + 1, i] = output;
+            //    i++;
+            //}
+
+            (var sheet, var columnIndex) = this.ExpandValueOrVariableAsExcelCurrentWorksheetAndColumnIndex(engine);
+
+            var rowIndex = sheet.FirstBlankRowIndex(columnIndex, 1, this.ExpandValueOrUserVariableAsSelectionItem(nameof(v_ValueType), "Value Type", engine));
+
+            var setFunc = this.ExpandValueOrVaribleAsSetValueAction(engine);
 
             var targetText = v_TextToSet.ExpandValueOrUserVariable(engine);
-            splittext = targetText.Split(',');
+            var separator = v_TextSeparator.ExpandValueOrUserVariable(engine);
 
-            foreach (var item in splittext)
+            if (separator.Length > 1)
             {
-                string output = item;
-                if (item.Contains("[") || item.Contains("]"))
-                    output = item.Trim('[', ']');
-                output = output.Trim('"');
-                if (output=="null")
-                {
-                    output = string.Empty;
-                }
-                excelSheet.Cells[lastUsedRow + 1, i] = output;
-                i++;
+                throw new Exception($"Separator must specify only one character. Value: '{v_TextSeparator}', Expand Value: '{separator}'");
+            }
+
+            var lst = targetText.Split(separator[0]);
+
+            int index = 0;
+            foreach(var v in lst)
+            {
+                setFunc(v, sheet, columnIndex + index, rowIndex);
+                index++;
             }
         }
 
-        public override List<Control> Render(UI.Forms.ScriptBuilder.CommandEditor.frmCommandEditor editor)
-        {
-            base.Render(editor);
+        //public override List<Control> Render(UI.Forms.ScriptBuilder.CommandEditor.frmCommandEditor editor)
+        //{
+        //    base.Render(editor);
 
-            var instanceCtrls = CommandControls.CreateDefaultDropdownGroupFor("v_InstanceName", this, editor);
-            CommandControls.AddInstanceNames((ComboBox)instanceCtrls.Where(t => (t.Name == "v_InstanceName")).FirstOrDefault(), editor, PropertyInstanceType.InstanceType.Excel);
-            RenderedControls.AddRange(instanceCtrls);
-            //RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_InstanceName", this, editor));
-            RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_TextToSet", this, editor));
+        //    var instanceCtrls = CommandControls.CreateDefaultDropdownGroupFor("v_InstanceName", this, editor);
+        //    CommandControls.AddInstanceNames((ComboBox)instanceCtrls.Where(t => (t.Name == "v_InstanceName")).FirstOrDefault(), editor, PropertyInstanceType.InstanceType.Excel);
+        //    RenderedControls.AddRange(instanceCtrls);
+        //    //RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_InstanceName", this, editor));
+        //    RenderedControls.AddRange(CommandControls.CreateDefaultInputGroupFor("v_TextToSet", this, editor));
 
-            if (editor.creationMode == UI.Forms.ScriptBuilder.CommandEditor.frmCommandEditor.CreationMode.Add)
-            {
-                this.v_InstanceName = editor.appSettings.ClientSettings.DefaultExcelInstanceName;
-            }
+        //    if (editor.creationMode == UI.Forms.ScriptBuilder.CommandEditor.frmCommandEditor.CreationMode.Add)
+        //    {
+        //        this.v_InstanceName = editor.appSettings.ClientSettings.DefaultExcelInstanceName;
+        //    }
 
-            return RenderedControls;
-        }
+        //    return RenderedControls;
+        //}
 
-        public override string GetDisplayValue()
-        {
-            return base.GetDisplayValue() + " [Append Row '" +v_TextToSet+ " to last row of workboook with Instance Name: '" + v_InstanceName + "']";
-        }
+        //public override string GetDisplayValue()
+        //{
+        //    return base.GetDisplayValue() + " [Append Row '" +v_TextToSet+ " to last row of workboook with Instance Name: '" + v_InstanceName + "']";
+        //}
     }
 }
