ef4815a0ec7ed28c432d5b802cf63ee834e40b4c
rcktrncn <rcktrncn@hotmail.com>
2024-03-02 15:50:05 +0900

    change base class, use new methods

--- taskt/Core/Automation/Commands/Excel/ExcelGetLastRowCommand.cs
+++ taskt/Core/Automation/Commands/Excel/ExcelGetLastRowCommand.cs
@@ -14,32 +14,39 @@ namespace taskt.Core.Automation.Commands
     [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_spreadsheet))]
     [Attributes.ClassAttributes.EnableAutomateRender(true)]
     [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
-    public class ExcelGetLastRowCommand : AExcelInstanceCommands
+    public class ExcelGetLastRowCommand : AExcelColumnSpecifiedCommands
     {
         //[XmlAttribute]
         //[PropertyVirtualProperty(nameof(ExcelControls), nameof(ExcelControls.v_InputInstanceName))]
         //public string v_InstanceName { get; set; }
 
+        //[XmlAttribute]
+        //public string v_ColumnType { get; set; }
+
         [XmlAttribute]
-        [PropertyDescription("Letter of the Column to check")]
-        [PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
-        [InputSpecification("Column Letter", true)]
-        //[SampleUsage("**A** or **B** or **{{{vColumn}}}**")]
-        [PropertyDetailSampleUsage("**A**", "Specify the Column **A**")]
-        [PropertyDetailSampleUsage("**B**", "Specify the Column **B**")]
-        [PropertyDetailSampleUsage("**{{{vColumn}}}**", PropertyDetailSampleUsage.ValueType.VariableValue, "Column")]
-        [Remarks("")]
-        [PropertyTextBoxSetting(1, false)]
-        [PropertyShowSampleUsageInDescription(true)]
+        //[PropertyDescription("Letter of the Column to check")]
+        //[PropertyUIHelper(PropertyUIHelper.UIAdditionalHelperType.ShowVariableHelper)]
+        //[InputSpecification("Column Letter", true)]
+        ////[SampleUsage("**A** or **B** or **{{{vColumn}}}**")]
+        //[PropertyDetailSampleUsage("**A**", "Specify the Column **A**")]
+        //[PropertyDetailSampleUsage("**B**", "Specify the Column **B**")]
+        //[PropertyDetailSampleUsage("**{{{vColumn}}}**", PropertyDetailSampleUsage.ValueType.VariableValue, "Column")]
+        //[Remarks("")]
+        //[PropertyTextBoxSetting(1, false)]
+        //[PropertyShowSampleUsageInDescription(true)]
         [PropertyIsOptional(true, "A")]
-        [PropertyDisplayText(true, "Column")]
-        [PropertyParameterOrder(6000)]
-        public string v_ColumnLetter { get; set; }
+        //[PropertyDisplayText(true, "Column")]
+        //[PropertyParameterOrder(6000)]
+        public override string v_ColumnIndex { get; set; }
 
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_Result))]
-        [PropertyParameterOrder(6001)]
+        [PropertyParameterOrder(8500)]
         public string v_Result { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(ExcelControls), nameof(ExcelControls.v_CheckableValueType))]
+        public override string v_ValueType { get; set; }
 
         public ExcelGetLastRowCommand()
         {
@@ -54,15 +61,20 @@ namespace taskt.Core.Automation.Commands
             //(_, var excelSheet) = v_InstanceName.ExpandValueOrUserVariableAsExcelInstanceAndWorksheet(engine);
             (_, var excelSheet) = this.ExpandValueOrVariableAsExcelInstanceAndCurrentWorksheet(engine);
 
-            if (string.IsNullOrEmpty(v_ColumnLetter))
-            {
-                v_ColumnLetter = "A";
-            }
-            var columnLetter = v_ColumnLetter.ExpandValueOrUserVariable(engine);
+            //if (string.IsNullOrEmpty(v_ColumnIndex))
+            //{
+            //    v_ColumnIndex = "A";
+            //}
+            //var columnLetter = v_ColumnIndex.ExpandValueOrUserVariable(engine);
 
-            var lastRow = (int)excelSheet.Cells[excelSheet.Rows.Count, columnLetter].End(Microsoft.Office.Interop.Excel.XlDirection.xlUp).Row;
+            //var lastRow = (int)excelSheet.Cells[excelSheet.Rows.Count, columnLetter].End(Microsoft.Office.Interop.Excel.XlDirection.xlUp).Row;
 
-            lastRow.ToString().StoreInUserVariable(engine, v_Result);
+            var columnIndex = this.ExpandValueOrVariableAsExcelColumnIndex(engine);
+            var valueType = this.ExpandValueOrUserVariableAsSelectionItem(nameof(v_ValueType), "Value Type", engine);
+            var lastRow = excelSheet.LastRowIndex(columnIndex, 1, valueType);
+
+            //lastRow.ToString().StoreInUserVariable(engine, v_Result);
+            lastRow.StoreInUserVariable(engine, v_Result);
         }
     }
 }
