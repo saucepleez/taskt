6dd8c97a54575986d5268f5ae561593cf8ef4c23
rcktrncn <rcktrncn@hotmail.com>
2024-03-03 20:26:38 +0900

    add new interface, class

---
+++ taskt/Core/Automation/Commands/Color/IColorCreateProperties.cs
@@ -0,0 +1,13 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// color create properties
+    /// </summary>
+    public interface IColorCreateProperties : ICanHandleColor
+    {
+        /// <summary>
+        /// color variable name
+        /// </summary>
+        string v_Color { get; set; }
+    }
+}
