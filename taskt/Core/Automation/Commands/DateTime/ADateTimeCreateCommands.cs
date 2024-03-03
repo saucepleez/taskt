9484d4b1c716df2062439d536c9a2cff5892fc54
rcktrncn <rcktrncn@hotmail.com>
2024-03-03 19:15:34 +0900

    add new interface, abstract class

---
+++ taskt/Core/Automation/Commands/DateTime/ADateTimeCreateCommands.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for Create DateTime variable commands
+    /// </summary>
+    public abstract class ADateTimeCreateCommands : ScriptCommand, IDateTimeCreateProperties
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(DateTimeControls), nameof(DateTimeControls.v_OutputDateTime))]
+        public virtual string v_DateTime { get; set; }
+    }
+}
