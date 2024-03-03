2076adafba0bb48bae83e988564518ff38bbd99c
rcktrncn <rcktrncn@hotmail.com>
2024-03-03 16:09:20 +0900

    add new interface, extension mehtods

---
+++ taskt/Core/Automation/Commands/DateTime/IDateTimeConvertProperties.cs
@@ -0,0 +1,18 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// DateTime convert properties
+    /// </summary>
+    public interface IDateTimeConvertProperties : ICanHandleDateTime
+    {
+        /// <summary>
+        /// DateTime variable name
+        /// </summary>
+        string v_DateTime { get; set; }
+
+        /// <summary>
+        /// Variable Name to Store Result
+        /// </summary>
+        string v_Result { get; set; }
+    }
+}
