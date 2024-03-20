476c9a451f53542825a2eeb2cf7cbdd5893b57ed
rcktrncn <rcktrncn@hotmail.com>
2024-03-20 13:51:49 +0900

    add new abstract class
    for Dictionary commands

---
+++ taskt/Core/Automation/Commands/Dictionary/ADictionaryCommands.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for Dictionary commands
+    /// </summary>
+    public abstract class ADictionaryCommands : ScriptCommand, ILDictionaryProperties
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(DictionaryControls), nameof(DictionaryControls.v_InputDictionaryName))]
+        public virtual string v_Dictionary { get; set; }
+    }
+}
