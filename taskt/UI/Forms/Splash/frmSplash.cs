b296d32a5f3250ec84fd995a072548d4aa87f9cd
rcktrncn <rcktrncn@hotmail.com>
2024-03-20 10:56:38 +0900

    change create folders process

--- taskt/UI/Forms/Splash/frmSplash.cs
+++ taskt/UI/Forms/Splash/frmSplash.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.IO;
-using taskt.Core.Script;
+using taskt.Core.IO;
 
 namespace taskt.UI.Forms.Splash
 {
@@ -20,30 +20,45 @@ namespace taskt.UI.Forms.Splash
             //{
             //    Directory.CreateDirectory(autoSavePath);
             //}
-            CreateFolderProcess(Script.GetAutoSaveFolderPath);
+            //CreateFolderProcess(Script.GetAutoSaveFolderPath);
+            CreateFolderProcess(Folders.GetAutoSaveFolderPath());
 
             //var runPath = Script.GetRunWithoutSavingFolderPath();
             //if (!Directory.Exists(runPath))
             //{
             //    Directory.CreateDirectory(runPath);
             //}
-            CreateFolderProcess(Script.GetRunWithoutSavingFolderPath);
+            //CreateFolderProcess(Script.GetRunWithoutSavingFolderPath);
+            CreateFolderProcess(Folders.GetRunWithoutSavingFolderPath());
 
             //var befPath = Script.GetBeforeConvertedFolderPath();
             //if (!Directory.Exists(befPath))
             //{
             //    Directory.CreateDirectory(befPath);
             //}
-            CreateFolderProcess(Script.GetBeforeConvertedFolderPath);
+            //CreateFolderProcess(Script.GetBeforeConvertedFolderPath);
+            CreateFolderProcess(Folders.GetBeforeConvertedFolderPath());
         }
+
+        ///// <summary>
+        ///// create folder process when not exists
+        ///// </summary>
+        ///// <param name="folderFunc"></param>
+        //private static void CreateFolderProcess(Func<string> folderFunc)
+        //{
+        //    var path = folderFunc();
+        //    if (!Directory.Exists(path))
+        //    {
+        //        Directory.CreateDirectory(path);
+        //    }
+        //}
 
         /// <summary>
         /// create folder process when not exists
         /// </summary>
-        /// <param name="folderFunc"></param>
-        private static void CreateFolderProcess(Func<string> folderFunc)
+        /// <param name="path"></param>
+        private static void CreateFolderProcess(string path)
         {
-            var path = folderFunc();
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
