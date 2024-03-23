4b811a3cd43596f43a033879879ee0b1188ee014
rcktrncn <rcktrncn@hotmail.com>
2024-03-23 13:10:30 +0900

    add interfaces, extension methods

---
+++ taskt/Core/Automation/Commands/DataTable/EM_CanHandleDataTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+
+namespace taskt.Core.Automation.Commands
+{
+    public static class EM_CanHandleDataTable
+    {
+        /// <summary>
+        /// expand user variable as DataTable
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static DataTable ExpandUserVariableAsDataTable(this ICanHandleDictionary command, string parameterName, Engine.AutomationEngineInstance engine)
+        {
+            var variableName = ((ScriptCommand)command).GetRawPropertyValueAsString(parameterName, "DataTable Variable");
+            var v = variableName.GetRawVariable(engine);
+            if (v.VariableValue is DataTable table)
+            {
+                return table;
+            }
+            else
+            {
+                throw new Exception($"Variable '{variableName}' is not DataTable");
+            }
+        }
+
+        /// <summary>
+        /// store DataTable in User Variable
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="table"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="engine"></param>
+        public static void StoreDataTableInUserVariable(this ICanHandleColor command, DataTable table, string parameterName, Engine.AutomationEngineInstance engine)
+        {
+            var variableName = ((ScriptCommand)command).GetRawPropertyValueAsString(parameterName, "DataTable Variable");
+            ExtensionMethods.StoreInUserVariable(variableName, table, engine);
+        }
+    }
+}
---
+++ taskt/Core/Automation/Commands/DataTable/ILDataTableProperties.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    public interface ILDataTableProperties : ILExpandableProperties, ICanHandleDataTable
+    {
+        /// <summary>
+        /// DataTable variabe name
+        /// </summary>
+        string v_DataTable { get; set; }
+    }
+}
