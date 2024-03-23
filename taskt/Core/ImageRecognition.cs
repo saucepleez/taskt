0491b9b8102bd59520665829ef0deed36279fa83
rcktrncn <rcktrncn@hotmail.com>
2024-03-23 21:57:11 +0900

    remove using

--- taskt/Core/ImageRecognition.cs
+++ taskt/Core/ImageRecognition.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 
 namespace taskt.Core
 {
@@ -18,5 +13,4 @@ namespace taskt.Core
         public int yLocation { get; set; }
         bool matchFound { get; set; }
     }
-
 }
