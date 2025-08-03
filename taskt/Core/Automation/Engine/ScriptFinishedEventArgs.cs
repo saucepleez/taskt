503a18fc02d6154bff976a59fdd54316eee50d0e
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-08-03 15:22:57 +0900

    separate class to files

---
+++ taskt/Core/Automation/Engine/ScriptFinishedEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace taskt.Core.Automation.Engine
+{
+    /// <summary>
+    /// script finished event args
+    /// </summary>
+    public class ScriptFinishedEventArgs : EventArgs
+    {
+        public DateTime LoggedOn { get; set; }
+        public ScriptFinishedResult Result { get; set; }
+        public string Error { get; set; }
+        public TimeSpan ExecutionTime { get; set; }
+        public string FileName { get; set; }
+        public enum ScriptFinishedResult
+        {
+            Successful,
+            Error,
+            Cancelled
+        }
+    }
+}
