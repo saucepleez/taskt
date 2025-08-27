960bd2dfe73d2841ef94ea290cd20b20ad988c54
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-08-27 15:59:43 +0900

    new extension methods

---
+++ taskt/Core/Automation/Commands/Window/EM_WindowNameActionPropertiesExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using taskt.Core.Automation.Engine;
+
+namespace taskt.Core.Automation.Commands.Window
+{
+    public static class EM_WindowNameActionPropertiesExtensionMethods
+    {
+        /// <summary>
+        /// window name action, and wait before execute ation
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="engine"></param>
+        /// <param name="actionFunc"></param>
+        /// <param name="errorFunc"></param>
+        public static void WindowNameActionAndWait(this IWindowNameActionProperties command, AutomationEngineInstance engine, Action<IntPtr, string> actionFunc, Action<Exception> errorFunc = null)
+        {
+            command.WindowNameAction(engine, 
+                new Action<IntPtr, string>((whnd, name) =>
+                {
+                    command.WaitAfterFindWindowProcess(engine);
+                    actionFunc(whnd, name);
+                }), 
+                errorFunc
+            );
+        }
+    }
+}
