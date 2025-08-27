60cc5651119aeb05ac0f19efa2d5702768beb202
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-08-27 16:15:46 +0900

    add new window activate commnad

---
+++ taskt/Core/Automation/Commands/Window/AWindowNameActionCommands.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands.Window
+{
+    /// <summary>
+    /// for one window name action commands
+    /// </summary>
+    public abstract class AWindowNameActionCommands : AWindowNameCommands, IWindowNameActionProperties
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowControls), nameof(WindowControls.v_WaitTimeBetweenFindAndAction))]
+        [PropertyParameterOrder(10000)]
+        public string v_WaitTimeBetweenFindAndAction { get; set; }
+    }
+}
---
+++ taskt/Core/Automation/Commands/Window/ActivateOneWindowPositionCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using taskt.Core.Automation.Commands.Window;
+
+namespace taskt.Core.Automation.Commands
+{
+    [Serializable]
+    [Attributes.ClassAttributes.Group("Window")]
+    [Attributes.ClassAttributes.SubGruop("Window Actions")]
+    [Attributes.ClassAttributes.CommandSettings("Activate One Winodw")]
+    [Attributes.ClassAttributes.Description("This command Activate one Window.")]
+    [Attributes.ClassAttributes.UsesDescription("Use this command when you want to Activate one Windown.")]
+    [Attributes.ClassAttributes.ImplementationDescription("")]
+    [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_window))]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    public class ActivateOneWindowCommand : AWindowNameActionCommands
+    {
+        public ActivateOneWindowCommand()
+        {
+        }
+
+        public override void RunCommand(Engine.AutomationEngineInstance engine)
+        {
+            this.WindowNameActionAndWait(engine, new Action<IntPtr, string>((whnd, name) =>
+            {
+                var activateCommand = new ActivateWindowByWindowHandleCommand()
+                {
+                    v_WindowHandle = whnd.ToString(),
+                    v_WaitTimeForWindow = this.v_WaitTimeForWindow,
+                };
+                activateCommand.RunCommand(engine);
+            }));
+        }
+    }
+}
