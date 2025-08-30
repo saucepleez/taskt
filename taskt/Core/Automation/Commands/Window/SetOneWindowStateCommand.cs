70de7b1a6d102377d22e0bc005d63980cce8856e
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-08-30 11:57:55 +0900

    add new commands

---
+++ taskt/Core/Automation/Commands/Window/SetOneWindowStateCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    [Serializable]
+    [Attributes.ClassAttributes.Group("Window")]
+    [Attributes.ClassAttributes.SubGruop("One Window Name Actions")]
+    [Attributes.ClassAttributes.CommandSettings("Set One Winodw State")]
+    [Attributes.ClassAttributes.Description("This command Set State one Window.")]
+    [Attributes.ClassAttributes.UsesDescription("Use this command when you want to Set State one Window.")]
+    [Attributes.ClassAttributes.ImplementationDescription("")]
+    [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_window))]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    public class SetOneWindowState : AWindowNameActionCommands
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowControls), nameof(WindowControls.v_WindowState))]
+        [PropertyParameterOrder(6500)]
+        public string v_WindowState { get; set; }
+
+        public SetOneWindowState()
+        {
+        }
+
+        public override void RunCommand(Engine.AutomationEngineInstance engine)
+        {
+            this.WindowNameActionAndWait(engine, new Action<IntPtr, string>((whnd, name) =>
+            {
+                var setState = new SetWindowStateByWindowHandleCommand()
+                {
+                    v_WindowHandle = whnd.ToString(),
+                    v_WindowState = this.v_WindowState,
+                };
+                setState.RunCommand(engine);
+            }));
+        }
+    }
+}
