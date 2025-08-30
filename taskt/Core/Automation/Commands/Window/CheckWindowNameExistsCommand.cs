6ddb22caae08b1f542dde6e266170633527cbe90
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-08-30 16:51:28 +0900

    change namespace

--- taskt/Core/Automation/Commands/Window/CheckWindowNameExistsCommand.cs
+++ taskt/Core/Automation/Commands/Window/CheckWindowNameExistsCommand.cs
@@ -15,7 +15,7 @@ namespace taskt.Core.Automation.Commands
     [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_window))]
     [Attributes.ClassAttributes.EnableAutomateRender(true)]
     [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
-    public sealed class CheckWindowNameExistsCommand : AWindowNameCoreCommands
+    public sealed class CheckWindowNameExistsCommand : AWindowNamesCommands
     {
         //[XmlAttribute]
         //[PropertyVirtualProperty(nameof(WindowNameControls), nameof(WindowNameControls.v_WindowName))]
--- taskt/Core/Automation/Commands/Window/GetWindowNamesCommand.cs
+++ taskt/Core/Automation/Commands/Window/GetWindowNamesCommand.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Serialization;
 using taskt.Core.Automation.Attributes.PropertyAttributes;
-using taskt.Core.Automation.Commands.Window;
 using taskt.Core.Automation.Engine;
 
 namespace taskt.Core.Automation.Commands
