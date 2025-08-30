d189a947b644fb5231ee43aa72e1a26aa6ef75a6
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-08-30 10:57:12 +0900

    add new commands

---
+++ taskt/Core/Automation/Commands/Window/GetOneWindowHandleFromOneWindowNameCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    [Serializable]
+    [Attributes.ClassAttributes.Group("Window")]
+    [Attributes.ClassAttributes.SubGruop("Get From Window Name")]
+    [Attributes.ClassAttributes.CommandSettings("Get One Window Handle From One Window Name")]
+    [Attributes.ClassAttributes.Description("This command returns one window handle.")]
+    [Attributes.ClassAttributes.UsesDescription("Use this command when you want to get one window handle.")]
+    [Attributes.ClassAttributes.ImplementationDescription("")]
+    [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_window))]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    public class GetOneWindowHandleFromOneWindowNameCommand : AWindowNameCommands
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_Result))]
+        [PropertyValidationRule("Result", PropertyValidationRule.ValidationRuleFlags.Empty)]
+        [PropertyParameterOrder(6500)]
+        public string v_Result { get; set; }
+
+        public GetOneWindowHandleFromOneWindowNameCommand()
+        {
+        }
+
+        public override void RunCommand(Engine.AutomationEngineInstance engine)
+        {
+            this.WindowNameAction(engine, new Action<IntPtr, string>((whnd, name) =>
+            {
+                whnd.StoreInUserVariable(engine, v_Result);
+            }));
+        }
+    }
+}
