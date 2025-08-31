7e912668fcf207647a62293f7913a639d440d2a3
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-08-31 09:35:19 +0900

    add new interface, class

---
+++ taskt/Core/Automation/Commands/Window/AWindowNamesActionCommnads.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands.Window
+{
+    /// <summary>
+    /// for window names action commands
+    /// </summary>
+    public abstract class AWindowNamesActionCommnads : AWindowNamesCommands, IWindowNamesActionProperties
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(WindowControls), nameof(WindowControls.v_WaitTimeBetweenFindAndAction))]
+        [PropertyParameterOrder(10000)]
+        public string v_WaitTimeBetweenFindAndAction { get; set; }
+    }
+}
---
+++ taskt/Core/Automation/Commands/Window/IWindowNamesActionProperties.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// window names action properties
+    /// </summary>
+    public interface IWindowNamesActionProperties : IWindowNamesProperties, IWindowWaitTimeBetweenFindAndActionProperties
+    {
+        // nothing
+    }
+}
