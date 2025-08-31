a7e19f9f63940ad6ba0f3ded6028c44b362f958c
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-08-31 13:20:37 +0900

    change inner process, sub group

--- taskt/Core/Automation/Commands/Window/WaitForWindowToExistsCommand.cs
+++ taskt/Core/Automation/Commands/Window/WaitForWindowToExistsCommand.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace taskt.Core.Automation.Commands
 {
     [Serializable]
     [Attributes.ClassAttributes.Group("Window")]
-    [Attributes.ClassAttributes.SubGruop("Window Actions")]
+    [Attributes.ClassAttributes.SubGruop("One Window Name Actions")]
     [Attributes.ClassAttributes.CommandSettings("Wait For Window To Exists")]
     [Attributes.ClassAttributes.Description("This command waits for a window to exist.")]
     [Attributes.ClassAttributes.UsesDescription("Use this command when you want to explicitly wait for a window to exist before continuing script execution.")]
@@ -49,12 +48,13 @@ namespace taskt.Core.Automation.Commands
 
         public override void RunCommand(Engine.AutomationEngineInstance engine)
         {
-            WindowControls.WindowAction(this, engine,
-                new Action<List<(IntPtr, string)>>(wins =>
-                {
-                    // nothing to do
-                })
-            );
+            //WindowControls.WindowAction(this, engine,
+            //    new Action<List<(IntPtr, string)>>(wins =>
+            //    {
+            //        // nothing to do
+            //    })
+            //);
+            this.WaitForWindowNames(engine);
         }
 
         //public override void Refresh(UI.Forms.ScriptBuilder.CommandEditor.frmCommandEditor editor)
