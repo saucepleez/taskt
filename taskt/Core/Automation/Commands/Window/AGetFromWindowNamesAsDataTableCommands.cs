48712492b5c7875698a71c78fe0a1ee1ec60b225
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-09-03 13:19:28 +0900

    add new commands

---
+++ taskt/Core/Automation/Commands/Window/AGetFromWindowNamesAsDataTableCommands.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for Get***FromWindowNamesAsDataTable commands
+    /// </summary>
+    public abstract class GetFromWindowNamesAsDataTableCommands : AWindowNamesCommands, IDataTableResultProperties
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_Result))]
+        [PropertyValidationRule("Result", PropertyValidationRule.ValidationRuleFlags.Empty)]
+        [PropertyInstanceType(PropertyInstanceType.InstanceType.DataTable)]
+        [PropertyParameterOrder(6500)]
+        public virtual string v_Result { get; set; }
+    }
+}
