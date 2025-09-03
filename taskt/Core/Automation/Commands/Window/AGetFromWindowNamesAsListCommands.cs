df1373e3a69916363c45334632fc132d0d4b95a0
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-09-03 16:40:34 +0900

    add new classes

---
+++ taskt/Core/Automation/Commands/Window/AGetFromWindowNamesAsListCommands.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for Get***FromWindowNamesAsList commands
+    /// </summary>
+    public abstract class GetFromWindowNamesAsListCommands : AWindowNamesCommands, ICanHandleList
+    {
+        // nothing, result paramters will be multiple
+    }
+}
---
+++ taskt/Core/Automation/Commands/Window/GetWindowHandlesFromWindowNamesAsDataTableCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    [Serializable]
+    [Attributes.ClassAttributes.Group("Window")]
+    [Attributes.ClassAttributes.SubGruop("Get From Window Name")]
+    [Attributes.ClassAttributes.CommandSettings("Get Window Handles From Window Names As DataTable")]
+    [Attributes.ClassAttributes.Description("This command returns window handles.")]
+    [Attributes.ClassAttributes.UsesDescription("Use this command when you want to get window handles.")]
+    [Attributes.ClassAttributes.ImplementationDescription("")]
+    [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_window))]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    public sealed class GetWindowHandlesFromWindowNamesAsDataTableCommand : GetFromWindowNamesAsDataTableCommands
+    {
+        [XmlAttribute]
+        [Remarks("Column Names are **WindowName**, **WindowHandle**")]
+        public override string v_Result { get; set; }
+
+        public GetWindowHandlesFromWindowNamesAsDataTableCommand()
+        {
+        }
+
+        public override void RunCommand(Engine.AutomationEngineInstance engine)
+        {
+            this.WindowNamesAction(engine, new Action<System.Collections.Generic.List<(IntPtr, string)>>(wins =>
+            {
+                var ret = new DataTable();
+                ret.Columns.Add("WindowName");
+                ret.Columns.Add("WindowHandle");
+                foreach ((var whnd, var name) in wins)
+                {
+                    ret.Rows.Add(new string[] { name, whnd.ToString(), });
+                }
+                this.StoreDataTableInUserVariable(ret, engine);
+            }));
+        }
+    }
+}
