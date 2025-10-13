c0a98aadd264f24a9a9291b831aae08e360fcdb1
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-10-13 11:31:55 +0900

    remove empty line

--- taskt/Core/Automation/Commands/UIAutomationGroup/UIAutomationSelectItemInUIElementCommand.cs
+++ taskt/Core/Automation/Commands/UIAutomationGroup/UIAutomationSelectItemInUIElementCommand.cs
@@ -5,7 +5,6 @@ using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
 {
-
     [Serializable]
     [Attributes.ClassAttributes.Group("UIAutomation")]
     [Attributes.ClassAttributes.SubGruop("UIElement Action")]
