8d02059914b46d82ca8462b89d6babc4eb57aa5e
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-10-13 21:59:31 +0900

    change inner process

--- taskt/Core/Automation/Commands/UIAutomationGroup/UIAutomationGetUIElementPositionCommand.cs
+++ taskt/Core/Automation/Commands/UIAutomationGroup/UIAutomationGetUIElementPositionCommand.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Windows;
+using System.Windows.Automation;
 using System.Xml.Serialization;
 using taskt.Core.Automation.Attributes.PropertyAttributes;
+using taskt.Core.Automation.Commands.UIAutomationGroup;
 
 namespace taskt.Core.Automation.Commands
 {
@@ -13,40 +16,40 @@ namespace taskt.Core.Automation.Commands
     [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_window))]
     [Attributes.ClassAttributes.EnableAutomateRender(true)]
     [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
-    public sealed class UIAutomationGetUIElementPositionCommand : ScriptCommand
+    public sealed class UIAutomationGetUIElementPositionCommand : AGetFromUIElementCommands, IPositionProperties
     {
-        [XmlAttribute]
-        [PropertyVirtualProperty(nameof(UIElementControls), nameof(UIElementControls.v_InputUIElementName))]
-        public string v_TargetElement { get; set; }
+        //[XmlAttribute]
+        //[PropertyVirtualProperty(nameof(UIElementControls), nameof(UIElementControls.v_InputUIElementName))]
+        //public string v_TargetElement { get; set; }
 
         [XmlAttribute]
-        [PropertyVirtualProperty(nameof(BooleanControls), nameof(BooleanControls.v_Result))]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_Result))]
         [PropertyDescription("Variable Name to Store X Position")]
         [PropertyIsOptional(true)]
         [PropertyValidationRule("X Position", PropertyValidationRule.ValidationRuleFlags.None)]
         [PropertyDisplayText(true, "X")]
+        [PropertyParameterOrder(6000)]
         public string v_XPosition { get; set; }
 
         [XmlAttribute]
-        [PropertyVirtualProperty(nameof(BooleanControls), nameof(BooleanControls.v_Result))]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_Result))]
         [PropertyDescription("Variable Name to Store Y Position")]
         [PropertyIsOptional(true)]
         [PropertyValidationRule("Y Position", PropertyValidationRule.ValidationRuleFlags.None)]
         [PropertyDisplayText(true, "Y Position")]
+        [PropertyParameterOrder(6100)]
         public string v_YPosition { get; set; }
 
         [XmlAttribute]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_ComboBox))]
         [PropertyDescription("Base position")]
-        [InputSpecification("", true)]
-        [SampleUsage("")]
-        [Remarks("")]
         [PropertyUISelectionOption("Top Left")]
         [PropertyUISelectionOption("Bottom Right")]
         [PropertyUISelectionOption("Top Right")]
         [PropertyUISelectionOption("Bottom Left")]
         [PropertyUISelectionOption("Center")]
-        [PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
         [PropertyIsOptional(true, "Top Left")]
+        [PropertyParameterOrder(6300)]
         public string v_PositionBase { get; set; }
 
         public UIAutomationGetUIElementPositionCommand()
@@ -55,43 +58,102 @@ namespace taskt.Core.Automation.Commands
 
         public override void RunCommand(Engine.AutomationEngineInstance engine)
         {
-            var targetElement = v_TargetElement.ExpandUserVariableAsUIElement(engine);
+            //var targetElement = v_TargetElement.ExpandUserVariableAsUIElement(engine);
 
-            var rct = targetElement.Current.BoundingRectangle;
+            //var rct = targetElement.Current.BoundingRectangle;
 
-            double x = 0.0, y = 0.0;
-            switch(this.ExpandValueOrUserVariableAsSelectionItem(nameof(v_PositionBase), engine))
-            {
-                case "top left":
-                    x = rct.Left;
-                    y = rct.Top;
-                    break;
-                case "bottom right":
-                    x = rct.Right;
-                    y = rct.Bottom;
-                    break;
-                case "top right":
-                    x = rct.Right;
-                    y = rct.Top;
-                    break;
-                case "bottom left":
-                    x = rct.Left;
-                    y = rct.Bottom;
-                    break;
-                case "center":
-                    x = (rct.Right - rct.Left) / 2.0;
-                    y = (rct.Bottom - rct.Top) / 2.0;
-                    break;
-            }
+            //double x = 0.0, y = 0.0;
+            //switch(this.ExpandValueOrUserVariableAsSelectionItem(nameof(v_PositionBase), engine))
+            //{
+            //    case "top left":
+            //        x = rct.Left;
+            //        y = rct.Top;
+            //        break;
+            //    case "bottom right":
+            //        x = rct.Right;
+            //        y = rct.Bottom;
+            //        break;
+            //    case "top right":
+            //        x = rct.Right;
+            //        y = rct.Top;
+            //        break;
+            //    case "bottom left":
+            //        x = rct.Left;
+            //        y = rct.Bottom;
+            //        break;
+            //    case "center":
+            //        x = (rct.Right - rct.Left) / 2.0;
+            //        y = (rct.Bottom - rct.Top) / 2.0;
+            //        break;
+            //}
 
-            if (!string.IsNullOrEmpty(v_XPosition))
-            {
-                x.StoreInUserVariable(engine, v_XPosition);
-            }
-            if (!string.IsNullOrEmpty(v_YPosition))
-            {
-                y.StoreInUserVariable(engine, v_YPosition);
-            }
+            //if (!string.IsNullOrEmpty(v_XPosition))
+            //{
+            //    x.StoreInUserVariable(engine, v_XPosition);
+            //}
+            //if (!string.IsNullOrEmpty(v_YPosition))
+            //{
+            //    y.StoreInUserVariable(engine, v_YPosition);
+            //}
+
+            this.UIElementAction(engine, 
+                new Action<AutomationElement>((targetElement) => {
+                    Rect rct;
+                    try
+                    {
+                        rct = targetElement.Current.BoundingRectangle;
+                    }
+                    catch
+                    {
+                        this.ValueCanNotRetrievedProcess("Position", new Action(() =>
+                        {
+                            if (!string.IsNullOrEmpty(v_XPosition))
+                            {
+                                "".StoreInUserVariable(engine, v_XPosition);
+                            }
+                            if (!string.IsNullOrEmpty(v_YPosition))
+                            {
+                                "".StoreInUserVariable(engine, v_YPosition);
+                            }
+                        }), engine);
+                        return;
+                    }
+
+                    double x = 0.0, y = 0.0;
+                    switch (this.ExpandValueOrUserVariableAsSelectionItem(nameof(v_PositionBase), engine))
+                    {
+                        case "top left":
+                            x = rct.Left;
+                            y = rct.Top;
+                            break;
+                        case "bottom right":
+                            x = rct.Right;
+                            y = rct.Bottom;
+                            break;
+                        case "top right":
+                            x = rct.Right;
+                            y = rct.Top;
+                            break;
+                        case "bottom left":
+                            x = rct.Left;
+                            y = rct.Bottom;
+                            break;
+                        case "center":
+                            x = (rct.Right - rct.Left) / 2.0;
+                            y = (rct.Bottom - rct.Top) / 2.0;
+                            break;
+                    }
+
+                    if (!string.IsNullOrEmpty(v_XPosition))
+                    {
+                        x.StoreInUserVariable(engine, v_XPosition);
+                    }
+                    if (!string.IsNullOrEmpty(v_YPosition))
+                    {
+                        y.StoreInUserVariable(engine, v_YPosition);
+                    }
+                })
+            );
         }
     }
 }
--- taskt/Core/Automation/Commands/UIAutomationGroup/UIAutomationGetUIElementSizeCommand.cs
+++ taskt/Core/Automation/Commands/UIAutomationGroup/UIAutomationGetUIElementSizeCommand.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Windows;
+using System.Windows.Automation;
 using System.Xml.Serialization;
 using taskt.Core.Automation.Attributes.PropertyAttributes;
+using taskt.Core.Automation.Commands.UIAutomationGroup;
 
 namespace taskt.Core.Automation.Commands
 {
@@ -13,26 +16,28 @@ namespace taskt.Core.Automation.Commands
     [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_window))]
     [Attributes.ClassAttributes.EnableAutomateRender(true)]
     [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
-    public sealed class UIAutomationGetUIElementSizeCommand : ScriptCommand
+    public sealed class UIAutomationGetUIElementSizeCommand : AGetFromUIElementCommands, ISizeProperties
     {
-        [XmlAttribute]
-        [PropertyVirtualProperty(nameof(UIElementControls), nameof(UIElementControls.v_InputUIElementName))]
-        public string v_TargetElement { get; set; }
+        //[XmlAttribute]
+        //[PropertyVirtualProperty(nameof(UIElementControls), nameof(UIElementControls.v_InputUIElementName))]
+        //public string v_TargetElement { get; set; }
 
         [XmlAttribute]
-        [PropertyVirtualProperty(nameof(BooleanControls), nameof(BooleanControls.v_Result))]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_Result))]
         [PropertyDescription("Variable Name to Store Width")]
         [PropertyIsOptional(true)]
         [PropertyValidationRule("Width", PropertyValidationRule.ValidationRuleFlags.None)]
         [PropertyDisplayText(true, "Width")]
+        [PropertyParameterOrder(6000)]
         public string v_Width { get; set; }
 
         [XmlAttribute]
-        [PropertyVirtualProperty(nameof(BooleanControls), nameof(BooleanControls.v_Result))]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_Result))]
         [PropertyDescription("Variable Name to Store Height")]
         [PropertyIsOptional(true)]
         [PropertyValidationRule("Height", PropertyValidationRule.ValidationRuleFlags.None)]
         [PropertyDisplayText(true, "Height")]
+        [PropertyParameterOrder(6100)]
         public string v_Height { get; set; }
 
         public UIAutomationGetUIElementSizeCommand()
@@ -41,17 +46,52 @@ namespace taskt.Core.Automation.Commands
 
         public override void RunCommand(Engine.AutomationEngineInstance engine)
         {
-            var targetElement = v_TargetElement.ExpandUserVariableAsUIElement(engine);
+            //var targetElement = v_TargetElement.ExpandUserVariableAsUIElement(engine);
 
-            var rct = targetElement.Current.BoundingRectangle;
-            if (!string.IsNullOrEmpty(v_Width))
-            {
-                rct.Width.StoreInUserVariable(engine, v_Width);
-            }
-            if (!string.IsNullOrEmpty(v_Height))
-            {
-                rct.Height.StoreInUserVariable(engine, v_Height);
-            }
+            //var rct = targetElement.Current.BoundingRectangle;
+            //if (!string.IsNullOrEmpty(v_Width))
+            //{
+            //    rct.Width.StoreInUserVariable(engine, v_Width);
+            //}
+            //if (!string.IsNullOrEmpty(v_Height))
+            //{
+            //    rct.Height.StoreInUserVariable(engine, v_Height);
+            //}
+
+            this.UIElementAction(engine,
+                new Action<AutomationElement>((targetElement) =>
+                {
+                    Rect rct;
+                    try
+                    {
+                        rct = targetElement.Current.BoundingRectangle;
+                    }
+                    catch
+                    {
+                        this.ValueCanNotRetrievedProcess("Size", new Action(() =>
+                        {
+                            if (!string.IsNullOrEmpty(v_Width))
+                            {
+                                "".StoreInUserVariable(engine, v_Width);
+                            }
+                            if (!string.IsNullOrEmpty(v_Height))
+                            {
+                                "".StoreInUserVariable(engine, v_Height);
+                            }
+                        }) , engine);
+                        return;
+                    }
+
+                    if (!string.IsNullOrEmpty(v_Width))
+                    {
+                        rct.Width.StoreInUserVariable(engine, v_Width);
+                    }
+                    if (!string.IsNullOrEmpty(v_Height))
+                    {
+                        rct.Height.StoreInUserVariable(engine, v_Height);
+                    }
+                })
+            );
         }
     }
 }
