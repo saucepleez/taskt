0699387786b82eb20ea9967e65a4cbf6022a7029
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-10-19 13:20:06 +0900

    add new command

---
+++ taskt/Core/Automation/Commands/UIAutomationGroup/UIAutomationGetWindowUIElementFromWindowHandleCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Automation;
+using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands
+{
+    [Serializable]
+    [Attributes.ClassAttributes.Group("UIAutomation")]
+    [Attributes.ClassAttributes.SubGruop("Window UIElement")]
+    [Attributes.ClassAttributes.CommandSettings("Get Window UIElement From Window Handle")]
+    [Attributes.ClassAttributes.Description("This command allows you to get UIElement from Window Handle")]
+    [Attributes.ClassAttributes.ImplementationDescription("Use this command when you want to get UIElement from Window Handle")]
+    [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_window))]
+    [Attributes.ClassAttributes.EnableAutomateRender(true)]
+    [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
+    public sealed class UIAutomationGetWindowUIElementFromWindowHandleCommand : AWindowHandleCommands
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(UIElementControls), nameof(UIElementControls.v_OutputUIElementName))]
+        [PropertyParameterOrder(5100)]
+        public string v_Result { get; set; }
+
+        public UIAutomationGetWindowUIElementFromWindowHandleCommand()
+        {
+        }
+
+        public override void RunCommand(Engine.AutomationEngineInstance engine)
+        {
+            this.WindowHandleAction(engine, new Action<IntPtr>(whnd =>
+            {
+                var ele = AutomationElement.FromHandle(whnd);
+                ele.StoreInUserVariable(engine, v_Result);
+            }));
+        }
+    }
+}
