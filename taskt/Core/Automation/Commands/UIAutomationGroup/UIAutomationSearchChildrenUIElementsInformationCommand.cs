dcd10c6f44a5cca3be1ad0c2be76dd8fd709b902
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-10-26 21:43:17 +0900

    change inner process

--- taskt/Core/Automation/Commands/UIAutomationGroup/UIAutomationSearchChildrenUIElementsInformationCommand.cs
+++ taskt/Core/Automation/Commands/UIAutomationGroup/UIAutomationSearchChildrenUIElementsInformationCommand.cs
@@ -1,62 +1,81 @@
 ﻿using System;
 using System.Data;
+using System.Windows.Automation;
 using System.Windows.Forms;
 using System.Xml.Serialization;
 using taskt.Core.Automation.Attributes.PropertyAttributes;
+using taskt.Core.Automation.Commands.UIAutomationGroup;
 
 namespace taskt.Core.Automation.Commands
 {
     [Serializable]
     [Attributes.ClassAttributes.Group("UIAutomation")]
     [Attributes.ClassAttributes.SubGruop("Get From UIElement")]
-    [Attributes.ClassAttributes.CommandSettings("Search Children Elements Information")]
+    [Attributes.ClassAttributes.CommandSettings("Search Children UIElements Information")]
     [Attributes.ClassAttributes.Description("This command allows you to get Children UIElements Information from UIElement.")]
     [Attributes.ClassAttributes.ImplementationDescription("Use this command when you want to get Children UIElements Information from UIElement.")]
     [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_window))]
     [Attributes.ClassAttributes.EnableAutomateRender(true)]
     [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
-    public sealed class UIAutomationSearchChildrenUIElementsInformationCommand : ScriptCommand, IHaveDataTableElements
+    public sealed class UIAutomationSearchChildrenUIElementsInformationCommand : ADoSomethingUIElementCommands, IUIElementSearchParametersProperties
     {
-        [XmlAttribute]
-        [PropertyVirtualProperty(nameof(UIElementControls), nameof(UIElementControls.v_InputUIElementName))]
-        public string v_TargetElement { get; set; }
+        //[XmlAttribute]
+        //[PropertyVirtualProperty(nameof(UIElementControls), nameof(UIElementControls.v_InputUIElementName))]
+        //public string v_TargetElement { get; set; }
 
         [XmlElement]
-        [PropertyVirtualProperty(nameof(UIElementControls), nameof(UIElementControls.v_SearchParameters))]
+        [PropertyVirtualProperty(nameof(VP_UIElementControls), nameof(VP_UIElementControls.v_SearchParameters))]
+        [PropertyParameterOrder(6000)]
         public DataTable v_SearchParameters { get; set; }
 
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_Result))]
+        [PropertyParameterOrder(6100)]
         public string v_Result { get; set; }
+
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(VP_UIElementControls), nameof(VP_UIElementControls.v_WaitTimeForUIElement))]
+        [PropertyParameterOrder(7100)]
+        public string v_WaitTimeForUIElement { get; set; }
 
         public UIAutomationSearchChildrenUIElementsInformationCommand()
         {
-            //this.CommandName = "UIAutomationGetChildrenElementsInformationCommand";
-            //this.SelectionName = "Get Children Elements Information";
-            //this.CommandEnabled = true;
-            //this.CustomRendering = true;
         }
 
         public override void RunCommand(Engine.AutomationEngineInstance engine)
         {
-            var targetElement = v_TargetElement.ExpandUserVariableAsUIElement(engine);
+            //var targetElement = v_TargetElement.ExpandUserVariableAsUIElement(engine);
 
-            var elems = UIElementControls.GetChildrenUIElements(targetElement, v_SearchParameters, engine);
+            //var elems = UIElementControls.GetChildrenUIElements(targetElement, v_SearchParameters, engine);
 
-            string result = "";
+            //string result = "";
 
-            int counts = elems.Count;
-            for (int i = 0; i < counts; i++)
+            //int counts = elems.Count;
+            //for (int i = 0; i < counts; i++)
+            //{
+            //    var elem = elems[i];
+            //    result += "Index: " + i + ", Name: " + elem.Current.Name + ", LocalizedControlType: " + elem.Current.LocalizedControlType + ", ControlType: " + UIElementControls.GetControlTypeText(elem.Current.ControlType) + "\n";
+            //}
+            //result.Trim().StoreInUserVariable(engine, v_Result);
+
+            this.UIElementAction(engine, new Action<AutomationElement>((targetElement) =>
             {
-                var elem = elems[i];
-                result += "Index: " + i + ", Name: " + elem.Current.Name + ", LocalizedControlType: " + elem.Current.LocalizedControlType + ", ControlType: " + UIElementControls.GetControlTypeText(elem.Current.ControlType) + "\n";
-            }
-            result.Trim().StoreInUserVariable(engine, v_Result);
+                var elems = this.SearchChildrenUIElements(targetElement, engine);
+
+                string result = "";
+
+                int counts = elems.Count;
+                for (int i = 0; i < counts; i++)
+                {
+                    var elem = elems[i];
+                    result += $"Index: {i}, Name: {elem.Current.Name}, LocalizedControlType: {elem.Current.LocalizedControlType}, ControlType: {EM_CanHandleUIElementExtentionMethods.GetControlTypeText(elem)}\n";
+                }
+                result.Trim().StoreInUserVariable(engine, v_Result);
+            }));
         }
 
         public override void AfterShown(UI.Forms.ScriptBuilder.CommandEditor.frmCommandEditor editor)
         {
-            //AutomationElementControls.RenderSearchParameterDataGridView((DataGridView)ControlsList[nameof(v_SearchParameters)]);
             UIElementControls.RenderSearchParameterDataGridView(ControlsList.GetPropertyControl<DataGridView>(nameof(v_SearchParameters)));
         }
 
