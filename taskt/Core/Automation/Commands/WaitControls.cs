dc49b9dc8d851333d1461eb8cac858a8e4154d30
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-10-27 22:39:56 +0900

    change text

--- taskt/Core/Automation/Commands/WaitControls.cs
+++ taskt/Core/Automation/Commands/WaitControls.cs
@@ -67,12 +67,12 @@ namespace taskt.Core.Automation.Commands
                     //test if we should exit and throw exception
                     if (DateTime.Now > stopWaiting)
                     {
-                        throw new Exception(targetName + " was not found in time!");
+                        throw new Exception($"{targetName} was not found in time!");
                     }
                     else
                     {
                         //put thread to sleep before iterating
-                        engine.ReportProgress(targetName + " Not Yet Found... " + (int)((stopWaiting - DateTime.Now).TotalSeconds) + "s remain");
+                        engine.ReportProgress($"{targetName} Not Yet Found... {(int)((stopWaiting - DateTime.Now).TotalSeconds)}s remain");
                         System.Threading.Thread.Sleep(1000);
                     }
                 }
