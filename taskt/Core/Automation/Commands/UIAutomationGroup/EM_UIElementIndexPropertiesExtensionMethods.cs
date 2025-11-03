9ae7c1a623bbecdc22255263d18eb8cb4e053b2a
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-11-03 13:50:21 +0900

    add new method

--- taskt/Core/Automation/Commands/UIAutomationGroup/EM_UIElementIndexPropertiesExtensionMethods.cs
+++ taskt/Core/Automation/Commands/UIAutomationGroup/EM_UIElementIndexPropertiesExtensionMethods.cs
@@ -1,4 +1,7 @@
-﻿namespace taskt.Core.Automation.Commands.UIAutomationGroup
+﻿using System.Collections.Generic;
+using System.Windows.Automation;
+
+namespace taskt.Core.Automation.Commands.UIAutomationGroup
 {
     public static class EM_UIElementIndexPropertiesExtensionMethods
     {
@@ -15,6 +18,31 @@
                 command.v_TargetUIElementIndex = "0";
             }
             return command.ToScriptCommand().ExpandValueOrUserVariableAsInteger(nameof(command.v_TargetUIElementIndex), "UIElement Index", engine);
+        }
+
+        /// <summary>
+        /// get UIElement from List-UIElement
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="elems"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        /// <exception cref="System.Exception"></exception>
+        public static AutomationElement GetUIElementFromLis(this IUIElementIndexProperties command, List<AutomationElement> elems, Engine.AutomationEngineInstance engine)
+        {
+            var index = command.ExpandValueOrUserVariableAsUIElementIndex(engine);
+            if (index < 0)
+            {
+                index += elems.Count;
+            }
+            if (index >= 0 && index < elems.Count)
+            {
+                return elems[index];
+            }
+            else
+            {
+                throw new System.Exception($"UIElement does not Exists. Index: {command.v_TargetUIElementIndex}, Expand Value: {index}");
+            }
         }
     }
 }
