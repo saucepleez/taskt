fe39596bb172d1f70b85fdc06e45da7475ae26b8
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-11-03 13:50:08 +0900

    add comment

--- taskt/Core/Automation/Commands/UIAutomationGroup/IUIElementDeepSearchAnyUIElementProperties.cs
+++ taskt/Core/Automation/Commands/UIAutomationGroup/IUIElementDeepSearchAnyUIElementProperties.cs
@@ -5,5 +5,6 @@
     /// </summary>
     public interface IUIElementDeepSearchAnyUIElementProperties : IUIElementDeepSearchParametersProperties, IUIElementIndexProperties
     {
+        // nothing
     }
 }
