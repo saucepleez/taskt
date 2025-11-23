0aa57771a9f35fc6384fc766c3b7e7752af8b159
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-11-23 20:28:58 +0900

    remove comment out codes

--- taskt/Core/Automation/Commands/UIAutomationGroup/EM_DoSomethingUIElementPropertiesExtensionMethods.cs
+++ taskt/Core/Automation/Commands/UIAutomationGroup/EM_DoSomethingUIElementPropertiesExtensionMethods.cs
@@ -7,34 +7,6 @@ namespace taskt.Core.Automation.Commands.UIAutomationGroup
 {
     public static class EM_DoSomethingUIElementPropertiesExtensionMethods
     {
-        ///// <summary>
-        ///// store Window name result
-        ///// </summary>
-        ///// <param name="command"></param>
-        ///// <param name="windowName"></param>
-        ///// <param name="engine"></param>
-        //public static void StoreWindowNameResultInUserVariable(this IDoSomethingUIElementProperties command, string windowName, AutomationEngineInstance engine)
-        //{
-        //    if (!string.IsNullOrEmpty(command.v_WindowNameResult))
-        //    {
-        //        windowName.StoreInUserVariable(engine, command.v_WindowNameResult);
-        //    }
-        //}
-
-        ///// <summary>
-        ///// store window handle result
-        ///// </summary>
-        ///// <param name="command"></param>
-        ///// <param name="whnd"></param>
-        ///// <param name="engine"></param>
-        //public static void StoreWindowHandleResultInUserVariable(this IDoSomethingUIElementProperties command, IntPtr whnd, AutomationEngineInstance engine)
-        //{
-        //    if (!string.IsNullOrEmpty(command.v_WindowHandleResult))
-        //    {
-        //        whnd.StoreInUserVariable(engine, command.v_WindowHandleResult);
-        //    }
-        //}
-
         /// <summary>
         /// UIElement action
         /// </summary>
