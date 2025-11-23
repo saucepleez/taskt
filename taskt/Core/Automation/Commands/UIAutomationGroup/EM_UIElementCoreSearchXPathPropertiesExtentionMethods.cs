e938f50004e699bad29360e2149ec5e9035ad9d4
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-11-23 11:08:06 +0900

    add methods xpath

---
+++ taskt/Core/Automation/Commands/UIAutomationGroup/EM_UIElementCoreSearchXPathPropertiesExtentionMethods.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Windows.Automation;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace taskt.Core.Automation.Commands.UIAutomationGroup
+{
+    public static class EM_UIElementCoreSearchXPathPropertiesExtentionMethods
+    {
+        /// <summary>
+        /// expand value or user variable string as XPath
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        public static string ExpandValueOrUserVariableAsXPath(this IUIElementCoreSearchXPathProperties command, Engine.AutomationEngineInstance engine)
+        {
+            var p = command.ToScriptCommand().ExpandValueOrUserVariable(nameof(command.v_SearchXPath), "XPath", engine);
+            if (!p.StartsWith("."))
+            {
+                p = $".{p}";
+            }
+            return p;
+        }
+
+        /// <summary>
+        /// search UIElement by xpath
+        /// </summary>
+        /// <param name="xpath"></param>
+        /// <param name="xml">UIElement XML</param>
+        /// <param name="elemsDic">UIElement dic</param>
+        /// <returns></returns>
+        public static AutomationElement SearchUIElementByXPath(string xpath, XElement xml, Dictionary<string, AutomationElement> elemsDic)
+        {
+            var e = xml.XPathSelectElement(xpath);
+            if (e != null)
+            {
+                return elemsDic[e.Attribute("Hash").Value];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- taskt/Core/Automation/Commands/UIAutomationGroup/EM_UIElementDeepSearchXPathPropertiesExtensionMethods.cs
+++ taskt/Core/Automation/Commands/UIAutomationGroup/EM_UIElementDeepSearchXPathPropertiesExtensionMethods.cs
@@ -11,9 +11,55 @@ namespace taskt.Core.Automation.Commands.UIAutomationGroup
 {
     public static class EM_UIElementDeepSearchXPathPropertiesExtensionMethods
     {
-
-        public static (XElement, Dictionary<string, AutomationElement>) DeepCreateUIElementXMLCore(this IUIElementDeepSearchXPathProperties command, AutomationElement targetElement, SafeAutomationEngineInstanceApplicationSettings engine)
+        /// <summary>
+        /// deep search UIElement by XPath
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="rootElement"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static AutomationElement DeepSearchUIElementByXPath(this IUIElementDeepSearchXPathProperties command, AutomationElement rootElement, Engine.AutomationEngineInstance engine)
         {
+            var waitTime = command.ExpandValueOrUserVariableAsWaitTimeForUIElement(engine);
+
+            var xpath = command.ExpandValueOrUserVariableAsXPath(engine);
+
+            var r = WaitControls.WaitProcess(waitTime, "UIElement", new Func<Func<bool>, (bool, object)>(waitFunc =>
+            {
+                (var xml, var dic) = command.DeepCreateUIElementXMLCore(rootElement, waitFunc, engine);
+                var elem = EM_UIElementCoreSearchXPathPropertiesExtentionMethods.SearchUIElementByXPath(xpath, xml, dic);
+                if (elem != null)
+                {
+                    return (true, elem);
+                }
+                else
+                {
+                    return (false, null);
+                }
+            }), engine);
+            if (r is AutomationElement e)
+            {
+                return e;
+            }
+            else
+            {
+                throw new Exception($"AutomationElement not Found");
+            }
+        }
+
+        /// <summary>
+        /// deep create UIElement xml
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="targetElement"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        public static (XElement, Dictionary<string, AutomationElement>) DeepCreateUIElementXMLCore(this IUIElementDeepSearchXPathProperties command, AutomationElement targetElement, Func<bool> timeFunc, Engine.AutomationEngineInstance engine)
+        {
+            var siblingFunc = command.GetMaxSiblingsFunc(engine);
+            var depthFunc = command.GetMaxDepthFunc(engine);
+
             var rootXML = EM_CanHandleUIElementXMLExtentionMethods.CreateXmlElement(targetElement);
 
             var hashDic = new Dictionary<string, AutomationElement>()
@@ -22,18 +68,31 @@ namespace taskt.Core.Automation.Commands.UIAutomationGroup
             };
 
             var walker = TreeWalker.RawViewWalker;
-            DeepCreateUIElementXML_DepthFirst(rootXML, targetElement, hashDic, walker);
+            DeepCreateUIElementXML_DepthFirst(rootXML, targetElement, hashDic, walker, 1, siblingFunc, depthFunc, timeFunc);
 
             return (rootXML, hashDic);
         }
 
-        private static void DeepCreateUIElementXML_DepthFirst(XElement rootNode, AutomationElement rootElement, Dictionary<string, AutomationElement> elemsDic, TreeWalker walker)
+        /// <summary>
+        /// deep create UIElement XML depth first
+        /// </summary>
+        /// <param name="parentXMLNode"></param>
+        /// <param name="rootElement"></param>
+        /// <param name="elemsDic"></param>
+        /// <param name="walker"></param>
+        /// <param name="depth"></param>
+        /// <param name="siblingFunc">when Func returns true, max siblings</param>
+        /// <param name="depthFunc">when Func returns true, max depth</param>
+        /// <param name="timeFunc">when Func returns true, time out</param>
+        private static void DeepCreateUIElementXML_DepthFirst(XElement parentXMLNode, AutomationElement rootElement, Dictionary<string, AutomationElement> elemsDic, 
+                                TreeWalker walker, int depth, Func<int, bool> siblingFunc, Func<int, bool> depthFunc, Func<bool> timeFunc)
         {
-            var node = walker.GetFirstChild(rootElement);
-            while (node != null)
+            var targetElement = walker.GetFirstChild(rootElement);
+            int sibCnt = 0;
+            while (targetElement != null)
             {
                 // check hash dup
-                string hash = node.GetHashCode().ToString();
+                string hash = targetElement.GetHashCode().ToString();
                 if (elemsDic.ContainsKey(hash))
                 {
                     int i = 1;
@@ -44,16 +103,33 @@ namespace taskt.Core.Automation.Commands.UIAutomationGroup
                     hash += $"-{i}";
                 }
 
-                var childNode = EM_CanHandleUIElementXMLExtentionMethods.CreateXmlElement(node, hash);
-                rootNode.Add(childNode);
-                elemsDic.Add(hash, node);
-
-                if (walker.GetFirstChild(node) != null)
+                var childNode = EM_CanHandleUIElementXMLExtentionMethods.CreateXmlElement(targetElement, hash);
+                parentXMLNode.Add(childNode);
+                elemsDic.Add(hash, targetElement);
+                sibCnt++;
+                if (siblingFunc(sibCnt) || timeFunc())
                 {
-                    DeepCreateUIElementXML_DepthFirst(childNode, node, elemsDic, walker);
+                    return;
                 }
 
-                node = walker.GetNextSibling(node);
+                if (walker.GetFirstChild(targetElement) != null)
+                {
+                    depth++;
+                    if (depthFunc(depth))
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        DeepCreateUIElementXML_DepthFirst(childNode, targetElement, elemsDic, walker, depth, siblingFunc, depthFunc, timeFunc);
+                    }
+                }
+                if (timeFunc())
+                {
+                    return;
+                }
+
+                targetElement = walker.GetNextSibling(targetElement);
             }
         }
     }
