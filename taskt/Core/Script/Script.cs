b4abccc8726dd2f5cc6633a5a4a8b5e099567df1
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-11-24 23:07:26 +0900

    remove using

--- taskt/Core/Script/Script.cs
+++ taskt/Core/Script/Script.cs
@@ -17,7 +17,6 @@ using System.Data;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using System.Xml;
