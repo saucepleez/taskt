c1c86f22cc142947618ce93cb44238c2fd69b313
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-01-11 18:12:13 +0900

    support header

--- taskt/Core/Automation/Commands/UIAutomationGroup/UIAutomationGetTableInformationFromUIElementCommand.cs
+++ taskt/Core/Automation/Commands/UIAutomationGroup/UIAutomationGetTableInformationFromUIElementCommand.cs
@@ -32,25 +32,88 @@ namespace taskt.Core.Automation.Commands
         [PropertyParameterOrder(7000)]
         public string v_Result { get; set; }
 
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(SelectionItemsControls), nameof(SelectionItemsControls.v_YesNoComboBox))]
+        [PropertyDescription("Contains Header Row, Column")]
+        [PropertyIsOptional(true, "No")]
+        [PropertyValidationRule("Contains Header", PropertyValidationRule.ValidationRuleFlags.None)]
+        [PropertyDisplayText(false, "Contains Header")]
+        [PropertyParameterOrder(8000)]
+        public string v_ContainsHeader { get; set; }
+
         public UIAutomationGetTableInformationFromUIElementCommand()
         {
         }
 
         public override void RunCommand(Engine.AutomationEngineInstance engine)
         {
+            var containsHeader = this.ExpandValueOrUserVariableAsYesNo(nameof(v_ContainsHeader), engine);
+            var infoType = this.ExpandValueOrUserVariableAsSelectionItem(nameof(v_InformationType), engine);
+
             this.UIElementAction(engine,
                 new Action<AutomationElement>((targetElement) =>
                 {
                     if (targetElement.TryGetCurrentPattern(GridPattern.Pattern, out object gridObj))
                     {
                         var grid = (GridPattern)gridObj;
-                        switch(this.ExpandValueOrUserVariableAsSelectionItem(nameof(v_InformationType), engine))
+                        if (containsHeader)
+                        {
+                            var customRows = EM_UIElementTableUIElement.GetRowsFromDataGridView(targetElement);
+                            if (customRows.Count > 0)
+                            {
+                                // DataGridView(.net)
+                                switch (infoType)
+                                {
+                                    case "column count":
+                                        var cols = EM_UIElementTableUIElement.GetColumnsFromDataGridView(customRows[0]);
+                                        cols.Count.StoreInUserVariable(engine, v_Result);
+                                        break;
+                                    case "row count":
+                                        customRows.Count.StoreInUserVariable(engine, v_Result);
+                                        break;
+                                }
+                            }
+                            else
+                            {
+                                // GridPattern
+                                var rows = EM_UIElementTableUIElement.GetRowsFromGridPattern(targetElement);
+                                switch (infoType)
+                                {
+                                    case "column count":
+                                        var cols = EM_UIElementTableUIElement.GetColumnsFromGridPattern(rows[0]);
+                                        cols.Count.StoreInUserVariable(engine, v_Result);
+                                        break;
+                                    case "row count":
+                                        rows.Count.StoreInUserVariable(engine, v_Result);
+                                        break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            switch (infoType)
+                            {
+                                case "column count":
+                                    grid.Current.ColumnCount.StoreInUserVariable(engine, v_Result);
+                                    break;
+                                case "row count":
+                                    grid.Current.RowCount.StoreInUserVariable(engine, v_Result);
+                                    break;
+                            }
+                        }
+                    }
+                    else if (targetElement.TryGetCurrentPattern(SelectionPattern.Pattern, out object selPattern))
+                    {
+                        var selPtn = (SelectionPattern)selPattern;
+                        var rows = EM_UIElementTableUIElement.GetRowsFromSelectionTable(targetElement);
+                        switch(infoType)
                         {
                             case "column count":
-                                grid.Current.ColumnCount.StoreInUserVariable(engine, v_Result);
+                                var cols = EM_UIElementTableUIElement.GetColumnsFromSelectionTable(rows[0]);
+                                cols.Count.StoreInUserVariable(engine, v_Result);
                                 break;
                             case "row count":
-                                grid.Current.RowCount.StoreInUserVariable(engine, v_Result);
+                                rows.Count.StoreInUserVariable(engine, v_Result);
                                 break;
                         }
                     }
