bc396c37dc9838be37eda0e9f1816ddfe6a7a7ae
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-01-25 10:52:29 +0900

    add new interface

---
+++ taskt/Core/Automation/Commands/WebBrowserGroup/ICanHandleWebDriver.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands.WebBrowserGroup
+{
+    /// <summary>
+    /// can handle WebDriver
+    /// </summary>
+    public interface ICanHandleWebDriver
+    {
+        // nothing
+    }
+}
