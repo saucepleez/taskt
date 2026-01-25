ac0329479e9890aebd6fd38973d76510c717dcf3
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-01-25 11:26:56 +0900

    remove methods not used

--- taskt/Core/Automation/Commands/WebBrowserGroup/EM_SeleniumDoSomethingToWebElementPropertiesExtensionMethods.cs
+++ taskt/Core/Automation/Commands/WebBrowserGroup/EM_SeleniumDoSomethingToWebElementPropertiesExtensionMethods.cs
@@ -14,64 +14,66 @@ namespace taskt.Core.Automation.Commands.WebBrowserGroup
         /// <exception cref="Exception"></exception>
         public static (IWebElement, IWebDriver) ExpandUserVariableAsWebElementAndWebDriver(this ISeleniumDoSomethingToWebElementProperties command, Engine.AutomationEngineInstance engine)
         {
-            var variableName = command.v_WebElement;
-            var v = variableName.GetRawVariable(engine);
-            // DBG
-            //Console.WriteLine(v.VariableValue.GetType().FullName);
+            //var variableName = command.v_WebElement;
+            //var v = variableName.GetRawVariable(engine);
+            //// DBG
+            ////Console.WriteLine(v.VariableValue.GetType().FullName);
 
-            if (IsWebElementAndWebDriverTuple(v.VariableValue, out ValueTuple<IWebElement, IWebDriver> elemDriv))
-            {
-                return elemDriv;
-            }
-            else
-            {
-                throw new Exception($"Variable '{variableName}' is not WebElement-WebDriver tuple");
-            }
+            //if (IsWebElementAndWebDriverTuple(v.VariableValue, out ValueTuple<IWebElement, IWebDriver> elemDriv))
+            //{
+            //    return elemDriv;
+            //}
+            //else
+            //{
+            //    throw new Exception($"Variable '{variableName}' is not WebElement-WebDriver tuple");
+            //}
+
+            return command.ExpandUserVariableAsWebElementAndWebDriver(command.v_WebElement, "WebElement", engine);
         }
 
-        /// <summary>
-        /// expand user variable as WebElement
-        /// </summary>
-        /// <param name="command"></param>
-        /// <param name="engine"></param>
-        /// <returns></returns>
-        public static IWebElement ExpandUserVariableAsWebElement(this ISeleniumDoSomethingToWebElementProperties command, Engine.AutomationEngineInstance engine)
-        {
-            (var e, _) = command.ExpandUserVariableAsWebElementAndWebDriver(engine);
-            return e;
-        }
+        ///// <summary>
+        ///// expand user variable as WebElement
+        ///// </summary>
+        ///// <param name="command"></param>
+        ///// <param name="engine"></param>
+        ///// <returns></returns>
+        //public static IWebElement ExpandUserVariableAsWebElement(this ISeleniumDoSomethingToWebElementProperties command, Engine.AutomationEngineInstance engine)
+        //{
+        //    (var e, _) = command.ExpandUserVariableAsWebElementAndWebDriver(engine);
+        //    return e;
+        //}
 
-        /// <summary>
-        /// expand user variable as WebDriver from WebElement
-        /// </summary>
-        /// <param name="command"></param>
-        /// <param name="engine"></param>
-        /// <returns></returns>
-        public static IWebDriver ExpandUserVariableAsWebDriverFromWebElement(this ISeleniumDoSomethingToWebElementProperties command, Engine.AutomationEngineInstance engine)
-        {
-            (_, var d) = command.ExpandUserVariableAsWebElementAndWebDriver(engine);
-            return d;
-        }
+        ///// <summary>
+        ///// expand user variable as WebDriver from WebElement
+        ///// </summary>
+        ///// <param name="command"></param>
+        ///// <param name="engine"></param>
+        ///// <returns></returns>
+        //public static IWebDriver ExpandUserVariableAsWebDriverFromWebElement(this ISeleniumDoSomethingToWebElementProperties command, Engine.AutomationEngineInstance engine)
+        //{
+        //    (_, var d) = command.ExpandUserVariableAsWebElementAndWebDriver(engine);
+        //    return d;
+        //}
 
-        /// <summary>
-        /// check object is WebElement-WebDriver tuple
-        /// </summary>
-        /// <param name="v"></param>
-        /// <param name="ret"></param>
-        /// <returns></returns>
-        private static bool IsWebElementAndWebDriverTuple(object v, out ValueTuple<IWebElement, IWebDriver> ret)
-        {
-            if (v is ValueTuple<IWebElement, IWebDriver> t)
-            {
-                ret = t;
-                return true;
-            }
-            else
-            {
-                ret = (null, null);
-                return false;
-            }
-        }
+        ///// <summary>
+        ///// check object is WebElement-WebDriver tuple
+        ///// </summary>
+        ///// <param name="v"></param>
+        ///// <param name="ret"></param>
+        ///// <returns></returns>
+        //private static bool IsWebElementAndWebDriverTuple(object v, out ValueTuple<IWebElement, IWebDriver> ret)
+        //{
+        //    if (v is ValueTuple<IWebElement, IWebDriver> t)
+        //    {
+        //        ret = t;
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        ret = (null, null);
+        //        return false;
+        //    }
+        //}
 
         /// <summary>
         /// WebElement action (not use try-catch)
