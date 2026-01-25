722b350b6b1e58ed297f2da3b343b13e56c1c8f1
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-01-25 10:50:11 +0900

    add new extension methods

---
+++ taskt/Core/Automation/Commands/WebBrowserGroup/EM_CanHandleWebElementExtensionMethods.cs
@@ -0,0 +1,69 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace taskt.Core.Automation.Commands.WebBrowserGroup
+{
+    public static class EM_CanHandleWebElementExtensionMethods
+    {
+        /// <summary>
+        /// expand user variable as WebElement and WebDriver
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static (IWebElement, IWebDriver) ExpandUserVariableAsWebElementAndWebDriver(this ICanHandleWebElement command, string str, string parameterName, Engine.AutomationEngineInstance engine)
+        {
+            var v = str.GetRawVariable(engine);
+
+            if (v.VariableValue is ValueTuple<IWebElement, IWebDriver> webs)
+            {
+                return webs;
+            }
+            else
+            {
+                throw new Exception($"{parameterName} '{str}' is not a WebElement-WebDriver tuple.");
+            }
+        }
+
+        /// <summary>
+        /// expand user variable as WebElement
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static IWebElement ExpandUserVariableAsWebElement(this ICanHandleWebElement command, string str, string parameterName, Engine.AutomationEngineInstance engine)
+        {
+            (var e, _) = command.ExpandUserVariableAsWebElementAndWebDriver(str, parameterName, engine);
+            return e;
+        }
+
+        /// <summary>
+        /// expand user variable as webDriver from WebElement
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="engine"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static IWebDriver ExpandUserVariableAsWebDriverFromWebElement(this ICanHandleWebElement command, string str, string parameterName, Engine.AutomationEngineInstance engine)
+        {
+            (_, var d) = command.ExpandUserVariableAsWebElementAndWebDriver(str, parameterName, engine);
+            return d;
+        }
+
+        /// <summary>
+        /// store WebElement (and WebDriver) to user variable
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="engine"></param>
+        /// <param name="targetVariable"></param>
+        public static void StoreInUserVariable(this ICanHandleWebElement command, IWebElement elem, IWebDriver driver, Engine.AutomationEngineInstance engine, string targetVariable)
+        {
+            ExtensionMethods.StoreInUserVariable(targetVariable, (elem, driver), engine, false);
+        }
+    }
+}
---
+++ taskt/Core/Automation/Commands/WebBrowserGroup/ICanHandleWebElement.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands.WebBrowserGroup
+{
+    /// <summary>
+    /// can handle WebElement
+    /// </summary>
+    public interface ICanHandleWebElement
+    {
+        // nothing
+    }
+}
