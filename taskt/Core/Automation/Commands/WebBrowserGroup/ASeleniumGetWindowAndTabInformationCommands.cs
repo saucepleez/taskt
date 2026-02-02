773842f748229d89e06a5606c7c06d1374a1eb1c
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-02-02 22:52:33 +0900

    add new abstract class

---
+++ taskt/Core/Automation/Commands/WebBrowserGroup/ASeleniumGetWindowAndTabInformationCommands.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands.WebBrowserGroup
+{
+    /// <summary>
+    /// for get Window And Tab information commands
+    /// </summary>
+    public abstract class ASeleniumGetWindowAndTabInformationCommands : ASeleniumGetFromWebDriverCommands
+    {
+        [XmlAttribute]
+        [PropertyParameterOrder(6000)]
+        public override abstract string v_Result { get; set; }
+    }
+}
