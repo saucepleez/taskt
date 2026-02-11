40165c982892070f2c6ed567f3d016da0d2ce830
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-02-11 15:36:35 +0900

    add new abstract class

---
+++ taskt/Core/Automation/Commands/WebBrowserGroup/ASeleniumSearchWebElementFromWebDriverCommands.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Xml.Serialization;
+using taskt.Core.Automation.Attributes.PropertyAttributes;
+
+namespace taskt.Core.Automation.Commands.WebBrowserGroup
+{
+    /// <summary>
+    /// for search one WebElement from WebDriver commands
+    /// </summary>
+    public abstract class ASeleniumSearchWebElementFromWebDriverCommands : ASeleniumSearchMultiWebElementsFromWebDriverCommands, ISeleniumSearchWebElementParametersProperties
+    {
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(VP_WebBrowserControls), nameof(VP_WebBrowserControls.v_WebElementIndex))]
+        [PropertyParameterOrder(7000)]
+        public string v_WebElementIndex { get; set; }
+
+        /// <summary>
+        /// search WebElement action
+        /// </summary>
+        /// <param name="actionFunc"></param>
+        /// <param name="engine"></param>
+        protected void SearchWebElementAction(Action<IWebElement> actionFunc, Engine.AutomationEngineInstance engine)
+        {
+            this.WebDriverActionCore(new System.Action<IWebDriver>(seleniumInsntance =>
+            {
+                var elem = this.SearchWebElement(seleniumInsntance, engine);
+                actionFunc(elem);
+            }), engine);
+        }
+    }
+}
