cffba4539a02cc8e1d9a92c3b9664ef61227371f
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-02-11 12:44:25 +0900

    remove using

--- taskt/Core/Automation/Commands/WebBrowserGroup/EM_SeleniumSearchParametersCorePropertiesExtensionMethods.cs
+++ taskt/Core/Automation/Commands/WebBrowserGroup/EM_SeleniumSearchParametersCorePropertiesExtensionMethods.cs
@@ -3,8 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using taskt.Core.Automation.Engine;
 
 namespace taskt.Core.Automation.Commands.WebBrowserGroup
