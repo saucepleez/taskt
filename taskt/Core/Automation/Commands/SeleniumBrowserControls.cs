c8cc3ca734178f66ae4deef6ceab2f326bcba73b
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-02-15 16:14:05 +0900

    remove using

--- taskt/Core/Automation/Commands/SeleniumBrowserControls.cs
+++ taskt/Core/Automation/Commands/SeleniumBrowserControls.cs
@@ -1,7 +1,5 @@
 ﻿using OpenQA.Selenium;
 using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
 using taskt.Core.Automation.Attributes.PropertyAttributes;
 
 namespace taskt.Core.Automation.Commands
