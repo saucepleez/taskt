3b659b6cd843f298e48867d8d77f74155748af8b
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-02-15 14:51:33 +0900

    change base class, inner process

--- taskt/Core/Automation/Commands/WebBrowserGroup/SeleniumBrowserGetWebElementsValuesAsDataTableCommand.cs
+++ taskt/Core/Automation/Commands/WebBrowserGroup/SeleniumBrowserGetWebElementsValuesAsDataTableCommand.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Windows.Forms;
 using System.Xml.Serialization;
 using taskt.Core.Automation.Attributes.PropertyAttributes;
+using taskt.Core.Automation.Commands.WebBrowserGroup;
+using taskt.Core.Script;
 
 namespace taskt.Core.Automation.Commands
 {
@@ -16,31 +20,33 @@ namespace taskt.Core.Automation.Commands
     [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_web))]
     [Attributes.ClassAttributes.EnableAutomateRender(true)]
     [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
-    public sealed class SeleniumBrowserGetWebElementsValuesAsDataTableCommand : ScriptCommand, ICanHandleDataTable, IHaveDataTableElements
+    public sealed class SeleniumBrowserGetWebElementsValuesAsDataTableCommand : ASeleniumSearchMultiWebElementsFromWebDriverCommands, IDataTableResultProperties, IHaveDataTableElements
     {
-        [XmlAttribute]
-        [PropertyVirtualProperty(nameof(SeleniumBrowserControls), nameof(SeleniumBrowserControls.v_InputInstanceName))]
-        public string v_InstanceName { get; set; }
+        //[XmlAttribute]
+        //[PropertyVirtualProperty(nameof(SeleniumBrowserControls), nameof(SeleniumBrowserControls.v_InputInstanceName))]
+        //public string v_InstanceName { get; set; }
 
-        [XmlAttribute]
-        [PropertyVirtualProperty(nameof(SeleniumBrowserControls), nameof(SeleniumBrowserControls.v_SearchMethod))]
-        public string v_SearchMethod { get; set; }
+        //[XmlAttribute]
+        //[PropertyVirtualProperty(nameof(SeleniumBrowserControls), nameof(SeleniumBrowserControls.v_SearchMethod))]
+        //public string v_SearchMethod { get; set; }
 
-        [XmlAttribute]
-        [PropertyVirtualProperty(nameof(SeleniumBrowserControls), nameof(SeleniumBrowserControls.v_SearchParameter))]
-        public string v_SearchParameter { get; set; }
+        //[XmlAttribute]
+        //[PropertyVirtualProperty(nameof(SeleniumBrowserControls), nameof(SeleniumBrowserControls.v_SearchParameter))]
+        //public string v_SearchParameter { get; set; }
 
         [XmlElement]
-        [PropertyVirtualProperty(nameof(SeleniumBrowserControls), nameof(SeleniumBrowserControls.v_AttributesName))]
+        [PropertyVirtualProperty(nameof(VP_WebBrowserControls), nameof(VP_WebBrowserControls.v_AttributeNames))]
+        [PropertyParameterOrder(7000)]
         public DataTable v_AttributesName { get; set; }
 
         [XmlAttribute]
         [PropertyVirtualProperty(nameof(DataTableControls), nameof(DataTableControls.v_OutputDataTableName))]
-        public string v_Result { get; set; }
+        [PropertyParameterOrder(8000)]
+        public override string v_Result { get; set; }
 
-        [XmlAttribute]
-        [PropertyVirtualProperty(nameof(SeleniumBrowserControls), nameof(SeleniumBrowserControls.v_WaitTime))]
-        public string v_WaitTimeForWebElement { get; set; }
+        //[XmlAttribute]
+        //[PropertyVirtualProperty(nameof(SeleniumBrowserControls), nameof(SeleniumBrowserControls.v_WaitTime))]
+        //public string v_WaitTimeForWebElement { get; set; }
 
         public SeleniumBrowserGetWebElementsValuesAsDataTableCommand()
         {
@@ -52,34 +58,88 @@ namespace taskt.Core.Automation.Commands
 
         public override void RunCommand(Engine.AutomationEngineInstance engine)
         {
-            //(var _, var elems) = SeleniumBrowserControls.GetSeleniumBrowserInstanceAndElements(this, nameof(v_InstanceName), nameof(v_SeleniumSearchType), nameof(v_SeleniumSearchParameter), engine);
-            (var _, var elems) = SeleniumBrowserControls.ExpandValueOrUserVariableAsSeleniumBrowserInstanceAndWebElements(this, nameof(v_InstanceName), nameof(v_SearchMethod), nameof(v_SearchParameter), nameof(v_WaitTimeForWebElement), engine);
+            ////(var _, var elems) = SeleniumBrowserControls.GetSeleniumBrowserInstanceAndElements(this, nameof(v_InstanceName), nameof(v_SeleniumSearchType), nameof(v_SeleniumSearchParameter), engine);
+            //(var _, var elems) = SeleniumBrowserControls.ExpandValueOrUserVariableAsSeleniumBrowserInstanceAndWebElements(this, nameof(v_InstanceName), nameof(v_SearchMethod), nameof(v_SearchParameter), nameof(v_WaitTimeForWebElement), engine);
 
-            var newDT = new DataTable();
+            //var newDT = new DataTable();
 
-            int attrs = v_AttributesName.Rows.Count;
-            for (int i = 0; i <attrs; i++)
+            //int attrs = v_AttributesName.Rows.Count;
+            //for (int i = 0; i <attrs; i++)
+            //{
+            //    var attr = v_AttributesName.Rows[i][0]?.ToString().ExpandValueOrUserVariable(engine) ?? "";
+            //    if (!newDT.Columns.Contains(attr))
+            //    {
+            //        newDT.Columns.Add(attr);
+            //    }
+            //}
+
+            //int rows = elems.Count;
+            //int cols = newDT.Columns.Count;
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    newDT.Rows.Add();
+            //    for (int j = 0; j < cols; j++)
+            //    {
+            //        newDT.Rows[i][j] = SeleniumBrowserControls.GetAttribute(elems[i], newDT.Columns[j].ColumnName, engine);
+            //    }
+            //}
+
+            ////newDT.StoreInUserVariable(engine, v_DataTableVariableName);
+            //this.StoreDataTableInUserVariable(newDT, nameof(v_Result), engine);
+
+            this.SearchMultiWebElementsAction(new Action<List<IWebElement>>(elems =>
             {
-                var attr = v_AttributesName.Rows[i][0]?.ToString().ExpandValueOrUserVariable(engine) ?? "";
-                if (!newDT.Columns.Contains(attr))
+                using (var elemVar = new InnerScriptVariable(engine))
                 {
-                    newDT.Columns.Add(attr);
-                }
-            }
+                    using (var resVar = new InnerScriptVariable(engine))
+                    {
+                        // create attribute names list
+                        var attrs = new List<string>();
+                        var showAttrs = new List<string>();
+                        foreach (DataRow row in v_AttributesName.Rows)
+                        {
+                            var expandAttr = (row[0]?.ToString() ?? string.Empty).ExpandValueOrUserVariable(engine);
+                            attrs.Add(expandAttr);
+                            showAttrs.Add((expandAttr.StartsWith("@") ? expandAttr.Substring(1) : expandAttr));
+                        }
 
-            int rows = elems.Count;
-            int cols = newDT.Columns.Count;
-            for (int i = 0; i < rows; i++)
-            {
-                newDT.Rows.Add();
-                for (int j = 0; j < cols; j++)
-                {
-                    newDT.Rows[i][j] = SeleniumBrowserControls.GetAttribute(elems[i], newDT.Columns[j].ColumnName, engine);
-                }
-            }
+                        // set column names
+                        var res = this.CreateEmptyDataTable();
+                        foreach (var attr in showAttrs)
+                        {
+                            res.Columns.Add(attr);
+                        }
 
-            //newDT.StoreInUserVariable(engine, v_DataTableVariableName);
-            this.StoreDataTableInUserVariable(newDT, nameof(v_Result), engine);
+                        foreach (var elem in elems)
+                        {
+                            elemVar.VariableValue = new ValueTuple<IWebElement, IWebDriver>(elem, null);
+
+                            var getAttr = new SeleniumBrowserGetAttributeFromWebElementCommand()
+                            {
+                                v_WebElement = elemVar.VariableName,
+                                v_Result = resVar.VariableName,
+                            };
+
+                            var row = res.NewRow();
+
+                            int idx = 0;
+                            foreach (var attr in attrs)
+                            {
+                                getAttr.v_AttributeName = attr;
+                                getAttr.RunCommand(engine);
+
+                                row[showAttrs[idx]] = resVar.VariableValue.ToString();
+
+                                idx++;
+                            }
+
+                            res.Rows.Add(row);
+                        }
+
+                        this.StoreDataTableInUserVariable(res, engine);
+                    }
+                }
+            }), engine);
         }
 
         public override void BeforeValidate()
