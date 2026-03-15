783051c2eddc0ea13acbe41660136d1ff9d5d928
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-03-15 10:54:29 +0900

    support viewport/screen

--- taskt/Core/Automation/Commands/WebBrowserGroup/SeleniumBrowserGetWebElementPositionCommand.cs
+++ taskt/Core/Automation/Commands/WebBrowserGroup/SeleniumBrowserGetWebElementPositionCommand.cs
@@ -2,6 +2,7 @@
 using System.Xml.Serialization;
 using taskt.Core.Automation.Attributes.PropertyAttributes;
 using taskt.Core.Automation.Commands.WebBrowserGroup;
+using taskt.Core.Script;
 
 namespace taskt.Core.Automation.Commands
 {
@@ -15,7 +16,7 @@ namespace taskt.Core.Automation.Commands
     [Attributes.ClassAttributes.CommandIcon(nameof(Properties.Resources.command_web))]
     [Attributes.ClassAttributes.EnableAutomateRender(true)]
     [Attributes.ClassAttributes.EnableAutomateDisplayText(true)]
-    public sealed class SeleniumWebElementPositionCommand : ASeleniumGetFromWebElementCommands
+    public sealed class SeleniumWebElementPositionCommand : ASeleniumGetFromWebElementCommands, IPositionProperties
     {
         //[XmlAttribute]
         //[PropertyVirtualProperty(nameof(SeleniumBrowserControls), nameof(SeleniumBrowserControls.v_InputWebElementName))]
@@ -52,51 +53,21 @@ namespace taskt.Core.Automation.Commands
         [PropertyParameterOrder(7000)]
         public string v_PositionBase { get; set; }
 
+        [XmlAttribute]
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_ComboBox))]
+        [PropertyDescription("Position Type")]
+        [PropertyUISelectionOption("Screen")]
+        [PropertyUISelectionOption("Viewport")]
+        [PropertyIsOptional(true, "Viewport")]
+        [PropertyParameterOrder(7100)]
+        public string v_PositionType { get; set; }
+
         public SeleniumWebElementPositionCommand()
         {
         }
 
         public override void RunCommand(Engine.AutomationEngineInstance engine)
         {
-            //var elem = v_WebElement.ExpandUserVariableAsWebElement("WebElement", engine);
-
-            //var loc = elem.Location;
-            //var size = elem.Size;
-
-            //int x = 0, y = 0;
-            //switch(this.ExpandValueOrUserVariableAsSelectionItem(nameof(v_PositionBase), engine))
-            //{
-            //    case "top left":
-            //        x = loc.X;
-            //        y = loc.Y;
-            //        break;
-            //    case "bottom right":
-            //        x = loc.X + size.Width;
-            //        y = loc.Y + size.Height;
-            //        break;
-            //    case "top right":
-            //        x = loc.X + size.Width;
-            //        y = loc.Y;
-            //        break;
-            //    case "bottom left":
-            //        x = loc.X;
-            //        y = loc.Y + size.Height;
-            //        break;
-            //    case "center":
-            //        x = (loc.X + size.Width) / 2;
-            //        y = (loc.Y + size.Height) / 2;
-            //        break;
-            //}
-
-            //if (!string.IsNullOrEmpty(v_XPosition))
-            //{
-            //    x.StoreInUserVariable(engine, v_XPosition);
-            //}
-            //if (!string.IsNullOrEmpty(v_YPosition))
-            //{
-            //    y.StoreInUserVariable(engine, v_YPosition);
-            //}
-
             this.GetFromWebElementAction(new Action<OpenQA.Selenium.IWebElement, OpenQA.Selenium.IWebDriver>((elem, seleniumInstance) =>
             {
                 var loc = elem.Location;
@@ -124,6 +95,35 @@ namespace taskt.Core.Automation.Commands
                     case "center":
                         x = (loc.X + size.Width) / 2;
                         y = (loc.Y + size.Height) / 2;
+                        break;
+                }
+
+                switch(this.ExpandValueOrUserVariableAsSelectionItem(nameof(v_PositionType), engine))
+                {
+                    case "screen":
+                        // get Screen(Desktop) position
+                        int baseX, baseY;
+                        using (var vX = new InnerScriptVariable(engine))
+                        {
+                            using (var vY = new InnerScriptVariable(engine))
+                            {
+                                var insName = this.GetInstanceNameFromWebBrowserInstance(seleniumInstance, engine);
+
+                                var getPos = new SeleniumBrowserGetWebBrowserPositionCommand()
+                                {
+                                    v_InstanceName = insName,
+                                    v_PositionType = "Viewport",
+                                    v_XPosition = vX.VariableName,
+                                    v_YPosition = vY.VariableName,
+                                };
+                                getPos.RunCommand(engine);
+
+                                baseX = int.Parse(vX.VariableValue.ToString());
+                                baseY = int.Parse(vY.VariableValue.ToString());
+                            }
+                        }
+                        x += baseX;
+                        y += baseY;
                         break;
                 }
 
