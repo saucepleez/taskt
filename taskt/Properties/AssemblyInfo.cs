3fbbd86d84400e0c14370affcb2c669ed747fb6f
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-03-15 17:40:57 +0900

    2026

--- taskt/Properties/AssemblyInfo.cs
+++ taskt/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("taskt")]
-[assembly: AssemblyCopyright("Copyright ©2025")]
+[assembly: AssemblyCopyright("Copyright ©2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
