51dc392920f9b2292e64c2c696c645d97d7c0b8f
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-03-22 16:36:50 +0900

    remove using

--- taskt/Core/Automation/Commands/_General/ISelectionMethodProperties.cs
+++ taskt/Core/Automation/Commands/_General/ISelectionMethodProperties.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace taskt.Core.Automation.Commands
+﻿namespace taskt.Core.Automation.Commands
 {
     /// <summary>
     /// general selection properties
