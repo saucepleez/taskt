74594dafcc62d648c2c2bd33e6251838d910416d
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-05-17 13:36:24 +0900

    add convert method

--- taskt/Core/Script/Script.cs
+++ taskt/Core/Script/Script.cs
@@ -17,7 +17,6 @@ using System.Data;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using System.Xml;
@@ -5696,7 +5695,8 @@ namespace taskt.Core.Script
 
         private static void convertTo3_5_2_64(XDocument doc)
         {
-            var cmds = GetCommands(doc, new Func<XElement, bool>(el =>
+            // add v_SelectionMethod in WebBrowser commands
+            var webCmds = GetCommands(doc, new Func<XElement, bool>(el =>
             {
                 switch (GetCommandName(el))
                 {
@@ -5711,7 +5711,7 @@ namespace taskt.Core.Script
                         return false;
                 }
             }));
-            foreach(var cmd in cmds)
+            foreach(var cmd in webCmds)
             {
                 var indexAttr = cmd.Attribute("v_WebElementIndex");
                 if (indexAttr != null)
@@ -5722,6 +5722,63 @@ namespace taskt.Core.Script
                     }
                 }
             }
+            webCmds = null;
+
+            // add v_WindowSelectionMethod when it does not exists
+            var uiAction = GetCommands(doc, new Func<XElement, bool>(el =>
+            {
+                switch (GetCommandName(el))
+                {
+                    case "UIAutomationSearchUIElementFromWindowNameCommand":
+                    case "UIAutomationUIElementActionAfterSearchUIElementFromWindowNameCommand":
+                        return true;
+                    default:
+                        return false;
+                }
+            }));
+            foreach (var cmd in uiAction)
+            {
+                var winSelAttr = cmd.Attribute("v_WindowSelectionMethod");
+                if (winSelAttr == null)
+                {
+                    var selAttr = cmd.Attribute("v_SelectionMethod");
+                    var winSelValue = selAttr.Value;
+                    selAttr.Remove();
+                    cmd.SetAttributeValue("v_WindowSelectionMethod", winSelValue);
+                }
+            }
+            uiAction = null;
+
+            // add v_SelectionMethod in UIAutomation commands
+            var uiCmds = GetCommands(doc, new Func<XElement, bool>(el =>
+            {
+                switch (GetCommandName(el))
+                {
+                    case "UIAutomationUIElementActionAfterSearchUIElementFromWindowHandleCommand":
+                    case "UIAutomationUIElementActionAfterSearchUIElementFromWindowNameCommand":
+                    case "UIAutomationCheckUIElementExistsCommand":
+                    case "UIAutomationSearchChildUIElementCommand":
+                    case "UIAutomationSearchUIElementFromUIElementCommand":
+                    case "UIAutomationWaitForUIElementToExistsCommand":
+                    case "UIAutomationSearchUIElementFromWindowHandleCommand":
+                    case "UIAutomationSearchUIElementFromWindowNameCommand":
+                        return true;
+                    default:
+                        return false;
+                }
+            }));
+            foreach (var cmd in uiCmds)
+            {
+                var indexAttr = cmd.Attribute("v_TargetUIElementIndex");
+                if (indexAttr != null)
+                {
+                    if (!string.IsNullOrEmpty(indexAttr.Value))
+                    {
+                        cmd.SetAttributeValue("v_SelectionMethod", "Index");
+                    }
+                }
+            }
+            uiCmds = null;
         }
 
         /// <summary>
