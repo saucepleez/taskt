4577322c59f3142572d3129f2159c8902784559f
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2026-08-02 15:41:50 +0900

    add interface, extensionmethods

---
+++ taskt/Core/Automation/Commands/ExcelGroup/AExcelShapesCommands.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+
+namespace taskt.Core.Automation.Commands.ExcelGroup
+{
+    /// <summary>
+    /// do something to Excel Shapes commands
+    /// </summary>
+    public abstract class AExcelShapesCommands : AExcelInstanceCommands, ICanHandleExcelShapes
+    {
+        /// <summary>
+        /// excel shapes action
+        /// </summary>
+        /// <param name="engine"></param>
+        /// <param name="shapesFunc"></param>
+        /// <param name="errorFunc"></param>
+        protected void ExcelShapesAction(Engine.AutomationEngineInstance engine, Action<List<Shape>> shapesFunc, Action<Exception> errorFunc = null)
+        {
+            (_, var sht) = this.ExpandValueOrVariableAsExcelInstanceAndCurrentWorksheet(engine);
+            var charts = this.GetShapesFromWorksheet(sht);
+            try
+            {
+                shapesFunc(charts);
+            }
+            catch (Exception ex)
+            {
+                if (errorFunc != null)
+                {
+                    errorFunc(ex);
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
+        }
+    }
+}
